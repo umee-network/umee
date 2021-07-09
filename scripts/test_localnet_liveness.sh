6d9dafd3c7d57df9ca2c3c0b2cd1cf8373586e18
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-07-09 08:44:43 -0400

    ci++

---
+++ scripts/test_localnet_liveness.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+CNT=0
+ITER=$1
+SLEEP=$2
+NUMBLOCKS=$3
+NODEADDR=$4
+
+if [ -z "$1" ]; then
+  echo "Invalid argument: missing number of iterations"
+  echo "sh test_localnet_liveness.sh <iterations> <sleep> <num-blocks> <node-address>"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "Invalid argument: missing sleep duration"
+  echo "sh test_localnet_liveness.sh <iterations> <sleep> <num-blocks> <node-address>"
+  exit 1
+fi
+
+if [ -z "$3" ]; then
+  echo "Invalid argument: missing number of blocks"
+  echo "sh test_localnet_liveness.sh <iterations> <sleep> <num-blocks> <node-address>"
+  exit 1
+fi
+
+if [ -z "$4" ]; then
+  echo "Invalid argument: missing node address"
+  echo "sh test_localnet_liveness.sh <iterations> <sleep> <num-blocks> <node-address>"
+  exit 1
+fi
+
+docker_containers=($(docker ps -q -f name=umeed --format='{{.Names}}'))
+
+while [ ${CNT} -lt $ITER ]; do
+  curr_block=$(curl -s $NODEADDR:26657/status | jq -r '.result.sync_info.latest_block_height')
+
+  if [ ! -z ${curr_block} ]; then
+    echo "Current block: ${curr_block}"
+  fi
+
+  if [ ! -z ${curr_block} ] && [ ${curr_block} -gt ${NUMBLOCKS} ]; then
+    echo "Success: number of blocks reached"
+    exit 0
+  fi
+
+  # Emulate network chaos:
+  #
+  # Every 10 blocks, pick a random container and restart it.
+  if ! ((${CNT} % 10)); then
+    rand_container=${docker_containers["$((RANDOM % ${#docker_containers[@]}))"]}
+    echo "Restarting random docker container: ${rand_container}"
+    docker restart ${rand_container} &>/dev/null &
+  fi
+
+  let CNT=CNT+1
+  sleep $SLEEP
+done
+
+echo "Failed: timeout reached"
+exit 1
