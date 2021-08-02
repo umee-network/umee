31fb137975082e978b71b32550520bdb8a7758d2
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-08-02 13:43:50 -0400

    e2e: doc++

--- tests/e2e/doc.go
+++ tests/e2e/doc.go
@@ -3,8 +3,13 @@
 //
 // The file e2e_suite_test.go defines the testing suite and contains the core
 // bootrapping logic that creates a testing environment via Docker containers.
-// A testing network is created dynamically and contains multiple Umee validator
-// processes, TODO: ...
+// A testing network is created dynamically and contains multiple Docker
+// containers:
+//
+// 1. A single Ethereum testnet process
+// 2. A configurable number of Umee validator processes
+// 3. An orchestrator process for each validator
+// 4. A gravity contract deployer process
 //
 // The file e2e_test.go contains the actual end-to-end integration tests that
 // utilize the testing suite.
