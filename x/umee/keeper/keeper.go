aae815379a5bd45150c60d213bf147a6fa43ec9a
toteki <63419657+toteki@users.noreply.github.com>
2021-09-22 10:42:42 -0600

    renamings and sdk.NewCoin

--- x/umee/keeper/keeper.go
+++ x/umee/keeper/keeper.go
@@ -35,13 +35,13 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 // IsAcceptedAsset returns true if a given denom is an accepted asset (not uToken) type.
 func (k Keeper) IsAcceptedAsset(ctx sdk.Context, denom string) bool {
 	// Has an associated utoken iff it's an accepted asset
-	return k.GetAssociatedUtoken(ctx, denom) != ""
+	return k.FromBaseAssetDenom(ctx, denom) != ""
 }
 
 // IsAcceptedUtoken returns true if a given denom is an accepted uToken (not asset) type.
 func (k Keeper) IsAcceptedUtoken(ctx sdk.Context, denom string) bool {
 	// Has an associated asset iff it's an accepted utoken
-	return k.GetAssociatedAsset(ctx, denom) != ""
+	return k.ToBaseAssetDenom(ctx, denom) != ""
 }
 
 // TotalUtokenSupply returns an sdk.Coin representing the total balance of a given uToken type if valid.
@@ -54,11 +54,8 @@ func (k Keeper) TotalUtokenSupply(ctx sdk.Context, denom string) sdk.Coin {
 		// then the balance returned here would decrease when the tokens are sent off, which is not
 		// what we want. In that case, the keeper should keep an sdk.Int total supply for each uToken type.
 	}
-	return sdk.Coin{
-		// Return zero amount on not existing uToken type
-		Denom:  denom,
-		Amount: sdk.ZeroInt(),
-	}
+	// Return zero amount on not existing uToken type
+	return sdk.NewCoin(denom, sdk.ZeroInt())
 }
 
 // TotalAssetBalance returns an sdk.Coin representing the total balance of a given asset type if valid.
@@ -66,21 +63,15 @@ func (k Keeper) TotalUtokenSupply(ctx sdk.Context, denom string) sdk.Coin {
 // denom.
 func (k Keeper) TotalAssetBalance(ctx sdk.Context, denom string) sdk.Coin {
 	if k.IsAcceptedAsset(ctx, denom) {
-		return sdk.Coin{
-			Denom:  denom,
-			Amount: sdk.ZeroInt(), // TODO: Amount = module account total balance of this token type
-		}
+		return sdk.NewCoin(denom, sdk.ZeroInt()) // TODO: Amount = module account total balance of this token type
 	}
-	return sdk.Coin{
-		// Return zero amount on not accepted asset
-		Denom:  denom,
-		Amount: sdk.ZeroInt(),
-	}
+	// Return zero amount on not accepted asset
+	return sdk.NewCoin(denom, sdk.ZeroInt())
 }
 
-// GetAssociatedAsset returns the asset type associated with a given uToken. Empty string on non-uToken
+// ToBaseAssetDenom returns the asset type associated with a given uToken. Empty string on non-uToken
 // input.
-func (k Keeper) GetAssociatedAsset(ctx sdk.Context, denom string) string {
+func (k Keeper) ToBaseAssetDenom(ctx sdk.Context, denom string) string {
 	// TODO: Remove hard-coding, and store this for each uToken denom using keeper
 	// 	under UtokenAssociatedAssetPrefix+denom
 	if denom == "u/uumee" {
@@ -89,9 +80,9 @@ func (k Keeper) GetAssociatedAsset(ctx sdk.Context, denom string) string {
 	return ""
 }
 
-// GetAssociatedUtoken returns the asset type associated with a given asset. Empty string on
+// FromBaseAssetDenom returns the uToken type associated with a given asset. Empty string on
 // non-accepted-asset input.
-func (k Keeper) GetAssociatedUtoken(ctx sdk.Context, denom string) string {
+func (k Keeper) FromBaseAssetDenom(ctx sdk.Context, denom string) string {
 	// TODO: Remove hard-coding, and store this for each accepted asset denom using keeper
 	// 	under AssetAssociatedUtokenPrefix+denom
 	if denom == "uumee" {
@@ -101,23 +92,20 @@ func (k Keeper) GetAssociatedUtoken(ctx sdk.Context, denom string) string {
 
 }
 
-// DepositAssets attempts to deposit assets into the leverage module in exchange for uTokens.
+// LendAssets attempts to deposit assets into the leverage module in exchange for uTokens.
 // If asset type is invalid or account balance is insufficient, does nothing and returns false.
 // TODO: Panic if partially executed then fail?
-func (k Keeper) DepositAssets(ctx sdk.Context, lender sdk.AccAddress, assets sdk.Coin) bool {
+func (k Keeper) LendAssets(ctx sdk.Context, lender sdk.AccAddress, assets sdk.Coin) bool {
 	if !k.IsAcceptedAsset(ctx, assets.Denom) {
 		// Not an accepted asset type for lending
 		return false
 	}
-	utokenDenom := k.GetAssociatedUtoken(ctx, assets.Denom)
+	utokenDenom := k.FromBaseAssetDenom(ctx, assets.Denom)
 	if !k.bankKeeper.HasBalance(ctx, lender, assets) {
 		// Lender does not have the assets they intend to lend
 		return false
 	}
-	utokens := sdk.Coin{
-		Denom:  utokenDenom,
-		Amount: assets.Amount, // TODO: Use exchange rate instead of 1:1 redeeming
-	}
+	utokens := sdk.NewCoin(utokenDenom, assets.Amount) // TODO: Use exchange rate instead of 1:1 redeeming
 	// TODO: Add additional checks to be sure tokens/accounts are good to send from before doing the
 	// irreversible
 	if k.bankKeeper.MintCoins(ctx, "leverage", sdk.Coins{utokens}) != nil {
@@ -146,15 +134,12 @@ func (k Keeper) RedeemUtokens(ctx sdk.Context, lender sdk.AccAddress, utokens sd
 		// Not an accepted utoken type
 		return false
 	}
-	assetDenom := k.GetAssociatedAsset(ctx, utokens.Denom)
+	assetDenom := k.ToBaseAssetDenom(ctx, utokens.Denom)
 	if !k.bankKeeper.HasBalance(ctx, lender, utokens) {
 		// Lender does not have the uTokens they intend to redeem
 		return false
 	}
-	assets := sdk.Coin{
-		Denom:  assetDenom,
-		Amount: utokens.Amount, // TODO: Use exchange rate instead of 1:1 redeeming
-	}
+	assets := sdk.NewCoin(assetDenom, utokens.Amount) // TODO: Use exchange rate instead of 1:1 redeeming
 	/*
 		TODO: once we have leverage module's account address
 		if k.bankKeeper.HasBalance(ctx,module_account_address,assets) == false {
