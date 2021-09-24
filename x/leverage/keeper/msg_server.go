229962f16ecf2ac65529c290277eea0a146c466f
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-09-24 09:49:15 -0400

    x/leverage updates

---
+++ x/leverage/keeper/msg_server.go
@@ -0,0 +1,36 @@
+package keeper
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/umee-network/umee/x/leverage/types"
+)
+
+var _ types.MsgServer = msgServer{}
+
+type msgServer struct {
+	Keeper
+}
+
+// NewMsgServerImpl returns an implementation of MsgServer for the x/leverage
+// module.
+func NewMsgServerImpl(keeper Keeper) types.MsgServer {
+	return &msgServer{Keeper: keeper}
+}
+
+func (s msgServer) LendAsset(goCtx context.Context, req *types.MsgLendAsset) (*types.MsgLendAssetResponse, error) {
+	// ctx := sdk.UnwrapSDKContext(goCtx)
+	// TODO: Implement...
+
+	return nil, status.Errorf(codes.Unimplemented, "method LendAsset not implemented")
+}
+
+func (s msgServer) WithdrawAsset(goCtx context.Context, req *types.MsgWithdrawAsset) (*types.MsgWithdrawAssetResponse, error) {
+	// ctx := sdk.UnwrapSDKContext(goCtx)
+	// TODO: Implement...
+
+	return nil, status.Errorf(codes.Unimplemented, "method WithdrawAsset not implemented")
+}
