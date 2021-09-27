fee8c4f739d4bb75ec8a2096e1d008ff623ac23a
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-09-27 10:26:53 -0400

    x/leverage: updates

--- x/leverage/types/codec.go
+++ x/leverage/types/codec.go
@@ -3,18 +3,40 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
-	// this line is used by starport scaffolding # 1
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
-func RegisterCodec(cdc *codec.LegacyAmino) {
-	// this line is used by starport scaffolding # 2
+var (
+	amino = codec.NewLegacyAmino()
+
+	// ModuleCdc references the global x/leverage module codec. Note, the codec
+	// should ONLY be used in certain instances of tests and for JSON encoding as
+	// Amino is still used for that purpose.
+	ModuleCdc = codec.NewAminoCodec(amino)
+)
+
+func init() {
+	RegisterLegacyAminoCodec(amino)
+	cryptocodec.RegisterCrypto(amino)
+	amino.Seal()
+}
+
+// RegisterLegacyAminoCodec registers the necessary x/leverage interfaces and
+// concrete types on the provided LegacyAmino codec. These types are used for
+// Amino JSON serialization.
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgLendAsset{}, "cosmos-sdk/MsgLendAsset", nil)
+	cdc.RegisterConcrete(&MsgWithdrawAsset{}, "cosmos-sdk/MsgWithdrawAsset", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	// this line is used by starport scaffolding # 3
-}
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgLendAsset{},
+		&MsgWithdrawAsset{},
+	)
 
-var (
-	amino     = codec.NewLegacyAmino()
-	ModuleCdc = codec.NewAminoCodec(amino)
-)
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+}
--- x/leverage/types/keys.go
+++ x/leverage/types/keys.go
@@ -33,38 +33,3 @@ func CreateUTokenDenomPrefix(uTokenDenom string) []byte {
 	key := append(KeyPrefixUTokenDenom, []byte(uTokenDenom)...)
 	return append(key, 0) // append 0 for null-termination
 }
-
-/*
-
-// Comment back in when no longer hard-coding associations between tokens
-
-// returns store key used to store a specific asset coin's associated utoken denom
-func assetAssociatedUtokenKey(coin sdk.Coin) []byte {
-	return prefixDenomStoreKey(AssetAssociatedUtokenPrefix, coin)
-	// intent: store[0x01+"uatom"] = "u/uatom"
-}
-
-// returns store key used to store a specific utoken coin's associated asset denom
-func utokenAssociatedAssetKey(coin sdk.Coin) []byte {
-	return prefixDenomStoreKey(UtokenAssociatedAssetPrefix, coin)
-	// intent: store[0x02+"u/uatom"] = "uatom"
-}
-
-// prefixDenomStoreKey turns a coin to the key used to store specific info by appending a prefix to the denom.
-// Returns empty bytes on invalid denom. Modeled after x/auth/types.AccountStoreKey
-func prefixDenomStoreKey(prefix byte, coin sdk.Coin) []byte {
-	if sdk.ValidateDenom(coin.Denom) != nil {
-		// Denom did not match ^[a-z][a-z0-9/]{2,63}$
-		return []byte{}
-	}
-	// example: byte(0x01) + []byte("uatom")
-	key := []byte{prefix}
-	key = append(key, []byte(coin.Denom)...)
-	return key
-	// Note: After IBC enable, want a reliable way to convert token denominations to bytes, such that
-	//	a) Each token type (e.g. Atom) has only one prefix/key, regardless of the path it took via IBC
-	//		to get to umee
-	//	b) Tokens cannot be spoofed (e.g. EvilChain cannot name a token 'atom', mint their own, and deposit)
-}
-
-*/
