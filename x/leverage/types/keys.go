d6c43aa2f08511aba5e3f6c4a855827caaa6e599
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-09-27 10:08:05 -0400

    x/leverage: updates

--- x/leverage/types/keys.go
+++ x/leverage/types/keys.go
@@ -12,13 +12,26 @@ const (
 
 	// QuerierRoute defines the module's query routing key
 	QuerierRoute = ModuleName
-
-	PrefixAssetAssociatedUtokenPrefix = 0x01 // Stores each asset denom's accepted uToken
-	// UtokenAssociatedAssetPrefix = 0x02 // Stores each uToken denom's accepted asset
 )
 
-func KeyPrefix(p string) []byte {
-	return []byte(p)
+// KVStore key prefixes
+var (
+	KeyPrefixTokenDenom  = []byte{0x01}
+	KeyPrefixUTokenDenom = []byte{0x02}
+)
+
+// CreateTokenDenomPrefix returns a KVStore prefix for getting and storing a
+// token's uToken denomination.
+func CreateTokenDenomPrefix(tokenDenom string) []byte {
+	key := append(KeyPrefixTokenDenom, []byte(tokenDenom)...)
+	return append(key, 0) // append 0 for null-termination
+}
+
+// CreateUTokenDenomPrefix returns a KVStore prefix for getting and storing a
+// uToken's token denomination.
+func CreateUTokenDenomPrefix(uTokenDenom string) []byte {
+	key := append(KeyPrefixUTokenDenom, []byte(uTokenDenom)...)
+	return append(key, 0) // append 0 for null-termination
 }
 
 /*
