fdc922a9290d02b68dd8bb89618c5680bcb78c7b
Adam Moser <63419657+toteki@users.noreply.github.com>
2021-10-12 22:52:01 -0600

    feat: borrow and repay assets (#134)
    * message proto, error types, keeper keys, keeper functions, grpc handler, and ADR correction
    
    * whitespace in ADR change
    
    * borrowing and repayment logic
    
    * Undo unintended go.sum diff from main
    
    * simplify key prefixes and iterators
    
    * borrow keeper tests - one currently failing
    
    * additional repay tests - still need to fix invalid prefixIterator
    
    * bug TODO + alternative line
    
    * borrow/repay fixed; tests working
    
    * SetCollateralSetting and GetCollateralSetting implementations and tests
    
    * events and logging
    
    * Simple suggestions batch
    
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
    
    * second batch of suggested changes
    
    * test cleanup suggestions
    
    * tCoin comment
    
    * remove tCoin helper
    
    * auxillary function for creating loan key without denom for prefixing
    
    * suggestion++
    
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>

--- x/leverage/keeper/msg_server.go
+++ x/leverage/keeper/msg_server.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -94,4 +95,120 @@ func (s msgServer) WithdrawAsset(
 	})
 
 	return &types.MsgWithdrawAssetResponse{}, nil
+}
+
+func (s msgServer) SetCollateral(
+	goCtx context.Context,
+	msg *types.MsgSetCollateral,
+) (*types.MsgSetCollateralResponse, error) {
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	borrowerAddr, err := sdk.AccAddressFromBech32(msg.Borrower)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.keeper.SetCollateralSetting(ctx, borrowerAddr, msg.Denom, msg.Enable); err != nil {
+		return nil, err
+	}
+
+	s.keeper.Logger(ctx).Debug(
+		"collateral setting set",
+		"borrower", borrowerAddr.String(),
+		"denom", msg.Denom,
+		"enable", strconv.FormatBool(msg.Enable),
+	)
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			types.EventTypeSetCollateralSetting,
+			sdk.NewAttribute(types.EventAttrBorrower, borrowerAddr.String()),
+			sdk.NewAttribute(types.EventAttrDenom, msg.Denom),
+			sdk.NewAttribute(types.EventAttrEnable, strconv.FormatBool(msg.Enable)),
+		),
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.EventAttrModule),
+			sdk.NewAttribute(sdk.AttributeKeySender, borrowerAddr.String()),
+		),
+	})
+
+	return &types.MsgSetCollateralResponse{}, nil
+}
+
+func (s msgServer) BorrowAsset(
+	goCtx context.Context,
+	msg *types.MsgBorrowAsset,
+) (*types.MsgBorrowAssetResponse, error) {
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	borrowerAddr, err := sdk.AccAddressFromBech32(msg.Borrower)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.keeper.BorrowAsset(ctx, borrowerAddr, msg.Amount); err != nil {
+		return nil, err
+	}
+
+	s.keeper.Logger(ctx).Debug(
+		"assets borrowed",
+		"borrower", borrowerAddr.String(),
+		"amount", msg.Amount.String(),
+	)
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			types.EventTypeBorrowAsset,
+			sdk.NewAttribute(types.EventAttrBorrower, borrowerAddr.String()),
+			sdk.NewAttribute(sdk.AttributeKeyAmount, msg.Amount.String()),
+		),
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.EventAttrModule),
+			sdk.NewAttribute(sdk.AttributeKeySender, borrowerAddr.String()),
+		),
+	})
+
+	return &types.MsgBorrowAssetResponse{}, nil
+}
+
+func (s msgServer) RepayAsset(
+	goCtx context.Context,
+	msg *types.MsgRepayAsset,
+) (*types.MsgRepayAssetResponse, error) {
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	borrowerAddr, err := sdk.AccAddressFromBech32(msg.Borrower)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.keeper.RepayAsset(ctx, borrowerAddr, msg.Amount); err != nil {
+		return nil, err
+	}
+
+	s.keeper.Logger(ctx).Debug(
+		"borrowed assets repaid",
+		"borrower", borrowerAddr.String(),
+		"amount", msg.Amount.String(),
+	)
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			types.EventTypeRepayBorrowedAsset,
+			sdk.NewAttribute(types.EventAttrBorrower, borrowerAddr.String()),
+			sdk.NewAttribute(sdk.AttributeKeyAmount, msg.Amount.String()),
+		),
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.EventAttrModule),
+			sdk.NewAttribute(sdk.AttributeKeySender, borrowerAddr.String()),
+		),
+	})
+
+	return &types.MsgRepayAssetResponse{}, nil
 }
--- x/leverage/types/tx.go
+++ x/leverage/types/tx.go
@@ -48,3 +48,61 @@ func (msg *MsgWithdrawAsset) GetSigners() []sdk.AccAddress {
 	lender, _ := sdk.AccAddressFromBech32(msg.GetLender())
 	return []sdk.AccAddress{lender}
 }
+
+func (msg *MsgSetCollateral) ValidateBasic() error {
+	borrower, err := sdk.AccAddressFromBech32(msg.GetBorrower())
+	if err != nil {
+		return err
+	}
+	if borrower.Empty() {
+		return errors.New("empty address")
+	}
+	return nil
+}
+
+func (msg *MsgSetCollateral) GetSigners() []sdk.AccAddress {
+	borrower, _ := sdk.AccAddressFromBech32(msg.GetBorrower())
+	return []sdk.AccAddress{borrower}
+}
+
+func (msg *MsgBorrowAsset) ValidateBasic() error {
+	borrower, err := sdk.AccAddressFromBech32(msg.GetBorrower())
+	if err != nil {
+		return err
+	}
+	if borrower.Empty() {
+		return errors.New("empty address")
+	}
+
+	if asset := msg.GetAmount(); !asset.IsValid() {
+		return sdkerrors.Wrap(ErrInvalidAsset, asset.String())
+	}
+
+	return nil
+}
+
+func (msg *MsgBorrowAsset) GetSigners() []sdk.AccAddress {
+	borrower, _ := sdk.AccAddressFromBech32(msg.GetBorrower())
+	return []sdk.AccAddress{borrower}
+}
+
+func (msg *MsgRepayAsset) ValidateBasic() error {
+	borrower, err := sdk.AccAddressFromBech32(msg.GetBorrower())
+	if err != nil {
+		return err
+	}
+	if borrower.Empty() {
+		return errors.New("empty address")
+	}
+
+	if asset := msg.GetAmount(); !asset.IsValid() {
+		return sdkerrors.Wrap(ErrInvalidAsset, asset.String())
+	}
+
+	return nil
+}
+
+func (msg *MsgRepayAsset) GetSigners() []sdk.AccAddress {
+	borrower, _ := sdk.AccAddressFromBech32(msg.GetBorrower())
+	return []sdk.AccAddress{borrower}
+}
