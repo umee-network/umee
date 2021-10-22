10aa1530c871bb756cbb8dc7fb3178a9f9b3c3eb
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-10-22 10:46:55 -0400

    updates

--- price-feeder/oracle/oracle.go
+++ price-feeder/oracle/oracle.go
@@ -2,15 +2,23 @@ package oracle
 
 import (
 	"context"
+	"sync"
+	"time"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+
 	pfsync "github.com/umee-network/umee/price-feeder/pkg/sync"
 )
 
 type Oracle struct {
 	logger zerolog.Logger
 	closer *pfsync.Closer
+
+	mtx             sync.RWMutex
+	lastPriceSyncTS time.Time
+	prices          map[string]sdk.Dec
 }
 
 func New() *Oracle {
@@ -36,4 +44,27 @@ func (o *Oracle) Start(ctx context.Context) {
 			// TODO: ...
 		}
 	}
+}
+
+// GetLastPriceSyncTimestamp returns the latest timestamp at which prices where
+// fetched from the oracle's set of exchange rate providers.
+func (o *Oracle) GetLastPriceSyncTimestamp() time.Time {
+	o.mtx.RLock()
+	defer o.mtx.RUnlock()
+
+	return o.lastPriceSyncTS
+}
+
+// GetPrices returns a copy of the current prices fetched from the oracle's
+// set of exchange rate providers.
+func (o *Oracle) GetPrices() map[string]sdk.Dec {
+	o.mtx.RLock()
+	defer o.mtx.RUnlock()
+
+	prices := make(map[string]sdk.Dec, len(o.prices))
+	for k, v := range o.prices {
+		prices[k] = v
+	}
+
+	return prices
 }
--- price-feeder/router/v1/router.go
+++ price-feeder/router/v1/router.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
@@ -18,12 +19,32 @@ const (
 func RegisterRoutes(v1SubRtr *mux.Router, oracle *oracle.Oracle, mChain alice.Chain) {
 	v1SubRtr.Handle(
 		"/healthz",
-		mChain.Then(healthzHandler()),
+		mChain.Then(healthzHandler(oracle)),
+	).Methods(httputil.MethodGET)
+
+	v1SubRtr.Handle(
+		"/prices",
+		mChain.Then(pricesHandler(oracle)),
 	).Methods(httputil.MethodGET)
 }
 
-func healthzHandler() http.HandlerFunc {
+func healthzHandler(oracle *oracle.Oracle) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		httputil.RespondWithJSON(w, http.StatusOK, HealthZResponse{})
+		resp := HealthZResponse{
+			Status: StatusAvailable,
+		}
+		resp.Oracle.LastSync = oracle.GetLastPriceSyncTimestamp().Format(time.RFC3339)
+
+		httputil.RespondWithJSON(w, http.StatusOK, resp)
+	}
+}
+
+func pricesHandler(oracle *oracle.Oracle) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		resp := PricesResponse{
+			Prices: oracle.GetPrices(),
+		}
+
+		httputil.RespondWithJSON(w, http.StatusOK, resp)
 	}
 }
