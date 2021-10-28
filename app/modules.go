7e863af369d45240f0f978bec5c96d3c9ae2000b
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2021-10-28 17:25:17 -0400

    chore: update SDK default genesis values (#184)

--- app/modules.go
+++ app/modules.go
@@ -2,11 +2,18 @@ package app
 
 import (
 	"encoding/json"
+	"time"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
+	"github.com/cosmos/cosmos-sdk/x/gov"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/cosmos/cosmos-sdk/x/mint"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	"github.com/cosmos/cosmos-sdk/x/slashing"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -36,4 +43,42 @@ func (crisisModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
 	return cdc.MustMarshalJSON(&crisistypes.GenesisState{
 		ConstantFee: sdk.NewCoin(BondDenom, sdk.NewInt(1000)),
 	})
+}
+
+type mintModule struct {
+	mint.AppModuleBasic
+}
+
+// DefaultGenesis returns custom Umee x/mint module genesis state.
+func (mintModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	genState := minttypes.DefaultGenesisState()
+	genState.Params.MintDenom = BondDenom
+
+	return cdc.MustMarshalJSON(genState)
+}
+
+type govModule struct {
+	gov.AppModuleBasic
+}
+
+// DefaultGenesis returns custom Umee x/gov module genesis state.
+func (govModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	minDeposit := sdk.NewCoins(sdk.NewCoin(BondDenom, govtypes.DefaultMinDepositTokens))
+	genState := govtypes.DefaultGenesisState()
+	genState.DepositParams.MinDeposit = minDeposit
+
+	return cdc.MustMarshalJSON(genState)
+}
+
+type slashingModule struct {
+	slashing.AppModuleBasic
+}
+
+// DefaultGenesis returns custom Umee x/slashing module genesis state.
+func (slashingModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	genState := slashingtypes.DefaultGenesisState()
+	genState.Params.SignedBlocksWindow = 10000
+	genState.Params.DowntimeJailDuration = 24 * time.Hour
+
+	return cdc.MustMarshalJSON(genState)
 }
