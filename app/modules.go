0cc0d56b19d472b35fbf1965387bbbdd7c4fbba0
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2021-10-29 09:30:29 -0400

    chore: update default genesis state (#187)
    ## Description
    
    Add default Umee bank metadata to x/bank
    
    ----
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [ ] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] targeted the correct branch (see [PR Targeting](https://github.com/umee-network/umee/blob/main/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)
    
    (cherry picked from commit 7ae8245e0a40728f6e90967e8e1b92065368506f)

--- app/modules.go
+++ app/modules.go
@@ -5,11 +5,47 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
+
+type bankModule struct {
+	bank.AppModuleBasic
+}
+
+// DefaultGenesis returns custom Umee x/bank module genesis state.
+func (bankModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	umeeMetadata := banktypes.Metadata{
+		Description: "The native staking token of the Umee network.",
+		Base:        BondDenom,
+		Name:        BondDenom,
+		Display:     "umee",
+		Symbol:      "UMEE",
+		DenomUnits: []*banktypes.DenomUnit{
+			{
+				Denom:    BondDenom,
+				Exponent: 0,
+				Aliases: []string{
+					"microumee",
+				},
+			},
+			{
+				Denom:    "umee",
+				Exponent: 6,
+				Aliases:  []string{},
+			},
+		},
+	}
+
+	genState := banktypes.DefaultGenesisState()
+	genState.DenomMetadata = append(genState.DenomMetadata, umeeMetadata)
+
+	return cdc.MustMarshalJSON(genState)
+}
 
 // stakingModule wraps the x/staking module in order to overwrite specific
 // ModuleManager APIs.
