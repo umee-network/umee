d817eae133f79174353279af9d04429247378e1e
Justyn Temme <justyntemme@gmail.com>
2021-11-02 14:13:30 -0500

    Add keeper functions for oracle

--- x/oracle/types/asset.go
+++ x/oracle/types/asset.go
@@ -1,9 +1,14 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // Denomination constants
 const (
 	MicroAtomDenom  = "uatom"
 	MicroUmeeDenom  = "uumee"
+	USDDenom        = "USD"
 	BlocksPerMinute = uint64(10)
 	BlocksPerHour   = BlocksPerMinute * 60
 	BlocksPerDay    = BlocksPerHour * 24
@@ -11,4 +16,27 @@ const (
 	BlocksPerMonth  = BlocksPerDay * 30
 	BlocksPerYear   = BlocksPerDay * 365
 	MicroUnit       = int64(1e6)
+)
+
+type (
+	// GenesisState - all oracle state that must be provided at genesis
+
+	// ExchangeRatePrevote - struct to store a validator's prevote on the rate of Luna in the denom asset
+	ExchangeRatePrevote struct {
+		Hash        VoteHash       `json:"hash"`  // Vote hex hash to protect centralize data source problem
+		Denom       string         `json:"denom"` // Ticker name of target fiat currency
+		Voter       sdk.ValAddress `json:"voter"` // Voter val address
+		SubmitBlock int64          `json:"submit_block"`
+	}
+
+	// ExchangeRateVote - struct to store a validator's vote on the rate of Luna in the denom asset
+	ExchangeRateVote struct {
+		ExchangeRate sdk.Dec        `json:"exchange_rate"` // ExchangeRate of Luna in target fiat currency
+		Denom        string         `json:"denom"`         // Ticker name of target fiat currency
+		Voter        sdk.ValAddress `json:"voter"`         // voter val address of validator
+	}
+
+	// VoteHash is hash value to hide vote exchange rate
+	// which is formatted as hex string in SHA256("{salt}:{exchange_rate}:{denom}:{voter}")
+	VoteHash []byte
 )
