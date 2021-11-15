f5b32604c04d1b447ff80d3f6b1febf9fa0d1a8b
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-11-15 13:31:09 -0500

    updates

--- x/oracle/ballot.go
+++ x/oracle/ballot.go
@@ -1,9 +1,7 @@
 package keeper
 
 import (
-	"fmt"
 	"sort"
-	"strings"
 
 	"github.com/umee-network/umee/x/oracle/types"
 
@@ -11,32 +9,34 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-// OrganizeBallotByDenom collects all oracle votes for the period, categorized by the votes' denom parameter
+// OrganizeBallotByDenom collects all oracle votes for the current vote period,
+// categorized by the votes' denom parameter.
 func (k Keeper) OrganizeBallotByDenom(
 	ctx sdk.Context,
-	validatorClaimMap map[string]types.Claim) (
-	votes map[string]types.ExchangeRateBallot) {
+	validatorClaimMap map[string]types.Claim,
+) (votes map[string]types.ExchangeRateBallot) {
+
 	votes = map[string]types.ExchangeRateBallot{}
 
-	// Organize aggregate votes
-	aggregateHandler := func(voterAddr sdk.ValAddress, vote types.AggregateExchangeRateVote) (stop bool) {
+	// collect aggregate votes
+	aggregateHandler := func(voterAddr sdk.ValAddress, vote types.AggregateExchangeRateVote) bool {
 		// organize ballot only for the active validators
 		claim, ok := validatorClaimMap[vote.Voter]
-
 		if ok {
 			power := claim.Power
+
 			for _, tuple := range vote.ExchangeRateTuples {
 				tmpPower := power
 				if !tuple.ExchangeRate.IsPositive() {
-					// Make the power of abstain vote zero
+					// make the power of abstain vote zero
 					tmpPower = 0
 				}
 
-				votes[tuple.Denom] = append(votes[tuple.Denom],
+				votes[tuple.Denom] = append(
+					votes[tuple.Denom],
 					types.NewVoteForTally(tuple.ExchangeRate, tuple.Denom, voterAddr, tmpPower),
 				)
 			}
-
 		}
 
 		return false
@@ -53,29 +53,37 @@ func (k Keeper) OrganizeBallotByDenom(
 	return votes
 }
 
-// ClearBallots clears all tallied prevotes and votes from the store
+// ClearBallots clears all tallied prevotes and votes from the store.
 func (k Keeper) ClearBallots(ctx sdk.Context, votePeriod uint64) {
-	// Clear all aggregate prevotes
-	k.IterateAggregateExchangeRatePrevotes(ctx, func(
-		voterAddr sdk.ValAddress,
-		aggregatePrevote types.AggregateExchangeRatePrevote) (stop bool) {
-		if ctx.BlockHeight() > int64(aggregatePrevote.SubmitBlock+votePeriod) {
-			k.DeleteAggregateExchangeRatePrevote(ctx, voterAddr)
-		}
+	// clear all aggregate prevotes
+	k.IterateAggregateExchangeRatePrevotes(
+		ctx,
+		func(voterAddr sdk.ValAddress, aggregatePrevote types.AggregateExchangeRatePrevote) bool {
+			if ctx.BlockHeight() > int64(aggregatePrevote.SubmitBlock+votePeriod) {
+				k.DeleteAggregateExchangeRatePrevote(ctx, voterAddr)
+			}
 
-		return false
-	})
+			return false
+		},
+	)
 
-	// Clear all aggregate votes
-	k.IterateAggregateExchangeRateVotes(ctx, func(voterAddr sdk.ValAddress,
-		aggregateVote types.AggregateExchangeRateVote) (stop bool) {
-		k.DeleteAggregateExchangeRateVote(ctx, voterAddr)
-		return false
-	})
+	// clear all aggregate votes
+	k.IterateAggregateExchangeRateVotes(
+		ctx,
+		func(voterAddr sdk.ValAddress, aggregateVote types.AggregateExchangeRateVote) bool {
+			k.DeleteAggregateExchangeRateVote(ctx, voterAddr)
+			return false
+		},
+	)
 }
 
-// ApplyWhitelist update vote target denom list and set tobin tax with params whitelist
-func (k Keeper) ApplyWhitelist(ctx sdk.Context, whitelist types.DenomList, voteTargets map[string]sdk.Dec) {
+// ApplyWhitelist updates vote targets denom list and set tobin tax with params
+// whitelist.
+func (k Keeper) ApplyWhitelist(
+	ctx sdk.Context,
+	whitelist types.DenomList,
+	voteTargets map[string]sdk.Dec,
+) {
 
 	// check is there any update in whitelist params
 	updateRequired := false
@@ -83,7 +91,8 @@ func (k Keeper) ApplyWhitelist(ctx sdk.Context, whitelist types.DenomList, voteT
 		updateRequired = true
 	} else {
 		for _, item := range whitelist {
-			if tobinTax, ok := voteTargets[item.Name]; !ok || !tobinTax.Equal(item.TobinTax) {
+			tobinTax, ok := voteTargets[item.Name]
+			if !ok || !tobinTax.Equal(item.TobinTax) {
 				updateRequired = true
 				break
 			}
@@ -96,21 +105,31 @@ func (k Keeper) ApplyWhitelist(ctx sdk.Context, whitelist types.DenomList, voteT
 		for _, item := range whitelist {
 			k.SetTobinTax(ctx, item.Name, item.TobinTax)
 
-			// Register meta data to bank module
+			// register metadata to bank module
 			if _, ok := k.bankKeeper.GetDenomMetaData(ctx, item.Name); !ok {
 				base := item.Name
 				display := base[1:]
 
-				k.bankKeeper.SetDenomMetaData(ctx, banktypes.Metadata{
-					Description: "The national currency of the United States.",
-					DenomUnits: []*banktypes.DenomUnit{
-						{Denom: display, Exponent: uint32(6), Aliases: []string{}},
+				k.bankKeeper.SetDenomMetaData(
+					ctx,
+					banktypes.Metadata{
+						Description: "The national currency of the United States",
+						DenomUnits: []*banktypes.DenomUnit{
+							{Denom: base, Exponent: uint32(0), Aliases: []string{}},
+							{Denom: display, Exponent: uint32(6), Aliases: []string{}},
+						},
+						Base:    base,
+						Display: display,
+						Name:    display,
+						Symbol:  display,
+						// TODO: The Gravity bridge requires that name, symbol and display
+						// are all equal. However, it is not currently clear if these assets
+						// will be bridged across to Ethereum. If not, we can uncomment below.
+						//
+						// Name:    fmt.Sprintf("%s United States Dollar", strings.ToUpper(display)),
+						// Symbol:  fmt.Sprintf("%sUSD", strings.ToUpper(display[:len(display)-1])),
 					},
-					Base:    base,
-					Display: display,
-					Name:    fmt.Sprintf("%s United States Dollar", strings.ToUpper(display)),
-					Symbol:  fmt.Sprintf("%sUSD", strings.ToUpper(display[:len(display)-1])),
-				})
+				)
 			}
 		}
 	}
