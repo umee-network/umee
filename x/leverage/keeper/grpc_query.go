be6654a771844f09305de2769ead2c85ce4573c1
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2021-11-19 15:15:15 -0500

    feat: gRPC handlers for x/leverage (#236)

--- x/leverage/keeper/grpc_query.go
+++ x/leverage/keeper/grpc_query.go
@@ -1,7 +1,49 @@
 package keeper
 
 import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	"github.com/umee-network/umee/x/leverage/types"
 )
 
-var _ types.QueryServer = Keeper{}
+var _ types.QueryServer = Querier{}
+
+// Querier implements a QueryServer for the x/leverage module.
+type Querier struct {
+	Keeper
+}
+
+func NewQuerier(k Keeper) Querier {
+	return Querier{Keeper: k}
+}
+
+func (q Querier) RegisteredTokens(
+	goCtx context.Context,
+	req *types.QueryRegisteredTokens,
+) (*types.QueryRegisteredTokensResponse, error) {
+
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	tokens, err := q.Keeper.GetAllRegisteredTokens(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &types.QueryRegisteredTokensResponse{
+		Registry: make([]types.Token, len(tokens)),
+	}
+
+	for i, t := range tokens {
+		resp.Registry[i] = t
+	}
+
+	return resp, nil
+}
