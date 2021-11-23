0e5fde5bdbbd1a4e9dcca67583e3d2814abaca76
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-11-23 10:49:54 -0500

    It's secret!

--- x/leverage/keeper/grpc_query_test.go
+++ x/leverage/keeper/grpc_query_test.go
@@ -60,7 +60,7 @@ func (s *IntegrationTestSuite) TestQuerier_ReserveAmount() {
 	// TODO: We need to setup borrowing first prior to testing this out.
 	//
 	// Ref: https://github.com/umee-network/umee/issues/93
-	req := &types.QueryReserveAmountRequest{}
+	req := &types.QueryReserveAmountRequest{Denom: "uumee"}
 	_, err := s.queryClient.ReserveAmount(context.Background(), req)
 	s.Require().NoError(err)
 }
