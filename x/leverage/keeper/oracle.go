0b51e634bc2f5b47649b642a71b9671838fc5fb1
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-12-09 14:45:52 -0500

    Version control is awful

--- x/leverage/keeper/oracle.go
+++ x/leverage/keeper/oracle.go
@@ -7,12 +7,12 @@ import (
 	"github.com/umee-network/umee/x/leverage/types"
 )
 
-// Price returns the USD value of a base token. Note, the token's denomination
+// TokenPrice returns the USD value of a base token. Note, the token's denomination
 // must be the base denomination, e.g. uumee. The x/oracle module must know of
 // the base and display/symbol denominations for each exchange pair. E.g. it must
 // know about the UMEE/USD exchange rate along with the uumee base denomination
 // and the exponent.
-func (k Keeper) Price(ctx sdk.Context, denom string) (sdk.Dec, error) {
+func (k Keeper) TokenPrice(ctx sdk.Context, denom string) (sdk.Dec, error) {
 	if !k.IsAcceptedToken(ctx, denom) {
 		return sdk.ZeroDec(), sdkerrors.Wrap(types.ErrInvalidAsset, denom)
 	}
@@ -20,27 +20,39 @@ func (k Keeper) Price(ctx sdk.Context, denom string) (sdk.Dec, error) {
 	return k.oracleKeeper.GetExchangeRateBase(ctx, denom)
 }
 
-// TotalPrice returns the total USD value of a set of Coins.
-func (k Keeper) TotalPrice(ctx sdk.Context, denoms []string) (sdk.Dec, error) {
-	price := sdk.ZeroDec()
-
-	for _, denom := range denoms {
-		p, err := k.Price(ctx, denom)
-		if err != nil {
-			return sdk.ZeroDec(), err
-		}
-
-		price = price.Add(p)
+// GetTokenValue returns the total token value given a Coin. An error is
+// returned if we cannot get the token's price or if it's not an accepted token.
+func (k Keeper) GetTokenValue(ctx sdk.Context, coin sdk.Coin) (sdk.Dec, error) {
+	p, err := k.TokenPrice(ctx, coin.Denom)
+	if err != nil {
+		return sdk.ZeroDec(), err
 	}
 
-	return price, nil
+	return p.Mul(coin.Amount.ToDec()), nil
+}
+
+// TotalTokenValue returns the total value of all supplied tokens. It is
+// equivalent to calling GetTokenValue on each coin individually.
+func (k Keeper) TotalTokenValue(ctx sdk.Context, coins sdk.Coins) (sdk.Dec, error) {
+	total := sdk.ZeroDec()
+
+	for _, c := range coins {
+		v, err := k.GetTokenValue(ctx, c)
+		if err != nil {
+			return sdk.ZeroDec(), nil
+		}
+
+		total = total.Add(v)
+	}
+
+	return total, nil
 }
 
 // EquivalentValue returns the amount of a selected denom which would have equal
 // USD value to a provided sdk.Coin
-func (k Keeper) EquivalentValue(ctx sdk.Context, fromCoin sdk.Coin, toDenom string) (sdk.Coin, error) {
+func (k Keeper) EquivalentTokenValue(ctx sdk.Context, fromCoin sdk.Coin, toDenom string) (sdk.Coin, error) {
 	// get total USD price of input (from) denomination
-	price, err := k.Price(ctx, fromCoin.Denom)
+	price, err := k.TokenPrice(ctx, fromCoin.Denom)
 	if err != nil {
 		return sdk.Coin{}, err
 	}
@@ -51,7 +63,7 @@ func (k Keeper) EquivalentValue(ctx sdk.Context, fromCoin sdk.Coin, toDenom stri
 	}
 
 	// first derive USD value of new denom if amount was unchanged
-	exchange, err := k.Price(ctx, toDenom)
+	exchange, err := k.TokenPrice(ctx, toDenom)
 	if err != nil {
 		return sdk.Coin{}, err
 	}
