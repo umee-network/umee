3489558fe1ab2a507f8e9ec9e9aeccc011bba3e8
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2021-12-12 09:32:12 -0500

    chore: x/oracle housekeeping (#284)

--- x/oracle/types/test_utils.go
+++ x/oracle/types/test_utils.go
@@ -9,7 +9,6 @@ import (
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-
 	"github.com/tendermint/tendermint/crypto/secp256k1"
 	tmprotocrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
 )
