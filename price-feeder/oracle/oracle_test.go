ed6177927015dde6c0a9410a5933f085abe68235
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-12-13 17:13:45 -0500

    This is where it all begins...

--- price-feeder/oracle/oracle_test.go
+++ price-feeder/oracle/oracle_test.go
@@ -4,11 +4,22 @@ import (
 	"testing"
 	"time"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/suite"
+
 	"github.com/umee-network/umee/price-feeder/config"
 	"github.com/umee-network/umee/price-feeder/oracle/client"
+	"github.com/umee-network/umee/price-feeder/oracle/provider"
 )
+
+type mockProvider struct {
+	prices map[string]provider.TickerPrice
+}
+
+func (m mockProvider) GetTickerPrices(ticker ...string) (map[string]provider.TickerPrice, error) {
+	return m.prices, nil
+}
 
 type OracleTestSuite struct {
 	suite.Suite
@@ -24,8 +35,13 @@ func (ots *OracleTestSuite) SetupSuite() {
 		[]config.CurrencyPair{
 			{
 				Base:      "UMEE",
-				Quote:     "USD",
-				Providers: []string{config.ProviderBinance, config.ProviderKraken},
+				Quote:     "USDT",
+				Providers: []string{config.ProviderBinance},
+			},
+			{
+				Base:      "UMEE",
+				Quote:     "USDC",
+				Providers: []string{config.ProviderKraken},
 			},
 		},
 	)
@@ -54,4 +70,29 @@ func (ots *OracleTestSuite) TestGetLastPriceSyncTimestamp() {
 func (ots *OracleTestSuite) TestPrices() {
 	// initial prices should be empty (not set)
 	ots.Require().Empty(ots.oracle.GetPrices())
+
+	// use a mock provider to provide prices for the configured exchange pairs
+	ots.oracle.priceProviders = map[string]provider.Provider{
+		config.ProviderBinance: mockProvider{
+			prices: map[string]provider.TickerPrice{
+				"UMEEUSDT": {
+					Price:  sdk.MustNewDecFromStr("3.72"),
+					Volume: sdk.MustNewDecFromStr("2396974.02000000"),
+				},
+			},
+		},
+		config.ProviderKraken: mockProvider{
+			prices: map[string]provider.TickerPrice{
+				"UMEEUSDC": {
+					Price:  sdk.MustNewDecFromStr("3.70"),
+					Volume: sdk.MustNewDecFromStr("1994674.34000000"),
+				},
+			},
+		},
+	}
+
+	ots.Require().NoError(ots.oracle.SetPrices())
+
+	prices := ots.oracle.GetPrices()
+	ots.Require().Len(prices, 1)
 }
