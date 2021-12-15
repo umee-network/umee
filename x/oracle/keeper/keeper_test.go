9d64fbf849608d8c92e09d4c29edf26dec95e9bc
Adam Wozniak <adam.e.wozniak@gmail.com>
2021-12-15 11:29:18 -0800

    0x0 -> 0

--- x/oracle/keeper/keeper_test.go
+++ x/oracle/keeper/keeper_test.go
@@ -131,12 +131,12 @@ func (s *IntegrationTestSuite) Test_MissCounter() {
 	app, ctx := s.app, s.ctx
 	missCounter := uint64(rand.Intn(100))
 
-	s.Require().Equal(app.OracleKeeper.GetMissCounter(ctx, valAddr), uint64(0x0))
+	s.Require().Equal(app.OracleKeeper.GetMissCounter(ctx, valAddr), uint64(0))
 	app.OracleKeeper.SetMissCounter(ctx, valAddr, missCounter)
 	s.Require().Equal(app.OracleKeeper.GetMissCounter(ctx, valAddr), missCounter)
 
 	app.OracleKeeper.DeleteMissCounter(ctx, valAddr)
-	s.Require().Equal(app.OracleKeeper.GetMissCounter(ctx, valAddr), uint64(0x0))
+	s.Require().Equal(app.OracleKeeper.GetMissCounter(ctx, valAddr), uint64(0))
 }
 
 func (s *IntegrationTestSuite) Test_AggregateExchangeRatePrevote() {
