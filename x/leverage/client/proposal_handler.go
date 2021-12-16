de1aaeffe65ec90b9d0d9519071b6b89e5dc827c
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-12-16 15:38:03 -0500

    This is where it all begins...

--- x/leverage/client/proposal_handler.go
+++ x/leverage/client/proposal_handler.go
@@ -1,10 +1,27 @@
 package client
 
 import (
+	"net/http"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/types/rest"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
+	govrest "github.com/cosmos/cosmos-sdk/x/gov/client/rest"
 
 	"github.com/umee-network/umee/x/leverage/client/cli"
 )
 
 // ProposalHandler defines an x/gov proposal handler for the CLI only.
-var ProposalHandler = govclient.NewProposalHandler(cli.NewCmdSubmitUpdateRegistryProposal, nil)
+var ProposalHandler = govclient.NewProposalHandler(
+	cli.NewCmdSubmitUpdateRegistryProposal,
+	updateRegistryProposalNoOpHandler,
+)
+
+func updateRegistryProposalNoOpHandler(clientCtx client.Context) govrest.ProposalRESTHandler {
+	return govrest.ProposalRESTHandler{
+		SubRoute: "update_registry",
+		Handler: func(w http.ResponseWriter, r *http.Request) {
+			rest.WriteErrorResponse(w, http.StatusNotFound, "unsupported route")
+		},
+	}
+}
