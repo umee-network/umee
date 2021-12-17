830f5e94595ea4330226cc0d5af168801a2828ed
Adam Wozniak <adam.e.wozniak@gmail.com>
2021-12-17 08:29:06 -0800

    I can't remember

--- x/oracle/keeper/msg_server_test.go
+++ x/oracle/keeper/msg_server_test.go
@@ -32,14 +32,38 @@ func (s *IntegrationTestSuite) TestMsgServer_AggregateExchangeRatePrevote() {
 	exchangeRatesStr := "123.2:UMEE"
 	salt, err := GenerateSalt(2)
 	s.Require().NoError(err)
-
 	hash := oracletypes.GetAggregateVoteHash(salt, exchangeRatesStr, valAddr)
-	msg := &types.MsgAggregateExchangeRatePrevote{
+
+	invalidHash := &types.MsgAggregateExchangeRatePrevote{
+		Hash:      "invalid_hash",
+		Feeder:    addr.String(),
+		Validator: valAddr.String(),
+	}
+	_, err = s.msgServer.AggregateExchangeRatePrevote(sdk.WrapSDKContext(ctx), invalidHash)
+	s.Require().Error(err)
+
+	invalidFeeder := &types.MsgAggregateExchangeRatePrevote{
+		Hash:      hash.String(),
+		Feeder:    "invalid_feeder",
+		Validator: valAddr.String(),
+	}
+	_, err = s.msgServer.AggregateExchangeRatePrevote(sdk.WrapSDKContext(ctx), invalidFeeder)
+	s.Require().Error(err)
+
+	invalidValidator := &types.MsgAggregateExchangeRatePrevote{
+		Hash:      hash.String(),
+		Feeder:    addr.String(),
+		Validator: "invalid_val",
+	}
+	_, err = s.msgServer.AggregateExchangeRatePrevote(sdk.WrapSDKContext(ctx), invalidValidator)
+	s.Require().Error(err)
+
+	validMsg := &types.MsgAggregateExchangeRatePrevote{
 		Hash:      hash.String(),
 		Feeder:    addr.String(),
 		Validator: valAddr.String(),
 	}
-	_, err = s.msgServer.AggregateExchangeRatePrevote(sdk.WrapSDKContext(ctx), msg)
+	_, err = s.msgServer.AggregateExchangeRatePrevote(sdk.WrapSDKContext(ctx), validMsg)
 	s.Require().NoError(err)
 }
 
@@ -49,24 +73,28 @@ func (s *IntegrationTestSuite) TestMsgServer_AggregateExchangeRateVote() {
 	exchangeRatesStr := "123.2:UMEE"
 	salt, err := GenerateSalt(2)
 	s.Require().NoError(err)
-
 	hash := oracletypes.GetAggregateVoteHash(salt, exchangeRatesStr, valAddr)
+
 	prevoteMsg := &types.MsgAggregateExchangeRatePrevote{
 		Hash:      hash.String(),
 		Feeder:    addr.String(),
 		Validator: valAddr.String(),
 	}
-	_, err = s.msgServer.AggregateExchangeRatePrevote(sdk.WrapSDKContext(ctx), prevoteMsg)
-	s.Require().NoError(err)
-
 	voteMsg := &types.MsgAggregateExchangeRateVote{
 		Feeder:        addr.String(),
 		Validator:     valAddr.String(),
 		Salt:          salt,
 		ExchangeRates: exchangeRatesStr,
 	}
+
+	// No existing prevote
 	_, err = s.msgServer.AggregateExchangeRateVote(sdk.WrapSDKContext(ctx), voteMsg)
+	s.Require().Error(err)
+	_, err = s.msgServer.AggregateExchangeRatePrevote(sdk.WrapSDKContext(ctx), prevoteMsg)
 	s.Require().NoError(err)
+	// Reveal period mismatch
+	_, err = s.msgServer.AggregateExchangeRateVote(sdk.WrapSDKContext(ctx), voteMsg)
+	s.Require().Error(err)
 }
 
 func (s *IntegrationTestSuite) TestMsgServer_DelegateFeedConsent() {
