f209014e8cfc88ccce59959a495a93e14a36e279
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-12-18 16:42:08 -0500

    updates

--- price-feeder/oracle/client/client.go
+++ price-feeder/oracle/client/client.go
@@ -210,7 +210,7 @@ func (oc OracleClient) CreateClientContext() (client.Context, error) {
 		JSONCodec:         oc.Encoding.Marshaler,
 		InterfaceRegistry: oc.Encoding.InterfaceRegistry,
 		Output:            os.Stderr,
-		BroadcastMode:     flags.BroadcastSync,
+		BroadcastMode:     flags.BroadcastBlock,
 		TxConfig:          oc.Encoding.TxConfig,
 		AccountRetriever:  authtypes.AccountRetriever{},
 		Codec:             oc.Encoding.Marshaler,
