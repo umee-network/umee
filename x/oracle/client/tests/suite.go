bed0d6b5ad1627608b449d2a951688258746522f
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-12-21 10:10:12 -0500

    updates

---
+++ x/oracle/client/tests/suite.go
@@ -0,0 +1,26 @@
+package tests
+
+import (
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/stretchr/testify/suite"
+)
+
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	cfg     network.Config
+	network *network.Network
+}
+
+func NewIntegrationTestSuite(cfg network.Config) *IntegrationTestSuite {
+	return &IntegrationTestSuite{cfg: cfg}
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	s.T().Log("setting up integration test suite")
+
+	s.network = network.New(s.T(), s.cfg)
+
+	_, err := s.network.WaitForHeight(1)
+	s.Require().NoError(err)
+}
