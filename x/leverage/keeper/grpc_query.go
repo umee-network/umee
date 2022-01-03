ee3fb2ba02e6dbcd0f825800e2c2afc27c307580
Rafael Tenfen <rafaeltenfen.rt@gmail.com>
2022-01-03 09:45:52 -0300

    Update x/leverage/keeper/grpc_query.go
    added safeguard to queries
    
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>

--- x/leverage/keeper/grpc_query.go
+++ x/leverage/keeper/grpc_query.go
@@ -105,6 +105,10 @@ func (q Querier) BorrowAPY(
 		return nil, status.Error(codes.InvalidArgument, "invalid denom")
 	}
 
+	if !q.Keeper.IsAcceptedToken(ctx, req.Denom) {
+		return nil, status.Error(codes.InvalidArgument, "not accepted Token denom")
+	}
+
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	if !q.Keeper.IsAcceptedToken(ctx, req.Denom) {
 		return nil, status.Error(codes.InvalidArgument, "not accepted Token denom")
