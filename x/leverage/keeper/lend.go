57bf779a18d159d0fe8c9ddd16ac6a575bb9244a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-01-05 13:49:17 -0300

    feat: update the order to marshal first then load the store

--- x/leverage/keeper/lend.go
+++ x/leverage/keeper/lend.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/umee-network/umee/x/leverage/types"
 )
@@ -27,14 +28,17 @@ func (k Keeper) GetLendAPY(ctx sdk.Context, denom string) sdk.Dec {
 
 // SetLendAPY sets the lend APY of an specific denom
 func (k Keeper) SetLendAPY(ctx sdk.Context, denom string, lendAPY sdk.Dec) error {
-	store := ctx.KVStore(k.storeKey)
-	key := types.CreateLendAPYKey(denom)
+	if !k.IsAcceptedToken(ctx, denom) {
+		return sdkerrors.Wrap(types.ErrInvalidAsset, denom)
+	}
 
 	bz, err := lendAPY.Marshal()
 	if err != nil {
 		return err
 	}
 
+	store := ctx.KVStore(k.storeKey)
+	key := types.CreateLendAPYKey(denom)
 	store.Set(key, bz)
 	return nil
 }
