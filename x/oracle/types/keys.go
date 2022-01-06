e8ff83ec71d786fa0f779076bcd6d176adb5d890
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2022-01-06 10:05:20 -0800

    keyprefix*

--- x/oracle/types/keys.go
+++ x/oracle/types/keys.go
@@ -21,40 +21,40 @@ const (
 
 // KVStore key prefixes
 var (
-	ExchangeRateKey                 = []byte{0x01} // prefix for each key to a rate
-	FeederDelegationKey             = []byte{0x02} // prefix for each key to a feeder delegation
-	MissCounterKey                  = []byte{0x03} // prefix for each key to a miss counter
-	AggregateExchangeRatePrevoteKey = []byte{0x04} // prefix for each key to a aggregate prevote
-	AggregateExchangeRateVoteKey    = []byte{0x05} // prefix for each key to a aggregate vote
+	KeyPrefixExchangeRate                 = []byte{0x01} // prefix for each key to a rate
+	KeyPrefixFeederDelegation             = []byte{0x02} // prefix for each key to a feeder delegation
+	KeyPrefixMissCounter                  = []byte{0x03} // prefix for each key to a miss counter
+	KeyPrefixAggregateExchangeRatePrevote = []byte{0x04} // prefix for each key to a aggregate prevote
+	KeyPrefixAggregateExchangeRateVote    = []byte{0x05} // prefix for each key to a aggregate vote
 )
 
 // GetExchangeRateKey - stored by *denom*
 func GetExchangeRateKey(denom string) (key []byte) {
-	key = append(key, ExchangeRateKey...)
+	key = append(key, KeyPrefixExchangeRate...)
 	key = append(key, []byte(denom)...)
 	return append(key, 0) // append 0 for null-termination
 }
 
 // GetFeederDelegationKey - stored by *Validator* address
 func GetFeederDelegationKey(v sdk.ValAddress) (key []byte) {
-	key = append(key, FeederDelegationKey...)
+	key = append(key, KeyPrefixFeederDelegation...)
 	return append(key, address.MustLengthPrefix(v)...)
 }
 
 // GetMissCounterKey - stored by *Validator* address
 func GetMissCounterKey(v sdk.ValAddress) (key []byte) {
-	key = append(key, MissCounterKey...)
+	key = append(key, KeyPrefixMissCounter...)
 	return append(key, address.MustLengthPrefix(v)...)
 }
 
 // GetAggregateExchangeRatePrevoteKey - stored by *Validator* address
 func GetAggregateExchangeRatePrevoteKey(v sdk.ValAddress) (key []byte) {
-	key = append(key, AggregateExchangeRatePrevoteKey...)
+	key = append(key, KeyPrefixAggregateExchangeRatePrevote...)
 	return append(key, address.MustLengthPrefix(v)...)
 }
 
 // GetAggregateExchangeRateVoteKey - stored by *Validator* address
 func GetAggregateExchangeRateVoteKey(v sdk.ValAddress) (key []byte) {
-	key = append(key, AggregateExchangeRateVoteKey...)
+	key = append(key, KeyPrefixAggregateExchangeRateVote...)
 	return append(key, address.MustLengthPrefix(v)...)
 }
