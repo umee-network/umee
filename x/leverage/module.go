d07ff9bbf3f8b938ebd433088048a48df8e4283d
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-01-18 14:17:55 -0300

    feat: register leverage invariants in module

--- x/leverage/module.go
+++ x/leverage/module.go
@@ -138,7 +138,9 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 }
 
 // RegisterInvariants registers the x/leverage module's invariants.
-func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
+func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
+	keeper.RegisterInvariants(ir, am.keeper)
+}
 
 // InitGenesis performs the x/leverage module's genesis initialization. It returns
 // no validator updates.
