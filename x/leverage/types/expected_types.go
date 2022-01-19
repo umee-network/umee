7a2da001b7bea865d2697167b82fae5b116ecb5d
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-01-19 10:11:03 -0300

    feat: add SpendableCoins to BankKeeper leverage interface to use in simulations

--- x/leverage/types/expected_types.go
+++ x/leverage/types/expected_types.go
@@ -20,6 +20,9 @@ type BankKeeper interface {
 	) error
 	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	HasBalance(ctx sdk.Context, addr sdk.AccAddress, amt sdk.Coin) bool
+
+	// only used for simulation
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 }
 
