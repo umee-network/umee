089321704a148794e3e975a81200d5a522f5acdc
Rafael Tenfen <rafaeltenfen.rt@gmail.com>
2022-01-25 15:04:58 -0300

    fix types and random int generated
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>

--- x/leverage/types/expected_types.go
+++ x/leverage/types/expected_types.go
@@ -2,12 +2,12 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 // AccountKeeper defines the expected account keeper used for leverage simulations (noalias)
 type AccountKeeper interface {
-	GetAccount(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
+	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI
 }
 
 // BankKeeper defines the expected x/bank keeper interface.
