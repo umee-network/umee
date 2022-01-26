e6fab9df3b5f294e27097ea8d6d18679d407caca
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2022-01-26 12:49:07 -0800

    cleanup

--- x/oracle/keeper/msg_server_test.go
+++ x/oracle/keeper/msg_server_test.go
@@ -111,7 +111,10 @@ func (s *IntegrationTestSuite) TestMsgServer_AggregateExchangeRateVote() {
 	// Run ValidateBasics
 	s.Require().NoError(prevoteMsg.ValidateBasic())
 	s.Require().NoError(voteMsg.ValidateBasic())
-	s.Require().EqualError(voteMsgShortSalt.ValidateBasic(), sdkerrors.Wrap(types.ErrInvalidSaltLength, "salt length must be [60, 80]").Error())
+	s.Require().EqualError(
+		voteMsgShortSalt.ValidateBasic(),
+		sdkerrors.Wrap(types.ErrInvalidSaltLength, "salt length must be [60, 80]").Error(),
+	)
 	s.Require().NoError(voteMsgInvalidRate.ValidateBasic())
 
 	// Flattened acceptList symbols to make checks easier
