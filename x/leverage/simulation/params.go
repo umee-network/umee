9379f14b701c00b45bb17adf81668b7786ba796c
Rafael Tenfen <rafaeltenfen.rt@gmail.com>
2022-01-27 16:10:59 -0300

    feat: x/leverage simulations (#401)
    * feat: add reserve amount and exchange rates invariants to leverage module
    
    * feat: add borrow amount to leverage invariants
    
    * feat: add collateral amount check of all positive values in the leverage module
    
    * feat: add borrow APY to leverage invariants
    
    * chore: removed unused iterate function
    
    * chore: update to use the Iterate function
    
    * feat: add lend APY to leverage invariants
    
    * chore: modified variable name and func comment
    
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>
    
    * chore: update iterate to private function in keeper leverage
    
    * feat: add test to ExchangeRate invariant
    
    * feat: add test to ReserveAmount invariant
    
    * feat: add test to BorrowAmount invariant
    
    * feat: add test to CollateralAmount invariant
    
    * feat: add test to Borrow and Lend APY invariant
    
    * chore: Update comments
    
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>
    
    * chore: update test comments
    
    * chore: update to use the umeeapp to get the BondDenom
    
    * chore: update APY invariant to check only for Negative values
    
    * chore: update expected message
    
    * chore: add operations package
    
    * feat: add msg lend asset operation to leverage simulations
    
    * refactor: update comment ume to umee
    
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>
    
    * refactor: update marshal error message and keys to DRY
    
    * refactor: applied keys to DRY
    
    * feat: add SpendableCoins to BankKeeper leverage interface to use in simulations
    
    * feat: update operations to use GenAndDeliverTxWithRandFees
    
    * feat: add AccountKeeper to leverage keeper
    
    * refactor: add AccountKeeper to AppModule and removed from Keeper
    
    * refactor: moved simulation operation weight to AppModule structure
    
    * test: add failing test
    
    * refactor: applied import convencions
    
    * feat: add leverage decoder simulation
    
    * refactor: removed comment
    
    * feat: add msg withdraw asset operation
    
    * feat: add leverage keeper to Weighted Operations
    
    * feat: add borrow asset operations
    
    * feat: add msg set collateral setting
    
    * feat: add msg repay borrowed asset
    
    * feat: add msg liquidate to sim operations
    
    * chore: merge main
    
    * chore: merge main
    
    * feat: commented test of sim msg lend
    
    * feat add params and genesis to leverage simulation
    
    * fix types and random int generated
    
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>
    
    * fix: removed unused method
    
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>
    
    * feat: removed unused app
    
    * feat: update last interest time type
    
    * fix: lint
    
    Co-authored-by: Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>

---
+++ x/leverage/simulation/params.go
@@ -0,0 +1,38 @@
+package simulation
+
+import (
+	"fmt"
+	"math/rand"
+
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+
+	"github.com/umee-network/umee/x/leverage/types"
+)
+
+// ParamChanges defines the parameters that can be modified by param change proposals
+// on the simulation
+func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
+	return []simtypes.ParamChange{
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyInterestEpoch),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%d\"", GenInterestEpoch(r))
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyCompleteLiquidationThreshold),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenCompleteLiquidationThreshold(r))
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyMinimumCloseFactor),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenMinimumCloseFactor(r))
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyOracleRewardFactor),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenOracleRewardFactor(r))
+			},
+		),
+	}
+}
