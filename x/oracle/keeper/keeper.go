0cbfb92761501af68e63981aec99a47709554dc4
Rafael Tenfen <rafaeltenfen.rt@gmail.com>
2022-01-31 21:20:41 -0300

    refactor: removed duplicated error check (#456)

--- x/oracle/keeper/keeper.go
+++ x/oracle/keeper/keeper.go
@@ -190,7 +190,7 @@ func (k Keeper) SetFeederDelegation(ctx sdk.Context, operator sdk.ValAddress, de
 	store.Set(types.GetFeederDelegationKey(operator), delegatedFeeder.Bytes())
 }
 
-type IterateFeederDelegationHandler = func(delegator sdk.ValAddress, delegate sdk.AccAddress) (stop bool)
+type IterateFeederDelegationHandler func(delegator sdk.ValAddress, delegate sdk.AccAddress) (stop bool)
 
 // IterateFeederDelegations iterates over the feed delegates and performs a
 // callback function.
@@ -271,10 +271,7 @@ func (k Keeper) GetAggregateExchangeRatePrevote(
 
 	bz := store.Get(types.GetAggregateExchangeRatePrevoteKey(voter))
 	if bz == nil {
-		err := sdkerrors.Wrap(types.ErrNoAggregatePrevote, voter.String())
-		if err != nil {
-			return types.AggregateExchangeRatePrevote{}, err
-		}
+		return types.AggregateExchangeRatePrevote{}, sdkerrors.Wrap(types.ErrNoAggregatePrevote, voter.String())
 	}
 
 	var aggregatePrevote types.AggregateExchangeRatePrevote
@@ -332,10 +329,7 @@ func (k Keeper) GetAggregateExchangeRateVote(
 
 	bz := store.Get(types.GetAggregateExchangeRateVoteKey(voter))
 	if bz == nil {
-		err := sdkerrors.Wrap(types.ErrNoAggregateVote, voter.String())
-		if err != nil {
-			return types.AggregateExchangeRateVote{}, err
-		}
+		return types.AggregateExchangeRateVote{}, sdkerrors.Wrap(types.ErrNoAggregateVote, voter.String())
 	}
 
 	var aggregateVote types.AggregateExchangeRateVote
