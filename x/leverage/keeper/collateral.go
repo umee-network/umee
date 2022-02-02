383ec2f2e0fa394cbc81609017fec2482271fe03
toteki <63419657+toteki@users.noreply.github.com>
2022-02-02 14:35:20 -0700

    reduce delta

--- x/leverage/keeper/collateral.go
+++ x/leverage/keeper/collateral.go
@@ -48,3 +48,79 @@ func (k Keeper) SetCollateralAmount(ctx sdk.Context, borrowerAddr sdk.AccAddress
 	}
 	return nil
 }
+
+// GetBorrowerCollateral returns an sdk.Coins containing all of a borrower's collateral.
+func (k Keeper) GetBorrowerCollateral(ctx sdk.Context, borrowerAddr sdk.AccAddress) sdk.Coins {
+	prefix := types.CreateCollateralAmountKeyNoDenom(borrowerAddr)
+	totalCollateral := sdk.NewCoins()
+
+	iterator := func(key, val []byte) error {
+		// get borrow denom from key
+		denom := types.DenomFromKeyWithAddress(key, types.KeyPrefixCollateralAmount)
+
+		// get collateral amount
+		var amount sdk.Int
+		if err := amount.Unmarshal(val); err != nil {
+			// improperly marshaled amount should never happen
+			panic(err)
+		}
+
+		// add to totalBorrowed
+		totalCollateral = totalCollateral.Add(sdk.NewCoin(denom, amount))
+		return nil
+	}
+
+	_ = k.iterate(ctx, prefix, iterator)
+
+	return totalCollateral.Sort()
+}
+
+// GetEligibleLiquidationTargets returns a list of borrower addresses eligible for liquidation.
+func (k Keeper) GetEligibleLiquidationTargets(ctx sdk.Context) ([]sdk.AccAddress, error) {
+	prefix := types.KeyPrefixAdjustedBorrow
+	liquidationTargets := []sdk.AccAddress{}
+	checkedAddrs := map[string]struct{}{}
+
+	iterator := func(key, val []byte) error {
+		// get borrower address from key
+		addr := types.AddressFromKey(key, prefix)
+
+		// if the address is already checked, do not check again
+		if _, ok := checkedAddrs[addr.String()]; ok {
+			return nil
+		}
+		checkedAddrs[addr.String()] = struct{}{}
+
+		// get borrower's total borrowed
+		borrowed := k.GetBorrowerBorrows(ctx, addr)
+
+		// get borrower's total collateral
+		collateral := k.GetBorrowerCollateral(ctx, addr)
+
+		// use oracle helper functions to find total borrowed value in USD
+		borrowValue, err := k.TotalTokenValue(ctx, borrowed)
+		if err != nil {
+			return err
+		}
+
+		// use collateral weights to compute borrow limit from enabled collateral
+		borrowLimit, err := k.CalculateBorrowLimit(ctx, collateral)
+		if err != nil {
+			return err
+		}
+
+		// if the borrowLimit is smaller then the borrowValue
+		// the address is eligible to liquidation
+		if borrowLimit.LT(borrowValue) {
+			liquidationTargets = append(liquidationTargets, addr)
+		}
+
+		return nil
+	}
+
+	if err := k.iterate(ctx, prefix, iterator); err != nil {
+		return nil, err
+	}
+
+	return liquidationTargets, nil
+}
