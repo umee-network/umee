241f27c7a59a857b44b067ff64e10be484f508ab
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2022-02-14 08:05:19 -0800

    test++

--- price-feeder/config/config_test.go
+++ price-feeder/config/config_test.go
@@ -31,6 +31,10 @@ func TestValidate(t *testing.T) {
 					Validator: "valaddr",
 					ChainID:   "chain-id",
 				},
+				Keyring: config.Keyring{
+					Backend: "test",
+					Dir:     "/Users/username/.umee",
+				},
 				RPC: config.RPC{
 					TMRPCEndpoint: "http://localhost:26657",
 					GRPCEndpoint:  "localhost:9090",
