e0afdaec9f65963b620eb8587193b78514d0ecb6
Adam Moser <63419657+toteki@users.noreply.github.com>
2022-02-22 13:20:00 -0700

    lint++

--- x/leverage/types/errors.go
+++ x/leverage/types/errors.go
@@ -25,5 +25,5 @@ var (
 	ErrInconsistentTotalBorrow = sdkerrors.Register(ModuleName, 1115, "total adjusted borrow inconsistency")
 	ErrInvalidInteresrScalar   = sdkerrors.Register(ModuleName, 1116, "interest scalar less than one")
 	ErrEmptyAddress            = sdkerrors.Register(ModuleName, 1117, "empty address")
-	ErrLiquidationRewardRatio  = sdkerrors.Register(ModuleName, 1118, "requested minimum liquidation reward exceeds derived amount")
+	ErrLiquidationRewardRatio  = sdkerrors.Register(ModuleName, 1118, "requested liquidation reward not met")
 )
