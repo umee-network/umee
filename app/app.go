985c126c4a85490c882a868ad11347d3825db7a9
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-02-25 12:24:17 -0500

    updates

--- app/app.go
+++ app/app.go
@@ -160,8 +160,6 @@ var (
 )
 
 func init() {
-	fmt.Println("DEBUG DO NOT MERGE")
-
 	userHomeDir, err := os.UserHomeDir()
 	if err != nil {
 		panic(fmt.Sprintf("failed to get user home directory: %s", err))
