35d5349ca7e6476ec32c250a9183b8c2e80b1207
Robert Zaremba <robert@zaremba.ch>
2022-05-27 14:51:46 +0200

    chore: clean protocgen.sh (#969)
    Co-authored-by: toteki <63419657+toteki@users.noreply.github.com>

--- contrib/scripts/protocgen.sh
+++ contrib/scripts/protocgen.sh
@@ -2,15 +2,6 @@
 
 set -eo pipefail
 
-protoc_gen_go() {
-  if ! grep "github.com/gogo/protobuf => github.com/umee-network/umee" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the umee-core folder."
-    return 1
-  fi
-
-  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
-}
-
 cd proto
 proto_dirs=$(find ./umee -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
