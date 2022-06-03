0022ae4ec513550fcb07727b18560fa176feed10
toteki <63419657+toteki@users.noreply.github.com>
2022-06-03 10:18:38 -0700

    ++

---
+++ x/incentive/client/cli/query.go
@@ -0,0 +1,64 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+
+	"github.com/umee-network/umee/v2/x/incentive/types"
+)
+
+// Flag constants
+const (
+	FlagDenom = "denom"
+)
+
+// GetQueryCmd returns the CLI query commands for the x/incentive module.
+func GetQueryCmd(queryRoute string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		GetCmdQueryParams(),
+	)
+
+	return cmd
+}
+
+// GetCmdQueryParams returns a CLI command handler to query for the x/leverage
+// module parameters.
+func GetCmdQueryParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the x/incentive module parameters",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			resp, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(resp)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// TODO: Add all queries
--- x/incentive/types/query.pb.go
+++ x/incentive/types/query.pb.go
@@ -29,6 +29,90 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
+// QueryParamsRequest defines the request structure for the Params gRPC service
+// handler.
+type QueryParamsRequest struct {
+}
+
+func (m *QueryParamsRequest) Reset()         { *m = QueryParamsRequest{} }
+func (m *QueryParamsRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryParamsRequest) ProtoMessage()    {}
+func (*QueryParamsRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_98af6650734ce845, []int{0}
+}
+func (m *QueryParamsRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryParamsRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryParamsRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryParamsRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryParamsRequest.Merge(m, src)
+}
+func (m *QueryParamsRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryParamsRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryParamsRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryParamsRequest proto.InternalMessageInfo
+
+// QueryParamsResponse defines the response structure for the Params gRPC
+// service handler.
+type QueryParamsResponse struct {
+	Params Params `protobuf:"bytes,1,opt,name=params,proto3" json:"params"`
+}
+
+func (m *QueryParamsResponse) Reset()         { *m = QueryParamsResponse{} }
+func (m *QueryParamsResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryParamsResponse) ProtoMessage()    {}
+func (*QueryParamsResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_98af6650734ce845, []int{1}
+}
+func (m *QueryParamsResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryParamsResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryParamsResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryParamsResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryParamsResponse.Merge(m, src)
+}
+func (m *QueryParamsResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryParamsResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryParamsResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryParamsResponse proto.InternalMessageInfo
+
+func (m *QueryParamsResponse) GetParams() Params {
+	if m != nil {
+		return m.Params
+	}
+	return Params{}
+}
+
 // QueryIncentiveProgramsRequest defines the request structure for the IncentivePrograms
 // gRPC service handler.
 type QueryIncentiveProgramsRequest struct {
@@ -38,7 +122,7 @@ func (m *QueryIncentiveProgramsRequest) Reset()         { *m = QueryIncentivePro
 func (m *QueryIncentiveProgramsRequest) String() string { return proto.CompactTextString(m) }
 func (*QueryIncentiveProgramsRequest) ProtoMessage()    {}
 func (*QueryIncentiveProgramsRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_98af6650734ce845, []int{0}
+	return fileDescriptor_98af6650734ce845, []int{2}
 }
 func (m *QueryIncentiveProgramsRequest) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -77,7 +161,7 @@ func (m *QueryIncentiveProgramsResponse) Reset()         { *m = QueryIncentivePr
 func (m *QueryIncentiveProgramsResponse) String() string { return proto.CompactTextString(m) }
 func (*QueryIncentiveProgramsResponse) ProtoMessage()    {}
 func (*QueryIncentiveProgramsResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_98af6650734ce845, []int{1}
+	return fileDescriptor_98af6650734ce845, []int{3}
 }
 func (m *QueryIncentiveProgramsResponse) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -114,6 +198,8 @@ func (m *QueryIncentiveProgramsResponse) GetPrograms() []IncentiveProgram {
 }
 
 func init() {
+	proto.RegisterType((*QueryParamsRequest)(nil), "umeenetwork.umee.incentive.v1.QueryParamsRequest")
+	proto.RegisterType((*QueryParamsResponse)(nil), "umeenetwork.umee.incentive.v1.QueryParamsResponse")
 	proto.RegisterType((*QueryIncentiveProgramsRequest)(nil), "umeenetwork.umee.incentive.v1.QueryIncentiveProgramsRequest")
 	proto.RegisterType((*QueryIncentiveProgramsResponse)(nil), "umeenetwork.umee.incentive.v1.QueryIncentiveProgramsResponse")
 }
@@ -121,26 +207,31 @@ func init() {
 func init() { proto.RegisterFile("umee/incentive/v1/query.proto", fileDescriptor_98af6650734ce845) }
 
 var fileDescriptor_98af6650734ce845 = []byte{
-	// 297 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2d, 0xcd, 0x4d, 0x4d,
-	0xd5, 0xcf, 0xcc, 0x4b, 0x4e, 0xcd, 0x2b, 0xc9, 0x2c, 0x4b, 0xd5, 0x2f, 0x33, 0xd4, 0x2f, 0x2c,
-	0x4d, 0x2d, 0xaa, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x02, 0x4b, 0xe7, 0xa5, 0x96, 0x94,
-	0xe7, 0x17, 0x65, 0xeb, 0x81, 0xd8, 0x7a, 0x70, 0xa5, 0x7a, 0x65, 0x86, 0x52, 0x32, 0xe9, 0xf9,
-	0xf9, 0xe9, 0x39, 0xa9, 0xfa, 0x89, 0x05, 0x99, 0xfa, 0x89, 0x79, 0x79, 0xf9, 0x25, 0x89, 0x25,
-	0x99, 0xf9, 0x79, 0xc5, 0x10, 0xcd, 0x52, 0x8a, 0x98, 0x66, 0x23, 0x74, 0x43, 0x94, 0x88, 0xa4,
-	0xe7, 0xa7, 0xe7, 0x83, 0x99, 0xfa, 0x20, 0x16, 0x44, 0x54, 0x49, 0x9e, 0x4b, 0x36, 0x10, 0xe4,
-	0x08, 0x4f, 0x98, 0xea, 0x80, 0xa2, 0xfc, 0xf4, 0xa2, 0xc4, 0xdc, 0xe2, 0xa0, 0xd4, 0xc2, 0xd2,
-	0xd4, 0xe2, 0x12, 0xa5, 0x62, 0x2e, 0x39, 0x5c, 0x0a, 0x8a, 0x0b, 0xf2, 0xf3, 0x8a, 0x53, 0x85,
-	0x02, 0xb9, 0x38, 0x0a, 0xa0, 0x62, 0x12, 0x8c, 0x0a, 0xcc, 0x1a, 0xdc, 0x46, 0xfa, 0x7a, 0x78,
-	0xfd, 0xa2, 0x87, 0x6e, 0x96, 0x13, 0xcb, 0x89, 0x7b, 0xf2, 0x0c, 0x41, 0x70, 0x63, 0x8c, 0x4e,
-	0x32, 0x72, 0xb1, 0x82, 0x6d, 0x15, 0xda, 0xcf, 0xc8, 0x25, 0x88, 0x61, 0xb5, 0x90, 0x0d, 0x01,
-	0x0b, 0xf0, 0x7a, 0x49, 0xca, 0x96, 0x4c, 0xdd, 0x10, 0xff, 0x2a, 0xe9, 0x36, 0x5d, 0x7e, 0x32,
-	0x99, 0x49, 0x5d, 0x48, 0x55, 0x1f, 0x4f, 0xa0, 0xc7, 0xc3, 0xfc, 0xe2, 0xe4, 0x7d, 0xe2, 0x91,
-	0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x4e, 0x78, 0x2c, 0xc7, 0x70, 0xe1,
-	0xb1, 0x1c, 0xc3, 0x8d, 0xc7, 0x72, 0x0c, 0x51, 0x86, 0xe9, 0x99, 0x25, 0x19, 0xa5, 0x49, 0x7a,
-	0xc9, 0xf9, 0xb9, 0x60, 0xa3, 0x74, 0xa1, 0x4e, 0x82, 0x98, 0x5b, 0x66, 0xa4, 0x5f, 0x81, 0x64,
-	0x78, 0x49, 0x65, 0x41, 0x6a, 0x71, 0x12, 0x1b, 0x38, 0xd6, 0x8c, 0x01, 0x01, 0x00, 0x00, 0xff,
-	0xff, 0x65, 0xb8, 0x88, 0x0f, 0x4c, 0x02, 0x00, 0x00,
+	// 370 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x92, 0xc1, 0x4e, 0xea, 0x40,
+	0x14, 0x86, 0x3b, 0xdc, 0x7b, 0xc9, 0xcd, 0xb0, 0x72, 0x64, 0x81, 0x55, 0x0a, 0x34, 0x21, 0xb2,
+	0xa1, 0x93, 0xd6, 0xad, 0x6e, 0x70, 0x65, 0xdc, 0x00, 0x4b, 0x36, 0xa6, 0x90, 0x49, 0x6d, 0x94,
+	0x99, 0xd2, 0x99, 0x56, 0xd9, 0xfa, 0x04, 0x46, 0x1f, 0xc8, 0x2d, 0x4b, 0x12, 0x37, 0xae, 0x88,
+	0x01, 0x1f, 0xc4, 0x30, 0x53, 0x10, 0x6d, 0x00, 0x75, 0x77, 0x72, 0x7a, 0xfe, 0xff, 0x7c, 0xfd,
+	0xe7, 0xc0, 0x62, 0xd4, 0x27, 0x04, 0xfb, 0xb4, 0x47, 0xa8, 0xf0, 0x63, 0x82, 0x63, 0x1b, 0x0f,
+	0x22, 0x12, 0x0e, 0xad, 0x20, 0x64, 0x82, 0x21, 0xf9, 0x99, 0x12, 0x71, 0xc3, 0xc2, 0x2b, 0x6b,
+	0x5e, 0x5b, 0xcb, 0x51, 0x2b, 0xb6, 0xf5, 0x03, 0x8f, 0x31, 0xef, 0x9a, 0x60, 0x37, 0xf0, 0xb1,
+	0x4b, 0x29, 0x13, 0xae, 0xf0, 0x19, 0xe5, 0x4a, 0xac, 0x57, 0xd2, 0xde, 0x1f, 0x6a, 0x35, 0x92,
+	0xf7, 0x98, 0xc7, 0x64, 0x89, 0xe7, 0x95, 0xea, 0x9a, 0x79, 0x88, 0x5a, 0x73, 0x88, 0xa6, 0x1b,
+	0xba, 0x7d, 0xde, 0x26, 0x83, 0x88, 0x70, 0x61, 0x76, 0xe0, 0xee, 0xa7, 0x2e, 0x0f, 0x18, 0xe5,
+	0x04, 0x9d, 0xc2, 0x6c, 0x20, 0x3b, 0x05, 0x50, 0x06, 0xb5, 0x9c, 0x53, 0xb5, 0x36, 0x32, 0x5b,
+	0x4a, 0xde, 0xf8, 0x3b, 0x9a, 0x94, 0xb4, 0x76, 0x22, 0x35, 0x4b, 0xb0, 0x28, 0xbd, 0xcf, 0x16,
+	0x93, 0xcd, 0x90, 0x79, 0xab, 0xcb, 0x39, 0x34, 0xd6, 0x0d, 0x24, 0x1c, 0x2d, 0xf8, 0x3f, 0x48,
+	0x7a, 0x05, 0x50, 0xfe, 0x53, 0xcb, 0x39, 0x78, 0x0b, 0xc9, 0x57, 0xaf, 0x84, 0x69, 0x69, 0xe3,
+	0x4c, 0x32, 0xf0, 0x9f, 0xdc, 0x8a, 0x1e, 0x00, 0xcc, 0x2a, 0x70, 0x64, 0x6f, 0x71, 0x4d, 0x27,
+	0xa7, 0x3b, 0x3f, 0x91, 0xa8, 0xdf, 0x31, 0x2b, 0x77, 0xcf, 0x6f, 0x8f, 0x99, 0x7d, 0xb4, 0x87,
+	0xd3, 0xaf, 0xa8, 0x42, 0x43, 0x4f, 0x00, 0xee, 0xa4, 0xf2, 0x40, 0xc7, 0xdf, 0x59, 0xb6, 0x2e,
+	0x67, 0xfd, 0xe4, 0x97, 0xea, 0x84, 0xba, 0x2e, 0xa9, 0x0f, 0x51, 0x15, 0x6f, 0xb8, 0xbd, 0x8b,
+	0x45, 0xc0, 0x8d, 0xf3, 0xd1, 0xd4, 0x00, 0xe3, 0xa9, 0x01, 0x5e, 0xa7, 0x06, 0xb8, 0x9f, 0x19,
+	0xda, 0x78, 0x66, 0x68, 0x2f, 0x33, 0x43, 0xeb, 0xd8, 0x9e, 0x2f, 0x2e, 0xa3, 0xae, 0xd5, 0x63,
+	0x7d, 0x69, 0x55, 0x4f, 0x90, 0x94, 0x6f, 0xec, 0xe0, 0xdb, 0x15, 0x73, 0x31, 0x0c, 0x08, 0xef,
+	0x66, 0xe5, 0xf1, 0x1e, 0xbd, 0x07, 0x00, 0x00, 0xff, 0xff, 0x86, 0xaf, 0x75, 0x04, 0x53, 0x03,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -155,6 +246,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type QueryClient interface {
+	// Params queries the parameters of the x/incentive module.
+	Params(ctx context.Context, in *QueryParamsRequest, opts ...grpc.CallOption) (*QueryParamsResponse, error)
 	// IncentivePrograms queries for all incentives programs that have ever existed.
 	IncentivePrograms(ctx context.Context, in *QueryIncentiveProgramsRequest, opts ...grpc.CallOption) (*QueryIncentiveProgramsResponse, error)
 }
@@ -165,6 +258,15 @@ type queryClient struct {
 
 func NewQueryClient(cc grpc1.ClientConn) QueryClient {
 	return &queryClient{cc}
+}
+
+func (c *queryClient) Params(ctx context.Context, in *QueryParamsRequest, opts ...grpc.CallOption) (*QueryParamsResponse, error) {
+	out := new(QueryParamsResponse)
+	err := c.cc.Invoke(ctx, "/umeenetwork.umee.incentive.v1.Query/Params", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *queryClient) IncentivePrograms(ctx context.Context, in *QueryIncentiveProgramsRequest, opts ...grpc.CallOption) (*QueryIncentiveProgramsResponse, error) {
@@ -178,6 +280,8 @@ func (c *queryClient) IncentivePrograms(ctx context.Context, in *QueryIncentiveP
 
 // QueryServer is the server API for Query service.
 type QueryServer interface {
+	// Params queries the parameters of the x/incentive module.
+	Params(context.Context, *QueryParamsRequest) (*QueryParamsResponse, error)
 	// IncentivePrograms queries for all incentives programs that have ever existed.
 	IncentivePrograms(context.Context, *QueryIncentiveProgramsRequest) (*QueryIncentiveProgramsResponse, error)
 }
@@ -186,12 +290,33 @@ type QueryServer interface {
 type UnimplementedQueryServer struct {
 }
 
+func (*UnimplementedQueryServer) Params(ctx context.Context, req *QueryParamsRequest) (*QueryParamsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Params not implemented")
+}
 func (*UnimplementedQueryServer) IncentivePrograms(ctx context.Context, req *QueryIncentiveProgramsRequest) (*QueryIncentiveProgramsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method IncentivePrograms not implemented")
 }
 
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
 	s.RegisterService(&_Query_serviceDesc, srv)
+}
+
+func _Query_Params_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryParamsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).Params(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/umeenetwork.umee.incentive.v1.Query/Params",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).Params(ctx, req.(*QueryParamsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _Query_IncentivePrograms_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -217,12 +342,72 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*QueryServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
+			MethodName: "Params",
+			Handler:    _Query_Params_Handler,
+		},
+		{
 			MethodName: "IncentivePrograms",
 			Handler:    _Query_IncentivePrograms_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "umee/incentive/v1/query.proto",
+}
+
+func (m *QueryParamsRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryParamsRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryParamsRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryParamsResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryParamsResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryParamsResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	{
+		size, err := m.Params.MarshalToSizedBuffer(dAtA[:i])
+		if err != nil {
+			return 0, err
+		}
+		i -= size
+		i = encodeVarintQuery(dAtA, i, uint64(size))
+	}
+	i--
+	dAtA[i] = 0xa
+	return len(dAtA) - i, nil
 }
 
 func (m *QueryIncentiveProgramsRequest) Marshal() (dAtA []byte, err error) {
@@ -296,6 +481,26 @@ func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
+func (m *QueryParamsRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *QueryParamsResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = m.Params.Size()
+	n += 1 + l + sovQuery(uint64(l))
+	return n
+}
+
 func (m *QueryIncentiveProgramsRequest) Size() (n int) {
 	if m == nil {
 		return 0
@@ -325,6 +530,139 @@ func sovQuery(x uint64) (n int) {
 }
 func sozQuery(x uint64) (n int) {
 	return sovQuery(uint64((x << 1) ^ uint64((int64(x) >> 63))))
+}
+func (m *QueryParamsRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryParamsRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryParamsRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryParamsResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryParamsResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryParamsResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Params", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := m.Params.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
 }
 func (m *QueryIncentiveProgramsRequest) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
