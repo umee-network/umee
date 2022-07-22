c476f45e510d24b56d25c70c550b322de0a5b0c2
Robert Zaremba <robert@zaremba.ch>
2022-07-22 11:05:08 +0200

    changelog

--- util/cli/print.go
+++ util/cli/print.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
+// PrintOrErr formats and print proto message to the standard output, unless the error
+// is not nil.
 func PrintOrErr(resp proto.Message, err error, cctx client.Context) error {
 	if err != nil {
 		return err
