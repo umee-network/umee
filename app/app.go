c09c9f133542d7203d59a549079ff2b3e641b8e6
Rafael Tenfen <rafaeltenfen.rt@gmail.com>
2022-08-04 10:34:14 -0300

    feat: enable swagger (#1203)
    ## Description
    
    closes: #957 
    
    ---
    
    ### Author Checklist
    
    _All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues._
    
    I have...
    
    - [ ] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] added appropriate labels to the PR
    - [ ] targeted the correct branch (see [PR Targeting](https://github.com/umee-network/umee/blob/main/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    _All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items._
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- app/app.go
+++ app/app.go
@@ -101,6 +101,7 @@ import (
 	appparams "github.com/umee-network/umee/v2/app/params"
 	"github.com/umee-network/umee/v2/app/upgrades"
 	"github.com/umee-network/umee/v2/app/upgrades/calypso"
+	"github.com/umee-network/umee/v2/swagger"
 	uibctransfer "github.com/umee-network/umee/v2/x/ibctransfer"
 	uibctransferkeeper "github.com/umee-network/umee/v2/x/ibctransfer/keeper"
 	"github.com/umee-network/umee/v2/x/leverage"
@@ -844,6 +845,10 @@ func (app *UmeeApp) RegisterAPIRoutes(apiSvr *api.Server, apiConfig config.APICo
 	// register legacy and grpc-gateway routes for all modules
 	ModuleBasics.RegisterRESTRoutes(clientCtx, apiSvr.Router)
 	ModuleBasics.RegisterGRPCGatewayRoutes(clientCtx, apiSvr.GRPCGatewayRouter)
+
+	if apiConfig.Swagger {
+		swagger.RegisterSwaggerAPI(apiSvr.Router)
+	}
 }
 
 // RegisterTxService implements the Application.RegisterTxService method.
