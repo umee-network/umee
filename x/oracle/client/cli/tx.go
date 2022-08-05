d412c7a3faac094fb9a019a58492dad6aee13a0f
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2022-08-04 22:47:05 -0700

    add cli commands for oracle prevoting / voting

--- x/oracle/client/cli/tx.go
+++ x/oracle/client/cli/tx.go
@@ -61,3 +61,78 @@ func GetCmdDelegateFeedConsent() *cobra.Command {
 
 	return cmd
 }
+
+// GetCmdAggregateExchangeRatePrevote creates a Cobra command to generate or
+// broadcast a transaction with a MsgAggregateExchangeRatePrevote message.
+func GetCmdAggregateExchangeRatePrevote() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "exchange-rate-prevote [hash] [feeder]",
+		Args:  cobra.ExactArgs(2),
+		Short: "Submit an exchange rate prevote msg.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := cmd.Flags().Set(flags.FlagFrom, args[0]); err != nil {
+				return err
+			}
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			feederAddr, err := sdk.AccAddressFromBech32(args[1])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgAggregateExchangeRatePrevote(
+				[]byte(args[0]),
+				feederAddr,
+				sdk.ValAddress(clientCtx.GetFromAddress()),
+			)
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+// GetCmdAggregateExchangeRateVote creates a Cobra command to generate or
+// broadcast a transaction with a NewMsgAggregateExchangeRateVote message.
+func GetCmdAggregateExchangeRateVote() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "exchange-rate-prevote [salt] [exchangeRates] [feeder]",
+		Args:  cobra.ExactArgs(2),
+		Short: "Submit an exchange rate vote msg.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := cmd.Flags().Set(flags.FlagFrom, args[0]); err != nil {
+				return err
+			}
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			feederAddr, err := sdk.AccAddressFromBech32(args[2])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgAggregateExchangeRateVote(
+				args[0],
+				args[1],
+				feederAddr,
+				sdk.ValAddress(clientCtx.GetFromAddress()),
+			)
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
