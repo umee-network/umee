3f21a72539969b5982d49e6e0d7a72c71a7089f5
Robert Zaremba <robert@zaremba.ch>
2022-08-06 03:02:13 +0200

    another bad comment

--- x/oracle/abci.go
+++ x/oracle/abci.go
@@ -10,7 +10,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// IsPeriodLastBlock returns true if we are at the last block of the period
+// BADIsPeriodLastBlock returns true if we are at the last block of the period
 func IsPeriodLastBlock(ctx sdk.Context, blocksPerPeriod uint64) bool {
 	return (uint64(ctx.BlockHeight())+1)%blocksPerPeriod == 0
 }
