95918de1fac6466d317aeeaec784b699efb9dee3
Robert Zaremba <robert@zaremba.ch>
2022-08-19 12:29:13 +0200

    improving ante handler fee tests

--- ante/fee_test.go
+++ ante/fee_test.go
@@ -11,57 +11,64 @@ import (
 )
 
 func (suite *IntegrationTestSuite) TestFeeAndPriority() {
+	require := suite.Require()
 	suite.txBuilder = suite.clientCtx.TxConfig.NewTxBuilder()
 	priv1, _, addr1 := testdata.KeyTestPubAddr()
-	require := suite.Require()
+	privs, accNums, accSeqs := []cryptotypes.PrivKey{priv1}, []uint64{0}, []uint64{0}
 
 	msgs := testdata.NewTestMsg(addr1)
 	require.NoError(suite.txBuilder.SetMsgs(msgs))
-	fee := testdata.NewTestFeeAmount()
+	fee := sdk.NewCoins(sdk.NewInt64Coin("atom", 150))
+	gasLimit := 200000
 	suite.txBuilder.SetFeeAmount(fee)
-	suite.txBuilder.SetGasLimit(testdata.NewTestGasLimit())
-
-	privs, accNums, accSeqs := []cryptotypes.PrivKey{priv1}, []uint64{0}, []uint64{0}
-	tx, err := suite.CreateTestTx(privs, accNums, accSeqs, suite.ctx.ChainID())
-	require.NoError(err)
+	suite.txBuilder.SetGasLimit(uint64(gasLimit))
 
 	// Test1: validator min gas price check
-	// set min gas price above the transaction gas limit, so the tx should fail
-	minGasPrice := sdk.NewDecCoinFromDec("atom", sdk.NewDec(200).Quo(sdk.NewDec(100000)))
+	// Ante should fail when validator min gas price is above the transaction gas limit
+	minGasPrice := sdk.NewDecCoinFromDec("atom", sdk.NewDecFromInt(fee[0].Amount).QuoInt64(int64(gasLimit/2)))
 	ctx := suite.ctx.
 		WithMinGasPrices([]sdk.DecCoin{minGasPrice}).
 		WithIsCheckTx(true)
+	tx, err := suite.CreateTestTx(privs, accNums, accSeqs, suite.ctx.ChainID())
+	require.NoError(err)
 	_, _, err = ante.FeeAndPriority(ctx, tx)
 	require.ErrorIs(sdkerrors.ErrInsufficientFee, err)
 
 	// Test2: min gas price not checked in DeliverTx
 	ctx = suite.ctx.WithIsCheckTx(false)
-	requiredFee, _, err := ante.FeeAndPriority(ctx, tx)
-	require.NoError(err)
-	require.True(fee.IsEqual(requiredFee))
+	suite.checkFeeAnte(tx, fee, ctx)
 
 	// Test3: should not error when min gas price is same or lower than the fee
 	ctx = ctx.WithMinGasPrices(sdk.NewDecCoinsFromCoins(fee...))
-	requiredFee, _, err = ante.FeeAndPriority(ctx, tx)
-	require.NoError(err)
-	require.True(fee.IsEqual(requiredFee))
+	suite.checkFeeAnte(tx, fee, ctx)
 
 	ctx = ctx.WithMinGasPrices([]sdk.DecCoin{sdk.NewDecCoin(fee[0].Denom, fee[0].Amount.QuoRaw(2))})
-	requiredFee, _, err = ante.FeeAndPriority(ctx, tx)
-	require.NoError(err)
-	require.True(fee.IsEqual(requiredFee))
+	suite.checkFeeAnte(tx, fee, ctx)
 
 	// Test4: ensure no fees for oracle msgs
 	require.NoError(suite.txBuilder.SetMsgs(
 		oracletypes.NewMsgAggregateExchangeRatePrevote(oracletypes.AggregateVoteHash{}, addr1, sdk.ValAddress(addr1)),
 		oracletypes.NewMsgAggregateExchangeRateVote("", "", addr1, sdk.ValAddress(addr1)),
 	))
+	suite.txBuilder.SetFeeAmount(sdk.Coins{})
+	suite.txBuilder.SetGasLimit(0)
 	oracleTx, err := suite.CreateTestTx(privs, accNums, accSeqs, suite.ctx.ChainID())
 	require.NoError(err)
+	require.True(oracleTx.GetFee().IsZero(), "got: %s", oracleTx.GetFee())
+	require.Equal(uint64(0), oracleTx.GetGas())
 	require.True(ante.IsOracleTx(oracleTx.GetMsgs()))
 
-	ctx = suite.ctx.WithIsCheckTx(true)
-	requiredFee, _, err = ante.FeeAndPriority(ctx, oracleTx)
-	require.NoError(err, "Decorator should not require fees for oracle tx")
-	require.True(requiredFee.IsZero(), "fee should be zero, got: %s", requiredFee)
+	suite.checkFeeAnte(oracleTx, sdk.Coins{}, suite.ctx.WithIsCheckTx(true))
+	suite.checkFeeAnte(oracleTx, sdk.Coins{}, suite.ctx.WithIsCheckTx(false))
+}
+
+func (suite *IntegrationTestSuite) checkFeeAnte(tx sdk.Tx, feeExpected sdk.Coins, ctx sdk.Context) {
+	require := suite.Require()
+	fee, _, err := ante.FeeAndPriority(ctx, tx)
+	require.NoError(err)
+	if len(feeExpected) == 0 {
+		require.True(fee.IsZero(), "fee should be zero, got: %s", fee)
+	} else {
+		require.True(fee.IsEqual(feeExpected), "Fee expected %s, got: %s", feeExpected, fee)
+	}
 }
