2403b2ec1dd92ebae6213db9f9d85d71ab304a25
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-09-05 16:32:03 -0300

    fix unzip

--- contrib/scripts/umeemainnet_fork.sh
+++ contrib/scripts/umeemainnet_fork.sh
@@ -9,34 +9,24 @@
 
 CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 UMEED_BIN_MAINNET_URL_TARBALL=${UMEED_BIN_MAINNET_URL_TARBALL:-"https://github.com/umee-network/umee/releases/download/v1.0.3/umeed-v1.0.3-linux-amd64.tar.gz"}
+UMEED_BIN_MAINNET=${UMEED_BIN_MAINNET:-"$CWD/umeed-v103"}
 
-if [ -z $UMEED_BIN_MAINNET_URL_TARBALL ]; then
-  echo You need to set the UMEED_BIN_MAINNET_URL_TARBALL variable
-  exit 1
+# Checks for the umeed v1 file
+if [ ! -f "$UMEED_BIN_MAINNET" ]; then
+  echo "$UMEED_BIN_MAINNET doesn't exist"
+
+  if [ -z $UMEED_BIN_MAINNET_URL_TARBALL ]; then
+    echo You need to set the UMEED_BIN_MAINNET_URL_TARBALL variable
+    exit 1
+  fi
+
+  UMEED_RELEASES_PATH=$CWD/umeed-releases
+  mkdir -p $UMEED_RELEASES_PATH
+  wget -c $UMEED_BIN_MAINNET_URL_TARBALL -O - | tar -xz -C $UMEED_RELEASES_PATH
+
+  UMEED_BIN_MAINNET_BASENAME=$(basename $UMEED_BIN_MAINNET_URL_TARBALL .tar.gz)
+  UMEED_BIN_MAINNET=$UMEED_RELEASES_PATH/$UMEED_BIN_MAINNET_BASENAME/umeed
 fi
-
-extracted_dir=$(basename $UMEED_BIN_MAINNET_URL_TARBALL .tar.gz)
-echo extracted_dir: $extracted_dir
-echo ls CWD
-ls $CWD
-# ls $CWD/$extracted_dir
-# ls $extracted_dir
-UMEED_BIN_MAINNET="${UMEED_BIN_MAINNET:-$CWD/$extracted_dir/umeed}"
-echo CWD $CWD
-echo UMEED_BIN_MAINNET $UMEED_BIN_MAINNET
-# ls $UMEED_BIN_MAINNET
-
-if [ ! -f $UMEED_BIN_MAINNET ]; then
-  wget -c $UMEED_BIN_MAINNET_URL_TARBALL -O - | tar -xz
-fi
-
-echo "CWD/extracted_dir"  $CWD/$extracted_dir
-
-echo "ls CWD"
-ls $CWD
-
-echo "ls CWD/extracted_dir"
-ls $CWD/$extracted_dir
 
 CHAIN_ID="${CHAIN_ID:-umeemain-local-testnet}"
 FORK_DIR="${FORK_DIR:-$CWD}"
