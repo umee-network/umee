adc16af38f3b4ced978a6d6752b3afcf28672ead
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-09-05 10:33:06 -0300

    upgrade from mainnet

---
+++ contrib/scripts/umeemainnet_fork.sh
@@ -0,0 +1,178 @@
+#!/bin/bash -eux
+
+# Start a single node chain from an exported genesis file,
+# process the genesis file if it wasn't already,
+# wait till the node starts to produce blocks,
+# upgrade this fork with a software-upgrade proposal.
+
+# USAGE: ./umeemainnet_fork.sh
+
+CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CHAIN_ID="${CHAIN_ID:-umeemain-local-testnet}"
+FORK_DIR="${FORK_DIR:-$CWD}"
+CHAIN_DIR="${CHAIN_DIR:-$FORK_DIR/node-data}"
+LOG_LEVEL="${LOG_LEVEL:-debug}"
+BLOCK_TIME="${BLOCK_TIME:-6}"
+UPGRADE_TITLE="${UPGRADE_TITLE:-"v1.0-v3.0"}"
+UMEED_BIN_CURRENT="${UMEED_BIN_CURRENT:-$FORK_DIR/../../build/umeed}"
+UMEED_BIN_MAINNET="${UMEED_BIN_MAINNET:-$FORK_DIR/mainnet-umeed-v103}"
+UMEEMAINNET_GENESIS_PATH="${UMEEMAINNET_GENESIS_PATH:-$CWD/tinkered_genesis.json}"
+NODE_PRIV_KEY="${NODE_PRIV_KEY:-$FORK_DIR/priv_validator_key-coping.json}"
+
+nodeHome="$CHAIN_DIR/$CHAIN_ID"
+home="--home $nodeHome"
+
+hdir="$CHAIN_DIR/$CHAIN_ID"
+
+# Folders for nodes
+nodeDir="$hdir/node"
+
+# Home flag for folder
+nodeHome="--home $nodeDir"
+
+# Config directories for nodes
+nodeCfgDir="$nodeDir/config"
+
+# Config files for nodes
+nodeCfg="$nodeCfgDir/config.toml"
+
+# App config files for nodes
+nodeApp="$nodeCfgDir/app.toml"
+
+# Node private key to sign blocks
+nodePrivateKeyPath=$nodeDir/config/priv_validator_key.json
+
+# Common flags
+kbt="--keyring-backend test"
+cid="--chain-id $CHAIN_ID"
+
+ADMIN_KEY="admin"
+NODE_MNEMONIC="festival dumb luxury boss forum clip scatter moral ribbon language rib unable burden agree burden misery suspect find crucial seat canvas endless bring lobster"
+NEWLINE=$'\n'
+
+genesisConfigPath="config/genesis.json"
+
+# Checks for private_key file
+if test -f "$NODE_PRIV_KEY"; then
+  echo "$NODE_PRIV_KEY exists."
+else
+  echo "$NODE_PRIV_KEY does not exists, exiting..."
+  exit 1
+fi
+
+# Checks for the tikered genesis file
+if [ ! -f "$UMEEMAINNET_GENESIS_PATH" ]; then
+  echo "$UMEEMAINNET_GENESIS_PATH doesn't exist"
+  EXPORTED_GENESIS_UNPROCESSED=$CWD/umeemainnet.genesis.json
+
+  if [ ! -f "$EXPORTED_GENESIS_UNPROCESSED" ]; then
+
+    EXPORTED_GENESIS_UNZIPED=$CWD/umeemainnet.genesis.json.gz
+
+    if [ ! -f $EXPORTED_GENESIS_UNZIPED ]; then
+      echo "$EXPORTED_GENESIS_UNZIPED doesn't exist, we need to curl it"
+      curl https://storage.googleapis.com/umeedropzone/jul-28-umee-1-export.json.gz > $EXPORTED_GENESIS_UNZIPED
+    fi
+
+    echo "$EXPORTED_GENESIS_UNPROCESSED doesn't exist, we need to unpack"
+    gunzip -k $EXPORTED_GENESIS_UNZIPED
+  fi
+
+  echo "$EXPORTED_GENESIS_UNPROCESSED exists and ready to be processed"
+  EXPORTED_GENESIS_UNPROCESSED_PATH=$EXPORTED_GENESIS_UNPROCESSED COSMOS_GENESIS_TINKERER_SCRIPT=umeemainnet-fork.py EXPORTED_GENESIS_PROCESSED_PATH=$UMEEMAINNET_GENESIS_PATH $CWD/process_genesis.sh
+fi
+
+echo Remove everything from the $CHAIN_DIR
+rm -rf $CHAIN_DIR
+
+echo Start the chain basic config
+$UMEED_BIN_MAINNET $nodeHome $cid init $ADMIN_KEY
+
+echo Create node admin wallet
+yes "$NODE_MNEMONIC$NEWLINE" | $UMEED_BIN_MAINNET $nodeHome keys add $ADMIN_KEY $kbt --recover
+NODE_ADDR="$($UMEED_BIN_MAINNET $nodeHome keys show $ADMIN_KEY -a $kbt)"
+
+cp $NODE_PRIV_KEY $nodePrivateKeyPath
+
+echo Coping addr is $NODE_ADDR
+
+echo Replace generated genesis with umeemania genesis
+rm $nodeDir/$genesisConfigPath
+
+cp $UMEEMAINNET_GENESIS_PATH $nodeDir/$genesisConfigPath
+
+perl -i -pe 's|fast_sync = true|fast_sync = false|g' $nodeCfg
+perl -i -pe 's|addr_book_strict = true|addr_book_strict = false|g' $nodeCfg
+perl -i -pe 's|external_address = ""|external_address = "tcp://127.0.0.1:26657"|g' $nodeCfg
+perl -i -pe 's|"tcp://127.0.0.1:26657"|"tcp://0.0.0.0:26657"|g' $nodeCfg
+perl -i -pe 's|allow_duplicate_ip = false|allow_duplicate_ip = true|g' $nodeCfg
+perl -i -pe 's|log_level = "info"|log_level = "'$LOG_LEVEL'"|g' $nodeCfg
+perl -i -pe 's|timeout_commit = ".*?"|timeout_commit = "5s"|g' $nodeCfg
+
+nodeLogPath=$hdir.umeed-main.log
+unset UMEE_ENABLE_BETA
+
+pid_path=$nodeDir.pid
+UMEE_ENABLE_BETA=false $UMEED_BIN_MAINNET $nodeHome start --grpc.address="0.0.0.0:9090" --x-crisis-skip-assert-invariants --grpc-web.enable=false --log_level $LOG_LEVEL > $nodeLogPath 2>&1 &
+
+# Gets the node piid
+echo $! > $pid_path
+pid_value=$(cat $pid_path)
+
+echo
+echo "Logs:"
+echo "  * tail -f $nodeLogPath"
+
+echo Wait for the node to load the genesis state and start to produce blocks ≃ 8min D:
+sleep 50
+
+# Any block number to be confirmed
+WAIT_UNTIL_HEIGHT=1000
+CURRENT_BLOCK_HEIGHT=0
+MAX_TRIES=50
+CURRENT_TRY=0
+
+while [ $CURRENT_BLOCK_HEIGHT -lt $WAIT_UNTIL_HEIGHT ]
+do
+  QUERY_RESPONSE="$($UMEED_BIN_MAINNET query block $nodeHome --chain-id $CHAIN_ID)"
+  CURRENT_BLOCK_HEIGHT=$(echo $QUERY_RESPONSE | jq -r '.block.header.height')
+  echo "Current block height $CURRENT_BLOCK_HEIGHT, waiting to reach $WAIT_UNTIL_HEIGHT"
+  ((CURRENT_TRY=CURRENT_TRY+1))
+
+  if [ $CURRENT_TRY -ge $MAX_TRIES ]; then
+    exit 1
+  fi
+
+  sleep $BLOCK_TIME
+done
+
+echo "Current Block: $CURRENT_BLOCK_HEIGHT == $WAIT_UNTIL_HEIGHT"
+
+CURRENT_TRY=0
+# we should produce at least 20 blocks with the new version
+((WAIT_UNTIL_HEIGHT=CURRENT_BLOCK_HEIGHT+20))
+
+UMEED_V1_PID=$pid_value CHAIN_DIR=$CHAIN_DIR CHAIN_ID=$CHAIN_ID LOG_LEVEL=$LOG_LEVEL NODE_NAME=node UPGRADE_TITLE=$UPGRADE_TITLE UMEED_BIN_V1=$UMEED_BIN_MAINNET UMEED_BIN_V2=$UMEED_BIN_CURRENT $CWD/upgrade-test-single-node.sh
+
+echo "UPGRADE FINISH, going to wait to produce 20 blocks from: $CURRENT_BLOCK_HEIGHT"
+sleep 30
+
+while [ $CURRENT_BLOCK_HEIGHT -lt $WAIT_UNTIL_HEIGHT ]
+do
+  QUERY_RESPONSE="$($UMEED_BIN_MAINNET query block $nodeHome --chain-id $CHAIN_ID)"
+  CURRENT_BLOCK_HEIGHT=$(echo $QUERY_RESPONSE | jq -r '.block.header.height')
+  echo "Current block height $CURRENT_BLOCK_HEIGHT, waiting to reach $WAIT_UNTIL_HEIGHT"
+  ((CURRENT_TRY=CURRENT_TRY+1))
+
+  if [ $CURRENT_TRY -ge $MAX_TRIES ]; then
+    exit 1
+  fi
+
+  sleep $BLOCK_TIME
+done
+
+echo
+echo Upgrade Process Finish
+echo
+
+exit 0
---
+++ contrib/scripts/upgrade-test-single-node.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -eux
+
+# It uses an chain already up and start an governance proposal to upgrade to a new binary version
+# vote 'yes' for that proposal, wait to reach to reach an upgrade height and kill the process id received by parameter $UMEED_V1_PID
+
+# USAGE: UMEED_V1_PID=$umee_pid ./upgrade-test-single-node.sh
+
+CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+UMEED_V1_PID="${UMEED_V1_PID:-0}"
+
+if [ $UMEED_V1_PID -le 0 ]; then
+  echo "You need to specify the process id of umeed v1 by setting UMEED_V1_PID"
+  exit 1
+fi
+
+CHAIN_ID="${CHAIN_ID:-888}"
+CHAIN_DIR="${CHAIN_DIR:-$CWD/node-data}"
+NODE_NAME="${NODE_NAME:-coping}"
+LOG_LEVEL="${LOG_LEVEL:-info}"
+NODE_URL="${NODE_URL:-"tcp://localhost:26657"}"
+BLOCK_TIME="${BLOCK_TIME:-6}"
+UPGRADE_TITLE="${UPGRADE_TITLE:-cosmwasm}"
+
+UMEED_BUILD_PATH="${UMEED_BUILD_PATH:-$CWD/umeed-builds}"
+UMEED_BIN_V1="${UMEED_BIN_V1:-$UMEED_BUILD_PATH/umeed-fix-testnet-halt}"
+UMEED_BIN_V2="${UMEED_BIN_V2:-$UMEED_BUILD_PATH/umeed-cosmwasm}"
+
+VOTING_PERIOD=${VOTING_PERIOD:-8}
+
+hdir="$CHAIN_DIR/$CHAIN_ID"
+
+# Folders for nodes
+nodeDir="$hdir/$NODE_NAME"
+
+# Home flag for folder
+nodeHomeFlag="--home $nodeDir"
+nodeUrlFlag="--node $NODE_URL"
+
+# Common flags
+kbt="--keyring-backend test"
+cid="--chain-id $CHAIN_ID"
+
+CURRENT_HEIGHT=$($UMEED_BIN_V1 q block $nodeUrlFlag | jq ".block.header.height" -r)
+echo blockchain CURRENT_HEIGHT is $CURRENT_HEIGHT
+
+UPGRADE_HEIGHT=$(($CURRENT_HEIGHT + 10))
+echo blockchain UPGRADE_HEIGHT is $UPGRADE_HEIGHT
+
+$UMEED_BIN_V1 tx gov submit-proposal software-upgrade $UPGRADE_TITLE --deposit 1000000000uumee \
+  --upgrade-height $UPGRADE_HEIGHT --upgrade-info '{"binaries":{"linux/amd64":"https://github.com/cosmos/gaia/releases/download/v6.0.0-rc1/gaiad-v6.0.0-rc1-linux-amd64","linux/arm64":"https://github.com/cosmos/gaia/releases/download/v6.0.0-rc1/gaiad-v6.0.0-rc1-linux-arm64","darwin/amd64":"https://github.com/cosmos/gaia/releases/download/v6.0.0-rc1/gaiad-v6.0.0-rc1-darwin-amd64"}}' \
+  -b block $nodeHomeFlag --from admin $nodeUrlFlag $kbt --title yeet --description megayeet $cid --yes
+
+PROPOSAL_ID=$($UMEED_BIN_V1 q gov $nodeUrlFlag proposals -o json | jq ".proposals[-1].proposal_id" -r)
+echo proposal ID is $PROPOSAL_ID
+
+$UMEED_BIN_V1 tx gov vote -b async --from admin $nodeUrlFlag $kbt $PROPOSAL_ID yes $nodeHomeFlag $cid --yes
+
+echo "..."
+echo "Finish voting in the proposal"
+echo "It will wait to reach the block height to upgrade"
+echo "..."
+BLOCK_HEIGHT=0
+while [ $BLOCK_HEIGHT -lt $UPGRADE_HEIGHT ]
+do
+  QUERY_RESPONSE="$($UMEED_BIN_V1 query block $nodeHomeFlag $nodeUrlFlag $cid)"
+  BLOCK_HEIGHT=$(echo $QUERY_RESPONSE | jq -r '.block.header.height')
+  echo "Current block height $BLOCK_HEIGHT, waiting to reach $UPGRADE_HEIGHT"
+  sleep $BLOCK_TIME
+done
+
+echo "Reached upgrade block height: $BLOCK_HEIGHT == $UPGRADE_HEIGHT"
+echo "Kill the process ID '$UMEED_V1_PID'"
+
+kill -s 15 $UMEED_V1_PID
+
+sleep 5
+
+echo "...."
+echo "Upgrade finish"
+echo "...."
+sleep $VOTING_PERIOD
+
+# Starts a different file for logging
+nodeLogPath=$hdir.umeed-v2.log
+
+$UMEED_BIN_V2 $nodeHomeFlag start --minimum-gas-prices=0.001uumee --grpc.address="0.0.0.0:9090"  --grpc-web.enable=false --log_level $LOG_LEVEL > $nodeLogPath 2>&1 &
+
+echo
+echo "Logs:"
+echo "  * tail -f $nodeLogPath"
