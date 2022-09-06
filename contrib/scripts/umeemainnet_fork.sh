4ca617acb8ffa023ef85c49fbbfd7b2356b8fa48
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-09-06 16:39:57 -0300

    add var for MAINNET_EXPORTED_GENESIS_URL

--- contrib/scripts/umeemainnet_fork.sh
+++ contrib/scripts/umeemainnet_fork.sh
@@ -34,6 +34,7 @@ CHAIN_DIR="${CHAIN_DIR:-$FORK_DIR/node-data}"
 LOG_LEVEL="${LOG_LEVEL:-debug}"
 BLOCK_TIME="${BLOCK_TIME:-6}"
 UPGRADE_TITLE="${UPGRADE_TITLE:-"v1.0-v3.0"}"
+MAINNET_EXPORTED_GENESIS_URL="${MAINNET_EXPORTED_GENESIS_URL:-"https://storage.googleapis.com/umeedropzone/jul-28-umee-1-export.json.gz"}"
 UMEED_BIN_CURRENT="${UMEED_BIN_CURRENT:-$FORK_DIR/../../build/umeed}"
 UMEEMAINNET_GENESIS_PATH="${UMEEMAINNET_GENESIS_PATH:-$CWD/tinkered_genesis.json}"
 NODE_PRIV_KEY="${NODE_PRIV_KEY:-$FORK_DIR/priv_validator_key-coping.json}"
@@ -94,7 +95,7 @@ if [ ! -f "$UMEEMAINNET_GENESIS_PATH" ]; then
 
     if [ ! -f $EXPORTED_GENESIS_UNZIPED ]; then
       echo "$EXPORTED_GENESIS_UNZIPED doesn't exist, we need to curl it"
-      curl https://storage.googleapis.com/umeedropzone/jul-28-umee-1-export.json.gz > $EXPORTED_GENESIS_UNZIPED
+      curl $MAINNET_EXPORTED_GENESIS_URL > $EXPORTED_GENESIS_UNZIPED
     fi
 
     echo "$EXPORTED_GENESIS_UNPROCESSED doesn't exist, we need to unpack"
