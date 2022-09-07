5cbf94802016cd271ff0415bd874dd1dfa90140f
toteki <63419657+toteki@users.noreply.github.com>
2022-09-06 17:43:11 -0700

    feat: implement MaxSupply (#1319)
    ## Description
    
    This implements one of the token safety parameters on the leverage checklist.
    
    closes: #1227
    
    ---
    
    ### Author Checklist
    
    - [x] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [x] added `!` to the type prefix if API or client breaking change
    - [x] added appropriate labels to the PR
    - [x] targeted the correct branch (see [PR Targeting](https://github.com/umee-network/umee/blob/main/CONTRIBUTING.md#pr-targeting))
    - [x] provided a link to the relevant issue or specification
    - [x] added a changelog entry to `CHANGELOG.md`
    - [x] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [x] confirmed all CI checks have passed

--- x/leverage/keeper/keeper_test.go
+++ x/leverage/keeper/keeper_test.go
@@ -28,6 +28,9 @@ func (s *IntegrationTestSuite) TestSupply() {
 	s.borrow(borrower, coin(atomDenom, 10_000000))
 	s.tk.SetBorrow(ctx, borrower, coin(atomDenom, 60_000000))
 
+	// create a supplier that will exceed token's default MaxSupply
+	whale := s.newAccount(coin(umeeDenom, 1_000_000_000000))
+
 	tcs := []testCase{
 		{
 			"unregistered denom",
@@ -77,6 +80,13 @@ func (s *IntegrationTestSuite) TestSupply() {
 			coin(atomDenom, 60_000000),
 			coin("u/"+atomDenom, 40_000000),
 			nil,
+		},
+		{
+			"max supply",
+			whale,
+			coin(umeeDenom, 1_000_000_000000),
+			sdk.Coin{},
+			types.ErrMaxSupply,
 		},
 	}
 
