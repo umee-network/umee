fb5df4d656a03c6d4c58c86bc60927409b307c92
Robert Zaremba <robert@zaremba.ch>
2022-09-08 20:48:47 +0200

    update free gravity msg set

--- ante/fee.go
+++ ante/fee.go
@@ -87,9 +87,7 @@ func IsOracleOrGravityTx(msgs []sdk.Msg) bool {
 			*oracletypes.MsgAggregateExchangeRateVote:
 			continue
 
-		// TODO: remove messages which should not be "free":
 		case *gbtypes.MsgValsetConfirm,
-			*gbtypes.MsgRequestBatch,
 			*gbtypes.MsgConfirmBatch,
 			*gbtypes.MsgERC20DeployedClaim,
 			*gbtypes.MsgConfirmLogicCall,
