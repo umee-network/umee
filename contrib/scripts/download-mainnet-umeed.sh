53aceba81af8fb2e57e5225d194116d14d733f03
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-09-12 16:18:09 -0300

    test cache umeed v103

---
+++ contrib/scripts/download-mainnet-umeed.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+
+# Donwload the umeed binary with the same version as mainnet and upack it
+
+# USAGE: ./download-mainnet-umeed.sh
+
+CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+UMEED_BIN_MAINNET_URL_TARBALL=${UMEED_BIN_MAINNET_URL_TARBALL:-"https://github.com/umee-network/umee/releases/download/v1.0.3/umeed-v1.0.3-linux-amd64.tar.gz"}
+UMEED_BIN_MAINNET=${UMEED_BIN_MAINNET:-"$CWD/umeed-releases/umeed-v1.0.3-linux-amd64/umeed"}
+
+# Checks for the umeed v1 file
+if [ ! -f "$UMEED_BIN_MAINNET" ]; then
+  echo "$UMEED_BIN_MAINNET doesn't exist"
+
+  if [ -z $UMEED_BIN_MAINNET_URL_TARBALL ]; then
+    echo You need to set the UMEED_BIN_MAINNET_URL_TARBALL variable
+    exit 1
+  fi
+
+  UMEED_RELEASES_PATH=$CWD/umeed-releases
+  mkdir -p $UMEED_RELEASES_PATH
+  wget -c $UMEED_BIN_MAINNET_URL_TARBALL -O - | tar -xz -C $UMEED_RELEASES_PATH
+
+  UMEED_BIN_MAINNET_BASENAME=$(basename $UMEED_BIN_MAINNET_URL_TARBALL .tar.gz)
+  UMEED_BIN_MAINNET=$UMEED_RELEASES_PATH/$UMEED_BIN_MAINNET_BASENAME/umeed
+fi
--- contrib/scripts/umeemainnet_fork.sh
+++ contrib/scripts/umeemainnet_fork.sh
@@ -8,25 +8,6 @@
 # USAGE: ./umeemainnet_fork.sh
 
 CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-UMEED_BIN_MAINNET_URL_TARBALL=${UMEED_BIN_MAINNET_URL_TARBALL:-"https://github.com/umee-network/umee/releases/download/v1.0.3/umeed-v1.0.3-linux-amd64.tar.gz"}
-UMEED_BIN_MAINNET=${UMEED_BIN_MAINNET:-"$CWD/umeed-releases/umeed-v1.0.3-linux-amd64/umeed"}
-
-# Checks for the umeed v1 file
-if [ ! -f "$UMEED_BIN_MAINNET" ]; then
-  echo "$UMEED_BIN_MAINNET doesn't exist"
-
-  if [ -z $UMEED_BIN_MAINNET_URL_TARBALL ]; then
-    echo You need to set the UMEED_BIN_MAINNET_URL_TARBALL variable
-    exit 1
-  fi
-
-  UMEED_RELEASES_PATH=$CWD/umeed-releases
-  mkdir -p $UMEED_RELEASES_PATH
-  wget -c $UMEED_BIN_MAINNET_URL_TARBALL -O - | tar -xz -C $UMEED_RELEASES_PATH
-
-  UMEED_BIN_MAINNET_BASENAME=$(basename $UMEED_BIN_MAINNET_URL_TARBALL .tar.gz)
-  UMEED_BIN_MAINNET=$UMEED_RELEASES_PATH/$UMEED_BIN_MAINNET_BASENAME/umeed
-fi
 
 CHAIN_ID="${CHAIN_ID:-umeemain-local-testnet}"
 FORK_DIR="${FORK_DIR:-$CWD}"
@@ -41,6 +22,7 @@ NODE_PRIV_KEY="${NODE_PRIV_KEY:-$FORK_DIR/priv_validator_key.json}"
 SEC_AWAIT_NODE_START="${SEC_AWAIT_NODE_START:-80}"
 
 # Loads another sources
+. $CWD/download-mainnet-umeed.sh
 . $CWD/blocks.sh
 
 nodeHome="$CHAIN_DIR/$CHAIN_ID"
