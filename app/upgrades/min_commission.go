312cbe41b0c2086cdc5071b7d37a9acb9c6b0059
Sai Kumar <sai@umee.cc>
2022-09-14 21:49:16 +0530

    test: add tests to setMinCommissionRate

--- app/upgrades/min_commission.go
+++ app/upgrades/min_commission.go
@@ -2,7 +2,6 @@ package upgrades
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	stakingKeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 )
 
 var (
@@ -10,7 +9,7 @@ var (
 )
 
 // UpdateMinimumCommissionRateParam is update the minimum commission rate param of staking.
-func UpdateMinimumCommissionRateParam(ctx sdk.Context, keeper *stakingKeeper.Keeper) (sdk.Dec, error) {
+func UpdateMinimumCommissionRateParam(ctx sdk.Context, keeper StakingKeeper) (sdk.Dec, error) {
 	params := keeper.GetParams(ctx)
 	// update the minCommissionRate param
 	params.MinCommissionRate = minCommissionRate
@@ -23,22 +22,20 @@ func UpdateMinimumCommissionRateParam(ctx sdk.Context, keeper *stakingKeeper.Kee
 // SetMinimumCommissionRateToValidatros is update the minimum commission rate to the validators rate
 // whose commission rate is below the minimum commission rate.
 func SetMinimumCommissionRateToValidatros(
-	ctx sdk.Context, keeper *stakingKeeper.Keeper, minCommissionRate sdk.Dec) error {
+	ctx sdk.Context, keeper StakingKeeper, minCommissionRate sdk.Dec) error {
 	validators := keeper.GetAllValidators(ctx)
 
 	for _, validator := range validators {
-		if validator.Commission.Rate.GTE(minCommissionRate) {
-			continue
+		if validator.Commission.Rate.IsNil() || validator.Commission.Rate.LT(minCommissionRate) {
+			if err := keeper.BeforeValidatorModified(ctx, validator.GetOperator()); err != nil {
+				return err
+			}
+
+			validator.Commission.Rate = minCommissionRate
+			validator.Commission.UpdateTime = ctx.BlockTime()
+
+			keeper.SetValidator(ctx, validator)
 		}
-
-		if err := keeper.BeforeValidatorModified(ctx, validator.GetOperator()); err != nil {
-			return err
-		}
-
-		validator.Commission.Rate = minCommissionRate
-		validator.Commission.UpdateTime = ctx.BlockTime()
-
-		keeper.SetValidator(ctx, validator)
 	}
 
 	return nil
