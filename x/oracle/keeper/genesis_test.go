770ed82d76c196bfc624d3b3b34ce1b66cc03e9b
zarazan <zarazan@users.noreply.github.com>
2022-11-16 08:21:55 -0700

    finish up genesis tests

--- x/oracle/keeper/genesis_test.go
+++ x/oracle/keeper/genesis_test.go
@@ -2,6 +2,7 @@ package keeper_test
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 	"github.com/umee-network/umee/v3/x/oracle/types"
 )
 
@@ -49,5 +50,73 @@ FOUND:
 			}
 		}
 		s.T().Errorf("did not find match for historic price: %+v", oldPrice)
+	}
+}
+
+func (s *IntegrationTestSuite) TestIterateAllMedianPrices() {
+	keeper, ctx := s.app.OracleKeeper, s.ctx
+	medians := []types.ExchangeRateTuple{
+		{Denom: "umee", ExchangeRate: sdk.MustNewDecFromStr("20.44")},
+		{Denom: "atom", ExchangeRate: sdk.MustNewDecFromStr("2.66")},
+		{Denom: "osmo", ExchangeRate: sdk.MustNewDecFromStr("13.64")},
+	}
+
+	for _, m := range medians {
+		keeper.SetMedian(ctx, m.Denom, m.ExchangeRate)
+	}
+
+	newMedians := []types.ExchangeRateTuple{}
+	keeper.IterateAllMedianPrices(
+		ctx,
+		func(median types.ExchangeRateTuple) bool {
+			newMedians = append(newMedians, median)
+			return false
+		},
+	)
+	require.Equal(s.T(), len(medians), len(newMedians))
+
+FOUND:
+	for _, oldMedian := range medians {
+		for _, newMedian := range newMedians {
+			if oldMedian.Denom == newMedian.Denom {
+				s.Require().Equal(oldMedian.ExchangeRate, newMedian.ExchangeRate)
+				continue FOUND
+			}
+		}
+		s.T().Errorf("did not find match for median price: %+v", oldMedian)
+	}
+}
+
+func (s *IntegrationTestSuite) TestIterateAllMedianDeviationPrices() {
+	keeper, ctx := s.app.OracleKeeper, s.ctx
+	medians := []types.ExchangeRateTuple{
+		{Denom: "umee", ExchangeRate: sdk.MustNewDecFromStr("21.44")},
+		{Denom: "atom", ExchangeRate: sdk.MustNewDecFromStr("3.66")},
+		{Denom: "osmo", ExchangeRate: sdk.MustNewDecFromStr("14.64")},
+	}
+
+	for _, m := range medians {
+		keeper.SetMedianDeviation(ctx, m.Denom, m.ExchangeRate)
+	}
+
+	newMedians := []types.ExchangeRateTuple{}
+	keeper.IterateAllMedianDeviationPrices(
+		ctx,
+		func(median types.ExchangeRateTuple) bool {
+			newMedians = append(newMedians, median)
+			return false
+		},
+	)
+	require.Equal(s.T(), len(medians), len(newMedians))
+
+FOUND:
+	for _, oldMedian := range medians {
+		for _, newMedian := range newMedians {
+			if oldMedian.Denom == newMedian.Denom {
+				s.Require().Equal(oldMedian.ExchangeRate, newMedian.ExchangeRate)
+				continue FOUND
+			}
+		}
+		s.T().Errorf("did not find match for median price: %+v", oldMedian)
 	}
 }
