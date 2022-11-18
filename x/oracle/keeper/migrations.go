bcd3e5aefc4292114e2fd889493dae9a4ee2f638
Robert Zaremba <robert@zaremba.ch>
2022-11-18 02:00:40 +0000

    feat: add oracle migration for v3.2 (#1593)
    ## Description
    
    Migration fixing tthe oracle params set.
    
    ---
    
    ### Author Checklist
    
    _All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues._
    
    I have...
    
    - [ ] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] added appropriate labels to the PR
    - [ ] targeted the correct branch (see [PR Targeting](https://github.com/umee-network/umee/blob/main/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    _All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items._
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

---
+++ x/oracle/keeper/migrations.go
@@ -0,0 +1,26 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/umee-network/umee/v3/x/oracle/types"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	keeper *Keeper
+}
+
+// NewMigrator creates a Migrator.
+func NewMigrator(keeper *Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+// Migrate1to2 migrates from version 1 to 2.
+func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	m.keeper.SetStampPeriod(ctx, 1)
+	m.keeper.SetPrunePeriod(ctx, 1)
+	m.keeper.SetMedianPeriod(ctx, 1)
+	m.keeper.SetHistoricAcceptList(ctx, types.DenomList{})
+	return nil
+}
