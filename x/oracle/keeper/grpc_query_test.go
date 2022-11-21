72c0550a7152a5ae45515034263b095f24367d60
Kyle Zarazan <zarazan@users.noreply.github.com>
2022-11-21 09:15:26 -0700

    feat: add historacle grpc queries (#1608)
    add query functions for median and median deviation

--- x/oracle/keeper/grpc_query_test.go
+++ x/oracle/keeper/grpc_query_test.go
@@ -202,6 +202,38 @@ func (s *IntegrationTestSuite) TestQuerier_AggregateVotesAppendVotes() {
 	s.Require().Nil(err)
 }
 
+func (s *IntegrationTestSuite) TestQuerier_Medians() {
+	atomMedian := sdk.DecCoin{Denom: "atom", Amount: sdk.MustNewDecFromStr("49.99")}
+	umeeMedian := sdk.DecCoin{Denom: "umee", Amount: sdk.MustNewDecFromStr("6541.48")}
+
+	s.app.OracleKeeper.SetMedian(s.ctx, atomMedian.Denom, atomMedian.Amount)
+	s.app.OracleKeeper.SetMedian(s.ctx, umeeMedian.Denom, umeeMedian.Amount)
+
+	res, err := s.queryClient.Medians(s.ctx.Context(), &types.QueryMedians{})
+	s.Require().NoError(err)
+	s.Require().Equal(res.Medians, sdk.NewDecCoins(atomMedian, umeeMedian))
+
+	res, err = s.queryClient.Medians(s.ctx.Context(), &types.QueryMedians{Denom: atomMedian.Denom})
+	s.Require().NoError(err)
+	s.Require().Equal(res.Medians, sdk.NewDecCoins(atomMedian))
+}
+
+func (s *IntegrationTestSuite) TestQuerier_MedianDeviations() {
+	atomMedianDeviation := sdk.DecCoin{Denom: "atom", Amount: sdk.MustNewDecFromStr("39.99")}
+	umeeMedianDeviation := sdk.DecCoin{Denom: "umee", Amount: sdk.MustNewDecFromStr("9541.48")}
+
+	s.app.OracleKeeper.SetMedianDeviation(s.ctx, atomMedianDeviation.Denom, atomMedianDeviation.Amount)
+	s.app.OracleKeeper.SetMedianDeviation(s.ctx, umeeMedianDeviation.Denom, umeeMedianDeviation.Amount)
+
+	res, err := s.queryClient.MedianDeviations(s.ctx.Context(), &types.QueryMedianDeviations{})
+	s.Require().NoError(err)
+	s.Require().Equal(res.MedianDeviations, sdk.NewDecCoins(atomMedianDeviation, umeeMedianDeviation))
+
+	res, err = s.queryClient.MedianDeviations(s.ctx.Context(), &types.QueryMedianDeviations{Denom: atomMedianDeviation.Denom})
+	s.Require().NoError(err)
+	s.Require().Equal(res.MedianDeviations, sdk.NewDecCoins(atomMedianDeviation))
+}
+
 func (s *IntegrationTestSuite) TestEmptyRequest() {
 	q := keeper.NewQuerier(keeper.Keeper{})
 	const emptyRequestErrorMsg = "empty request"
