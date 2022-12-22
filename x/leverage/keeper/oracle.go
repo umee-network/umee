1dc6d2116e688529ac14296a7579a51d626e399c
toteki <63419657+toteki@users.noreply.github.com>
2022-12-22 12:31:05 -0700

    lint++

--- x/leverage/keeper/oracle.go
+++ x/leverage/keeper/oracle.go
@@ -41,9 +41,9 @@ func (k Keeper) TokenBasePrice(ctx sdk.Context, baseDenom string) (sdk.Dec, erro
 	return price, nil
 }
 
-// TokenDefaultDenomPrice returns the USD value of a token's symbol denom, e.g. `UMEE` (rather than `uumee`). Note, the input
-// denom must still be the base denomination, e.g. uumee. When error is nil, price is guaranteed
-// to be positive. Also returns the token's exponent to reduce redundant registry reads.
+// TokenDefaultDenomPrice returns the USD value of a token's symbol denom, e.g. `UMEE` (rather than `uumee`).
+// Note, the input denom must still be the base denomination, e.g. uumee. When error is nil, price is
+// guaranteed to be positive. Also returns the token's exponent to reduce redundant registry reads.
 // If the historic parameter is true, uses a median of recent prices instead of current price.
 func (k Keeper) TokenDefaultDenomPrice(ctx sdk.Context, baseDenom string, historic bool) (sdk.Dec, uint32, error) {
 	t, err := k.GetTokenSettings(ctx, baseDenom)
