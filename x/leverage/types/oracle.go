1cca350f7e97e9d46fbd0c8d1783bc245ee09738
Adam Moser <63419657+toteki@users.noreply.github.com>
2023-01-18 15:57:39 -0700

    Update x/leverage/types/oracle.go
    Co-authored-by: Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>

--- x/leverage/types/oracle.go
+++ x/leverage/types/oracle.go
@@ -1,10 +1,15 @@
 package types
 
-type PriceMode uint64 // Enumerates different ways to request the price of a token
+// Enumerates different ways to request the price of a token
+type PriceMode uint64 
 
 const (
-	PriceModeSpot     PriceMode = iota // Spot mode requests the most recent prices from oracle
-	PriceModeHistoric                  // Historic mode requests the median of the most recent historic medians
-	PriceModeHigh                      // High mode uses the higher of either Spot or Historic prices
-	PriceModeLow                       // Low mode uses the lower of either Spot or Historic prices
+        // Spot mode requests the most recent prices from oracle
+	PriceModeSpot     PriceMode = iota 
+	// Historic mode requests the median of the most recent historic medians
+	PriceModeHistoric             
+	// High mode uses the higher of either Spot or Historic prices     
+	PriceModeHigh                      
+	// Low mode uses the lower of either Spot or Historic prices
+	PriceModeLow                       
 )
