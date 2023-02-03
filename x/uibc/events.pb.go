4c8d2da6700891c97e4d2cbe03818f727a011b0f
Robert Zaremba <robert@zaremba.ch>
2023-02-03 14:56:34 +0100

    feat(uibc): pause event

--- x/uibc/events.pb.go
+++ x/uibc/events.pb.go
@@ -25,8 +25,6 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // EventBadRevert is emitted on failure of ibc-transfer quota.
 type EventBadRevert struct {
-	// module name
-	Module string `protobuf:"bytes,1,opt,name=module,proto3" json:"module,omitempty"`
 	// failure event type
 	FailureType string `protobuf:"bytes,2,opt,name=failure_type,json=failureType,proto3" json:"failure_type,omitempty"`
 	// ibc packet data
@@ -66,28 +64,68 @@ func (m *EventBadRevert) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_EventBadRevert proto.InternalMessageInfo
 
+// EventPause is emitted on quota tracking pause status change.
+type EventPause struct {
+	Paused bool `protobuf:"varint,1,opt,name=paused,proto3" json:"paused,omitempty"`
+}
+
+func (m *EventPause) Reset()         { *m = EventPause{} }
+func (m *EventPause) String() string { return proto.CompactTextString(m) }
+func (*EventPause) ProtoMessage()    {}
+func (*EventPause) Descriptor() ([]byte, []int) {
+	return fileDescriptor_c64e60b79cebf048, []int{1}
+}
+func (m *EventPause) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *EventPause) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_EventPause.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *EventPause) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_EventPause.Merge(m, src)
+}
+func (m *EventPause) XXX_Size() int {
+	return m.Size()
+}
+func (m *EventPause) XXX_DiscardUnknown() {
+	xxx_messageInfo_EventPause.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_EventPause proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*EventBadRevert)(nil), "umee.uibc.v1.EventBadRevert")
+	proto.RegisterType((*EventPause)(nil), "umee.uibc.v1.EventPause")
 }
 
 func init() { proto.RegisterFile("umee/uibc/v1/events.proto", fileDescriptor_c64e60b79cebf048) }
 
 var fileDescriptor_c64e60b79cebf048 = []byte{
-	// 218 bytes of a gzipped FileDescriptorProto
+	// 229 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2c, 0xcd, 0x4d, 0x4d,
 	0xd5, 0x2f, 0xcd, 0x4c, 0x4a, 0xd6, 0x2f, 0x33, 0xd4, 0x4f, 0x2d, 0x4b, 0xcd, 0x2b, 0x29, 0xd6,
 	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x01, 0x49, 0xe9, 0x81, 0xa4, 0xf4, 0xca, 0x0c, 0xa5,
-	0x44, 0xd2, 0xf3, 0xd3, 0xf3, 0xc1, 0x12, 0xfa, 0x20, 0x16, 0x44, 0x8d, 0x52, 0x32, 0x17, 0x9f,
-	0x2b, 0x48, 0x8f, 0x53, 0x62, 0x4a, 0x50, 0x6a, 0x59, 0x6a, 0x51, 0x89, 0x90, 0x18, 0x17, 0x5b,
-	0x6e, 0x7e, 0x4a, 0x69, 0x4e, 0xaa, 0x04, 0xa3, 0x02, 0xa3, 0x06, 0x67, 0x10, 0x94, 0x27, 0xa4,
-	0xc8, 0xc5, 0x93, 0x96, 0x98, 0x99, 0x53, 0x5a, 0x94, 0x1a, 0x5f, 0x52, 0x59, 0x90, 0x2a, 0xc1,
-	0x04, 0x96, 0xe5, 0x86, 0x8a, 0x85, 0x54, 0x16, 0xa4, 0x82, 0xb4, 0x16, 0x24, 0x26, 0x67, 0xa7,
-	0x96, 0x48, 0x30, 0x43, 0xb4, 0x42, 0x78, 0x4e, 0x2e, 0x27, 0x1e, 0xca, 0x31, 0x9c, 0x78, 0x24,
-	0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78,
-	0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x5a, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e,
-	0x72, 0x7e, 0xae, 0x3e, 0xc8, 0xc5, 0xba, 0x79, 0xa9, 0x25, 0xe5, 0xf9, 0x45, 0xd9, 0x60, 0x8e,
-	0x7e, 0x99, 0x89, 0x7e, 0x05, 0xd8, 0x7b, 0x49, 0x6c, 0x60, 0x17, 0x1b, 0x03, 0x02, 0x00, 0x00,
-	0xff, 0xff, 0xf1, 0x7b, 0x48, 0xea, 0xf2, 0x00, 0x00, 0x00,
+	0x44, 0xd2, 0xf3, 0xd3, 0xf3, 0xc1, 0x12, 0xfa, 0x20, 0x16, 0x44, 0x8d, 0x92, 0x37, 0x17, 0x9f,
+	0x2b, 0x48, 0x8f, 0x53, 0x62, 0x4a, 0x50, 0x6a, 0x59, 0x6a, 0x51, 0x89, 0x90, 0x22, 0x17, 0x4f,
+	0x5a, 0x62, 0x66, 0x4e, 0x69, 0x51, 0x6a, 0x7c, 0x49, 0x65, 0x41, 0xaa, 0x04, 0x93, 0x02, 0xa3,
+	0x06, 0x67, 0x10, 0x37, 0x54, 0x2c, 0xa4, 0xb2, 0x20, 0x55, 0x48, 0x8c, 0x8b, 0xad, 0x20, 0x31,
+	0x39, 0x3b, 0xb5, 0x44, 0x82, 0x19, 0x2c, 0x09, 0xe5, 0x29, 0xa9, 0x70, 0x71, 0x81, 0x0d, 0x0b,
+	0x48, 0x2c, 0x2d, 0x86, 0xaa, 0x2a, 0x2d, 0x4e, 0x4d, 0x91, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x08,
+	0x82, 0xf2, 0x9c, 0x5c, 0x4e, 0x3c, 0x94, 0x63, 0x38, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39,
+	0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0xb8, 0xf0, 0x58, 0x8e, 0xe1, 0xc6, 0x63,
+	0x39, 0x86, 0x28, 0xb5, 0xf4, 0xcc, 0x92, 0x8c, 0xd2, 0x24, 0xbd, 0xe4, 0xfc, 0x5c, 0x7d, 0x90,
+	0xfb, 0x75, 0xf3, 0x52, 0x4b, 0xca, 0xf3, 0x8b, 0xb2, 0xc1, 0x1c, 0xfd, 0x32, 0x13, 0xfd, 0x0a,
+	0xb0, 0x67, 0x93, 0xd8, 0xc0, 0xee, 0x37, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xb1, 0xbc, 0x9a,
+	0x74, 0x00, 0x01, 0x00, 0x00,
 }
 
 func (m *EventBadRevert) Marshal() (dAtA []byte, err error) {
@@ -124,12 +162,38 @@ func (m *EventBadRevert) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i--
 		dAtA[i] = 0x12
 	}
-	if len(m.Module) > 0 {
-		i -= len(m.Module)
-		copy(dAtA[i:], m.Module)
-		i = encodeVarintEvents(dAtA, i, uint64(len(m.Module)))
+	return len(dAtA) - i, nil
+}
+
+func (m *EventPause) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *EventPause) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *EventPause) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Paused {
 		i--
-		dAtA[i] = 0xa
+		if m.Paused {
+			dAtA[i] = 1
+		} else {
+			dAtA[i] = 0
+		}
+		i--
+		dAtA[i] = 0x8
 	}
 	return len(dAtA) - i, nil
 }
@@ -151,10 +215,6 @@ func (m *EventBadRevert) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.Module)
-	if l > 0 {
-		n += 1 + l + sovEvents(uint64(l))
-	}
 	l = len(m.FailureType)
 	if l > 0 {
 		n += 1 + l + sovEvents(uint64(l))
@@ -162,6 +222,18 @@ func (m *EventBadRevert) Size() (n int) {
 	l = len(m.Packet)
 	if l > 0 {
 		n += 1 + l + sovEvents(uint64(l))
+	}
+	return n
+}
+
+func (m *EventPause) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.Paused {
+		n += 2
 	}
 	return n
 }
@@ -201,38 +273,6 @@ func (m *EventBadRevert) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: EventBadRevert: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Module", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowEvents
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthEvents
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthEvents
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Module = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field FailureType", wireType)
@@ -297,6 +337,76 @@ func (m *EventBadRevert) Unmarshal(dAtA []byte) error {
 			}
 			m.Packet = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipEvents(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthEvents
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *EventPause) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowEvents
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: EventPause: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: EventPause: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Paused", wireType)
+			}
+			var v int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEvents
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				v |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			m.Paused = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := skipEvents(dAtA[iNdEx:])
