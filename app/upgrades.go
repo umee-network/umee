cc856abf590f9575aaf0f969715413c353abed26
Robert Zaremba <robert@zaremba.ch>
2023-02-07 13:12:25 +0100

    chore: remove rc upgrade handlers (#1787)
    ## Description
    
    Let's cleanup "test" upgrade handlers. Especially that we are going to remove canon-2

--- app/upgrades.go
+++ app/upgrades.go
@@ -32,30 +32,6 @@ func (app UmeeApp) RegisterUpgradeHandlers(bool) {
 	app.registerUpgrade3_1to3_3(upgradeInfo)
 	app.registerUpgrade3_2to3_3(upgradeInfo)
 	app.registerUpgrade3_3to4_0(upgradeInfo)
-	app.registerUpgrade4_0to4_0rc3(upgradeInfo)
-	app.registerUpgrade4_0rc3to4_0rc4(upgradeInfo)
-}
-
-// performs upgrade from v4.0-rc3 -> v4.0-rc4
-func (app UmeeApp) registerUpgrade4_0rc3to4_0rc4(_ upgradetypes.Plan) {
-	const planName = "v4.0-rc4"
-	app.UpgradeKeeper.SetUpgradeHandler(
-		planName,
-		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			ctx.Logger().Info("Upgrade handler execution", "name", planName)
-			return fromVM, nil
-		})
-}
-
-// performs upgrade from v4.0-rc2 (or rc1) -> v4.0-rc3
-func (app UmeeApp) registerUpgrade4_0to4_0rc3(_ upgradetypes.Plan) {
-	const planName = "v4.0-rc3"
-	app.UpgradeKeeper.SetUpgradeHandler(
-		planName,
-		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			ctx.Logger().Info("Upgrade handler execution", "name", planName)
-			return fromVM, nil
-		})
 }
 
 // performs upgrade from v3.3 -> v4
