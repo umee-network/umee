9d04ef41c1bb6434aa7d1cd3aa5549333fd2433b
toteki <63419657+toteki@users.noreply.github.com>
2023-02-13 00:45:34 -0700

    lint++

--- app/upgrades.go
+++ app/upgrades.go
@@ -41,23 +41,24 @@ func (app UmeeApp) RegisterUpgradeHandlers(bool) {
 // performs upgrade from v4.0 to v4.1
 func (app *UmeeApp) registerUpgrade4_1(upgradeInfo upgradetypes.Plan) {
 	const planName = "v4.1"
-	app.UpgradeKeeper.SetUpgradeHandler(planName, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		ctx.Logger().Info("Upgrade handler execution", "name", planName)
-		ctx.Logger().Info("Run v4.1 migration")
-		leverageUpgrader := leveragekeeper.NewMigrator(&app.LeverageKeeper)
-		err := leverageUpgrader.MigrateBNB(ctx)
-		if err != nil {
-			ctx.Logger().Error("Unable to run v4.1 leverage Migration!", "err", err)
-			return fromVM, err
-		}
-		oracleUpgrader := oraclekeeper.NewMigrator(&app.OracleKeeper)
-		err = oracleUpgrader.MigrateBNB(ctx)
-		if err != nil {
-			ctx.Logger().Error("Unable to run v4.1 oracle Migration!", "err", err)
-			return fromVM, err
-		}
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-	},
+	app.UpgradeKeeper.SetUpgradeHandler(planName,
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			ctx.Logger().Info("Upgrade handler execution", "name", planName)
+			ctx.Logger().Info("Run v4.1 migration")
+			leverageUpgrader := leveragekeeper.NewMigrator(&app.LeverageKeeper)
+			err := leverageUpgrader.MigrateBNB(ctx)
+			if err != nil {
+				ctx.Logger().Error("Unable to run v4.1 leverage Migration!", "err", err)
+				return fromVM, err
+			}
+			oracleUpgrader := oraclekeeper.NewMigrator(&app.OracleKeeper)
+			err = oracleUpgrader.MigrateBNB(ctx)
+			if err != nil {
+				ctx.Logger().Error("Unable to run v4.1 oracle Migration!", "err", err)
+				return fromVM, err
+			}
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
 	)
 
 	app.storeUpgrade(planName, upgradeInfo, storetypes.StoreUpgrades{
