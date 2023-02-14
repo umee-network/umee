945944cc0c3d636f1b8b0b20a30b972fc4293166
toteki <63419657+toteki@users.noreply.github.com>
2023-02-13 21:34:11 -0700

    conflict--

--- x/oracle/types/errors.go
+++ x/oracle/types/errors.go
@@ -9,28 +9,6 @@ import (
 
 // Oracle sentinel errors
 var (
-<<<<<<< HEAD
-	ErrInvalidExchangeRate   = sdkerrors.Register(ModuleName, 1, "invalid exchange rate")
-	ErrNoPrevote             = sdkerrors.Register(ModuleName, 2, "no prevote")
-	ErrNoVote                = sdkerrors.Register(ModuleName, 3, "no vote")
-	ErrNoVotingPermission    = sdkerrors.Register(ModuleName, 4, "unauthorized voter")
-	ErrInvalidHash           = sdkerrors.Register(ModuleName, 5, "invalid hash")
-	ErrInvalidHashLength     = sdkerrors.Register(ModuleName, 6, fmt.Sprintf("invalid hash length; should equal %d", tmhash.TruncatedSize)) //nolint: lll
-	ErrVerificationFailed    = sdkerrors.Register(ModuleName, 7, "hash verification failed")
-	ErrRevealPeriodMissMatch = sdkerrors.Register(ModuleName, 8, "reveal period of submitted vote does not match with registered prevote") //nolint: lll
-	ErrInvalidSaltLength     = sdkerrors.Register(ModuleName, 9, "invalid salt length; must be 64")
-	ErrInvalidSaltFormat     = sdkerrors.Register(ModuleName, 10, "invalid salt format")
-	ErrNoAggregatePrevote    = sdkerrors.Register(ModuleName, 11, "no aggregate prevote")
-	ErrNoAggregateVote       = sdkerrors.Register(ModuleName, 12, "no aggregate vote")
-	ErrUnknownDenom          = sdkerrors.Register(ModuleName, 13, "unknown denom")
-	ErrNegativeOrZeroRate    = sdkerrors.Register(ModuleName, 14, "invalid exchange rate; should be positive")
-	ErrExistingPrevote       = sdkerrors.Register(ModuleName, 15, "prevote already submitted for this voting period")
-	ErrBallotNotSorted       = sdkerrors.Register(ModuleName, 16, "ballot must be sorted before this operation")
-	ErrNotImplemented        = sdkerrors.Register(ModuleName, 17, "functon not implemented")
-	ErrNoHistoricPrice       = sdkerrors.Register(ModuleName, 18, "no historic price for this denom at this block")
-	ErrNoMedian              = sdkerrors.Register(ModuleName, 19, "no median for this denom at this block")
-	ErrNoMedianDeviation     = sdkerrors.Register(ModuleName, 20, "no median deviation for this denom at this block")
-=======
 	ErrInvalidExchangeRate     = errors.Register(ModuleName, 1, "invalid exchange rate")
 	ErrNoPrevote               = errors.Register(ModuleName, 2, "no prevote")
 	ErrNoVote                  = errors.Register(ModuleName, 3, "no vote")
@@ -53,5 +31,4 @@ var (
 	ErrNoMedianDeviation       = errors.Register(ModuleName, 20, "no median deviation for this denom at this block")
 	ErrMalformedLatestAvgPrice = errors.Register(ModuleName, 21, "malformed latest avg price, expecting one byte")
 	ErrNoLatestAvgPrice        = errors.Register(ModuleName, 22, "no latest average price")
->>>>>>> 37909a3 (fix: deprecated use of sdkerrors (#1788))
 )
