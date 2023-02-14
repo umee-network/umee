b11c442f5df6027faa1ae108208545eefecbceb7
Robert Zaremba <robert@zaremba.ch>
2023-02-15 00:24:06 +0100

    fix: bnb migration

--- x/leverage/keeper/migrations.go
+++ x/leverage/keeper/migrations.go
@@ -17,30 +17,29 @@ func NewMigrator(keeper *Keeper) Migrator {
 
 // MigrateBNB fixes the BNB base denom for the 4.1 upgrade
 func (m Migrator) MigrateBNB(ctx sdk.Context) error {
+	// Bad BNB token denom
 	badDenom := "ibc/77BCD42E49E5B7E0FC6B269FEBF0185B15044F13F6F38CA285DF0AF883459F40"
 	// Ensure zero supply of the token being removed from leverage registry
 	uSupply := m.keeper.GetUTokenSupply(ctx, types.ToUTokenDenom(badDenom))
 	if !uSupply.IsZero() {
-		return types.ErrBadSupplyDetected.Wrap(uSupply.String())
+		ctx.Logger().Error("can't correctly migrate leverage with existing supply",
+			"token", badDenom, "total_u_supply", uSupply)
+		return nil
 	}
-	// Get the existing BNB token settings
-	correctDenom := "ibc/8184469200C5E667794375F5B0EC3B9ABB6FF79082941BF5D0F8FF59FEBA862E"
 	token, err := m.keeper.GetTokenSettings(ctx, badDenom)
 	if err != nil {
 		return err
 	}
-	// Modify base denom
-	token.BaseDenom = correctDenom
 	// Delete initial entry in token registry
 	store := ctx.KVStore(m.keeper.storeKey)
-	badKey := types.KeyRegisteredToken(badDenom)
-	store.Delete(badKey)
-	// Add back to store, but bypass the hooks in SetRegisteredToken
-	trueKey := types.KeyRegisteredToken(correctDenom)
+	store.Delete(types.KeyRegisteredToken(badDenom))
+	// Modify and back to store, but bypass the hooks in SetRegisteredToken
+	correctDenom := "ibc/8184469200C5E667794375F5B0EC3B9ABB6FF79082941BF5D0F8FF59FEBA862E"
+	token.BaseDenom = correctDenom
 	bz, err := m.keeper.cdc.Marshal(&token)
 	if err != nil {
 		return err
 	}
-	store.Set(trueKey, bz)
+	store.Set(types.KeyRegisteredToken(correctDenom), bz)
 	return nil
 }
