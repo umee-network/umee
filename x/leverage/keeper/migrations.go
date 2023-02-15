32ea821cefdf71dbd665e78eac914c8d4b281630
toteki <63419657+toteki@users.noreply.github.com>
2023-02-15 06:59:18 -0700

    log level

--- x/leverage/keeper/migrations.go
+++ x/leverage/keeper/migrations.go
@@ -29,7 +29,7 @@ func (m Migrator) MigrateBNB(ctx sdk.Context) (bool, error) {
 	}
 	token, err := m.keeper.GetTokenSettings(ctx, badDenom)
 	if err != nil {
-		ctx.Logger().Info("leverage migration skipped due to missing token", "err", err.Error())
+		ctx.Logger().Error("leverage migration skipped due to missing token", "err", err.Error())
 		// If it's not a registered token, then we don't need to run this migration
 		return false, nil
 	}
