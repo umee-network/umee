90f7fa90d7d71d580bfabfd6b74d5a6f3824ac4a
toteki <63419657+toteki@users.noreply.github.com>
2023-02-17 18:49:10 -0700

    fix conflicts

--- app/upgrades.go
+++ app/upgrades.go
@@ -35,23 +35,7 @@ func (app UmeeApp) RegisterUpgradeHandlers(bool) {
 	app.registerUpgrade3_3to4_0(upgradeInfo)
 	app.registerUpgrade4_0_1(upgradeInfo)
 	app.registerUpgrade4_1(upgradeInfo)
-<<<<<<< HEAD
-=======
 	app.registerUpgrade4_1_0rc3(upgradeInfo)
-	app.registerUpgrade4_2(upgradeInfo)
-}
-
-// performs upgrade from v4.1 to v4.2
-func (app *UmeeApp) registerUpgrade4_2(upgradeInfo upgradetypes.Plan) {
-	const planName = "v4.2"
-	app.UpgradeKeeper.SetUpgradeHandler(planName, onlyModuleMigrations(app, planName))
-
-	app.storeUpgrade(planName, upgradeInfo, storetypes.StoreUpgrades{
-		Added: []string{
-			uibc.ModuleName,
-		},
-	})
->>>>>>> 95924b6 (chore: add 4.1.0-rc3 migration (#1846))
 }
 
 // performs upgrade from v4.1.0-rc2 to v4.1.0-rc3
