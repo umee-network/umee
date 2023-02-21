f358b811a4f2760a5ddb0c8ef372600410d74ca3
Sai Kumar <sai@umee.cc>
2023-02-21 18:07:43 +0530

    remove the experimental tests

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -52,6 +52,38 @@ func (s *IntegrationTestSuite) TestIBCTokenTransfer() {
 		s.Require().NotEmpty(ibcStakeDenom)
 	})
 
+	s.Run("ibc_txs_registered_token", func() {
+		recipient := valAddr.String()
+		token := sdk.NewInt64Coin(appparams.BondDenom, 100000000) // 100UMEE
+		s.sendIBC(gaiaChainID, s.chain.id, recipient, token)
+
+		umeeAPIEndpoint := fmt.Sprintf("http://%s", s.valResources[0].GetHostPort("1317/tcp"))
+
+		// require the recipient account receives the IBC tokens (IBC packets ACKd)
+		var (
+			balances sdk.Coins
+			err      error
+		)
+		s.Require().Eventually(
+			func() bool {
+				balances, err = queryUmeeAllBalances(umeeAPIEndpoint, recipient)
+				s.Require().NoError(err)
+
+				return balances.Len() == 3
+			},
+			time.Minute,
+			5*time.Second,
+		)
+
+		for _, c := range balances {
+			if strings.Contains(c.Denom, "ibc/") {
+				s.Require().NotEmpty(c.Denom)
+				s.Require().Equal(token.Amount.Int64(), c.Amount.Int64())
+				break
+			}
+		}
+	})
+
 	var ibcStakeERC20Addr string
 	s.Run("deploy_stake_erc20 ibcStakeERC20Addr", func() {
 		s.T().Skip("paused due to Ethereum PoS migration and PoW fork")
