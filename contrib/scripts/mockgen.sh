81bd0669d2e07f80477638a0685bcc8f662fa251
Robert Zaremba <robert@zaremba.ch>
2023-02-22 11:55:49 +0100

    move mocks to the mocks package

--- contrib/scripts/mockgen.sh
+++ contrib/scripts/mockgen.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-#mockgen_cmd="mockgen"
 mockgen_cmd="go run github.com/golang/mock/mockgen"
 
-$mockgen_cmd -source ./x/uibc/expected_keepers.go -package fixtures -destination ./x/uibc/fixtures/keepers.go
+$mockgen_cmd -source ./x/uibc/expected_keepers.go -package mocks -destination ./x/uibc/mocks/keepers.go
