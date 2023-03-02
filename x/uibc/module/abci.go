3ee7d21c5c8cf1fe47587b54a55cddaa7fbfda95
Sai Kumar <sai@umee.cc>
2023-03-02 20:42:59 +0530

    fix: fix the quota duration reset

--- x/uibc/module/abci.go
+++ x/uibc/module/abci.go
@@ -1,6 +1,8 @@
 package uibc
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/umee-network/umee/v4/util"
@@ -10,6 +12,9 @@ import (
 // BeginBlock implements BeginBlock for the x/uibc module.
 func BeginBlock(ctx sdk.Context, keeper keeper.Keeper) {
 	quotaExpires, err := keeper.GetExpire(ctx)
+	fmt.Println("quota expire ", quotaExpires)
+	fmt.Println("ctx block time  ", ctx.BlockTime())
+
 	util.Panic(err)
 
 	// reset quotas
