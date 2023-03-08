bff6d8602a04a7d848268f145a98b54e4f200551
toteki <63419657+toteki@users.noreply.github.com>
2023-03-08 16:34:43 -0700

    add paginated iterator util

--- util/store/iter.go
+++ util/store/iter.go
@@ -21,3 +21,25 @@ func Iterate(store sdk.KVStore, prefix []byte, cb func(key, val []byte) error) e
 
 	return nil
 }
+
+// IteratePaginated through keys in a kvStore that start with a given prefix
+// using a provided function. If the provided function returns an error,
+// iteration stops and the error is returned.
+// Accepts pagination parameters: limit defines a number of keys per page, and page
+// indicates what page to skip to when iterating.
+// For example, page = 3 and limit = 10 will iterate over the 21st - 30th keys that
+// would be found by a non-paginated iterator.
+func IteratePaginated(store sdk.KVStore, prefix []byte, page, limit uint, cb func(key, val []byte) error) error {
+	iter := sdk.KVStorePrefixIteratorPaginated(store, prefix, page, limit)
+	defer iter.Close()
+
+	for ; iter.Valid(); iter.Next() {
+		key, val := iter.Key(), iter.Value()
+
+		if err := cb(key, val); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
