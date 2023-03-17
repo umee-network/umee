a04a86a41514f5bf9a5b65a17f79a35252dd2e8e
Robert Zaremba <robert@zaremba.ch>
2023-03-17 15:48:53 +0100

    rename

--- x/uibc/quota/keeper/keeper.go
+++ x/uibc/quota/keeper/keeper.go
@@ -16,7 +16,7 @@ type Keeper struct {
 	storeKey       storetypes.StoreKey
 	cdc            codec.BinaryCodec
 	leverageKeeper uibc.LeverageKeeper
-	oracleKeeper   uibc.Oracle
+	oracle         uibc.Oracle
 	ics4Wrapper    porttypes.ICS4Wrapper
 }
 
@@ -29,7 +29,7 @@ func NewKeeper(
 		storeKey:       key,
 		ics4Wrapper:    ics4Wrapper,
 		leverageKeeper: leverageKeeper,
-		oracleKeeper:   oracleKeeper,
+		oracle:         oracleKeeper,
 	}
 }
 
