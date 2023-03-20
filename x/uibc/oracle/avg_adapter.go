cd83922df20d2edaaaef0211ed85793ed9943d59
Robert Zaremba <robert@zaremba.ch>
2023-03-20 16:37:51 +0100

    feat: uibc oracle adapter (#1928)
    * feat: proper segregation of uibc -- oracle dependency
    
    * rename
    
    * mock regeneration
    
    * integrate oracle adapter

---
+++ x/uibc/oracle/avg_adapter.go
@@ -0,0 +1,23 @@
+package oracle
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/umee-network/umee/v4/x/uibc"
+)
+
+type UmeeAvgPriceOracle interface {
+	HistoricAvgPrice(ctx sdk.Context, denom string) (sdk.Dec, error)
+}
+
+func FromUmeeAvgPriceOracle(o UmeeAvgPriceOracle) uibc.Oracle {
+	return umeeAvgPriceOracle{o}
+}
+
+type umeeAvgPriceOracle struct {
+	o UmeeAvgPriceOracle
+}
+
+func (o umeeAvgPriceOracle) Price(ctx sdk.Context, denom string) (sdk.Dec, error) {
+	return o.o.HistoricAvgPrice(ctx, denom)
+}
