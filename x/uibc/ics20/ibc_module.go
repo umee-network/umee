be9749efa4768dc105a1f5908fce67460464b73a
Sai Kumar <sai@umee.cc>
2023-03-27 11:04:08 +0530

    feat: ibc inflow only for registered tokens

--- x/uibc/ics20/ibc_module.go
+++ x/uibc/ics20/ibc_module.go
@@ -2,25 +2,31 @@ package ics20
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	ibctransfer "github.com/cosmos/ibc-go/v5/modules/apps/transfer"
 	ibctransfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
 	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
 
+	ibcutil "github.com/umee-network/umee/v4/util/ibc"
+	ltypes "github.com/umee-network/umee/v4/x/leverage/types"
 	"github.com/umee-network/umee/v4/x/uibc/ics20/keeper"
 )
 
 // IBCModule embeds the ICS-20 transfer IBCModule where we only override specific
 // methods.
 type IBCModule struct {
+	// leverage keeper
+	lkeeper keeper.LeverageKeeper
 	// embed the ICS-20 transfer's AppModule
 	ibctransfer.IBCModule
 
 	keeper keeper.Keeper
 }
 
-func NewIBCModule(am ibctransfer.IBCModule, k keeper.Keeper) IBCModule {
+func NewIBCModule(leverageKeeper keeper.LeverageKeeper, am ibctransfer.IBCModule, k keeper.Keeper) IBCModule {
 	return IBCModule{
+		lkeeper:   leverageKeeper,
 		IBCModule: am,
 		keeper:    k,
 	}
@@ -33,6 +39,23 @@ func (am IBCModule) OnRecvPacket(
 	packet channeltypes.Packet,
 	relayer sdk.AccAddress,
 ) ibcexported.Acknowledgement {
+	// Allowing only registered token
+	var data ibctransfertypes.FungibleTokenPacketData
+	if err := ibctransfertypes.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
+		ackErr := sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot unmarshal ICS-20 transfer packet data")
+		return channeltypes.NewErrorAcknowledgement(ackErr)
+	}
+
+	_, denom, err := ibcutil.GetFundsFromPacket(packet)
+	if err != nil {
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+
+	_, err = am.lkeeper.GetTokenSettings(ctx, denom)
+	if err != nil && ltypes.ErrNotRegisteredToken.Is(err) {
+		return channeltypes.NewErrorAcknowledgement(err)
+	}
+
 	ack := am.IBCModule.OnRecvPacket(ctx, packet, relayer)
 	if ack.Success() {
 		var data ibctransfertypes.FungibleTokenPacketData
