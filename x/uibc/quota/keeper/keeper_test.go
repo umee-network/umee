6de9801cc755a18d07ca786f074f390f439760bf
Egor Kostetskiy <kosegor@gmail.com>
2023-03-31 20:41:17 -0300

    test uibc/quota.go

--- x/uibc/quota/keeper/keeper_test.go
+++ x/uibc/quota/keeper/keeper_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/codec"
+	porttypes "github.com/cosmos/ibc-go/v6/modules/core/05-port/types"
+
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
@@ -93,6 +96,20 @@ func initKeeperTestSuite(t *testing.T) *KeeperTestSuite {
 func initSimpleKeeper(t *testing.T) (sdk.Context, keeper.Keeper) {
 	storeKey := storetypes.NewMemoryStoreKey("quota")
 	k := keeper.NewKeeper(nil, storeKey, nil, nil, nil)
+	ctx, _ := tsdk.NewCtxOneStore(t, storeKey)
+	return ctx, k
+}
+
+// creates keeper with all external dependencies (app, leverage etc...)
+func initFullKeeper(
+	t *testing.T,
+	cdc codec.BinaryCodec,
+	ics4Wrapper porttypes.ICS4Wrapper,
+	leverageKeeper uibc.LeverageKeeper,
+	oracleKeeper uibc.Oracle,
+) (sdk.Context, keeper.Keeper) {
+	storeKey := storetypes.NewMemoryStoreKey("quota")
+	k := keeper.NewKeeper(cdc, storeKey, ics4Wrapper, leverageKeeper, oracleKeeper)
 	ctx, _ := tsdk.NewCtxOneStore(t, storeKey)
 	return ctx, k
 }
--- x/uibc/quota/keeper/quota_test.go
+++ x/uibc/quota/keeper/quota_test.go
@@ -3,6 +3,22 @@ package keeper_test
 import (
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/codec"
+	ctypes "github.com/cosmos/cosmos-sdk/codec/types"
+
+	"github.com/umee-network/umee/v4/x/uibc"
+
+	"github.com/umee-network/umee/v4/x/oracle/types"
+
+	lfixtures "github.com/umee-network/umee/v4/x/leverage/fixtures"
+
+	sdkmath "cosmossdk.io/math"
+
+	ltypes "github.com/umee-network/umee/v4/x/leverage/types"
+
+	"github.com/golang/mock/gomock"
+	"github.com/umee-network/umee/v4/x/uibc/mocks"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"gotest.tools/v3/assert"
 
@@ -51,4 +67,66 @@ func TestResetQuota(t *testing.T) {
 	q, err = k.GetOutflows(ctx, umeeQuota.Denom)
 	assert.NilError(t, err)
 	assert.DeepEqual(t, q.Amount, sdk.NewDec(0))
+}
+
+func TestKeeper_CheckAndUpdateQuota(t *testing.T) {
+	invalidToken := sdk.NewCoin("u/u/umee", sdkmath.NewInt(100))
+	umeeUToken := sdk.NewCoin("u/umee", sdkmath.NewInt(100))
+	atomToken := sdk.NewCoin("atom", sdkmath.NewInt(1000))
+	daiToken := sdk.NewCoin("dai", sdkmath.NewInt(50))
+	// gomock initializations
+	leverageCtrl := gomock.NewController(t)
+	defer leverageCtrl.Finish()
+	leverageMlk := mocks.NewMockLeverageKeeper(leverageCtrl)
+
+	oracleCtrl := gomock.NewController(t)
+	defer oracleCtrl.Finish()
+	oracleMlk := mocks.NewMockOracle(oracleCtrl)
+
+	interfaceRegistry := ctypes.NewInterfaceRegistry()
+	marshaller := codec.NewProtoCodec(interfaceRegistry)
+	ctx, k := initFullKeeper(t, marshaller, nil, leverageMlk, oracleMlk)
+	err := k.ResetAllQuotas(ctx)
+	assert.NilError(t, err)
+
+	// invalid token
+	leverageMlk.EXPECT().ExchangeUToken(ctx, invalidToken).Return(sdk.Coin{}, ltypes.ErrNotUToken).AnyTimes()
+
+	err = k.CheckAndUpdateQuota(ctx, invalidToken.Denom, invalidToken.Amount)
+	assert.ErrorIs(t, err, ltypes.ErrNotUToken)
+
+	// uumee
+	leverageMlk.EXPECT().ExchangeUToken(ctx, umeeUToken).Return(
+		sdk.NewCoin("umee", sdkmath.NewInt(100)),
+		nil,
+	).AnyTimes()
+	leverageMlk.EXPECT().GetTokenSettings(ctx, "umee").Return(ltypes.Token{}, ltypes.ErrNotRegisteredToken).AnyTimes()
+
+	err = k.CheckAndUpdateQuota(ctx, umeeUToken.Denom, umeeUToken.Amount)
+	// returns nil when the error is ErrNotRegisteredToken
+	assert.NilError(t, err)
+
+	// atom
+	leverageMlk.EXPECT().GetTokenSettings(ctx, "atom").Return(
+		lfixtures.Token("atom", "ATOM", 6), nil,
+	).AnyTimes()
+	oracleMlk.EXPECT().Price(ctx, "ATOM").Return(sdk.Dec{}, types.ErrMalformedLatestAvgPrice)
+
+	err = k.CheckAndUpdateQuota(ctx, atomToken.Denom, atomToken.Amount)
+	assert.ErrorIs(t, err, types.ErrMalformedLatestAvgPrice)
+
+	// dai
+	leverageMlk.EXPECT().GetTokenSettings(ctx, "dai").Return(
+		lfixtures.Token("dai", "DAI", 6), nil,
+	).AnyTimes()
+	oracleMlk.EXPECT().Price(ctx, "DAI").Return(sdk.MustNewDecFromStr("0.37"), nil)
+
+	err = k.SetParams(ctx, uibc.DefaultParams())
+	assert.NilError(t, err)
+
+	setQuotas := sdk.DecCoins{sdk.NewInt64DecCoin("dai", 10000)}
+	k.SetOutflows(ctx, setQuotas)
+
+	err = k.CheckAndUpdateQuota(ctx, daiToken.Denom, daiToken.Amount)
+	assert.NilError(t, err)
 }
