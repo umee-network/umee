56f4cf9781d1081f1ba66fa5242f5a23dc004b5d
Robert Zaremba <robert@zaremba.ch>
2023-05-09 17:30:46 +0200

    review

--- util/coin/fixtures.go
+++ util/coin/fixtures.go
@@ -11,20 +11,29 @@ const umee = appparams.BondDenom
 
 // common coins used in tests
 var (
+	// the uToken denom "u/uumee"
 	UumeeDenom = leveragetypes.ToUTokenDenom(umee)
-	Umee1      = New(umee, 1)
-	Umee10k    = New(umee, 10_000)
-	UUmee1     = Utoken(umee, 1)
+	// 1uumee Coin
+	Umee1 = New(umee, 1)
+	// 10_000uumee Coin
+	Umee10k = New(umee, 10_000)
+	// 1u/uumee Coin
+	UUmee1 = Utoken(umee, 1)
 
+	// 1uumee DecCoin
 	Umee1dec = DecF(umee, 1)
 
-	Atom   = "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9"
-	Atom1  = New(Atom, 1)
+	Atom = "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9"
+	// 1<atom ibc denom> Coin
+	Atom1 = New(Atom, 1)
+	// 1u/<atom ibc denom> Coin
 	UAtom1 = Utoken(Atom, 1)
 
-	Atom1dec    = DecF(Atom, 1)
+	Atom1dec = DecF(Atom, 1)
+	// 1.25<atom ibc denom> DecCoin
 	Atom1_25dec = DecF(Atom, 1.25)
 
+	// a test denom
 	Dollar = "dollar"
 )
 
@@ -33,7 +42,7 @@ func UmeeDec(amount float64) sdk.DecCoin {
 	return DecF(appparams.BondDenom, amount)
 }
 
-// Utoken creates a uToken coin.
+// Utoken creates a uToken DecCoin.
 func Utoken(denom string, amount int64) sdk.Coin {
 	return New(leveragetypes.ToUTokenDenom(denom), amount)
 }
