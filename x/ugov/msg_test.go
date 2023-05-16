f176c4086209c9a14b72dc2398850fac938b19d6
Adam Moser <63419657+toteki@users.noreply.github.com>
2023-05-16 09:04:42 -0600

    revert unintentional change

--- x/ugov/msg_test.go
+++ x/ugov/msg_test.go
@@ -28,9 +28,7 @@ func TestMsgGovUpdateMinGasPrice(t *testing.T) {
 
 	// TODO: probably we should use Dec string
 	require.Equal(
-		`authority:"umee10d07y265gmmuvt4z0w9aw880jnsr700jg5w6jp"`+
-			` min_gas_price:<denom:"ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9"`+
-			` amount:"1250000000000000000" > `,
+		`<authority: umee10d07y265gmmuvt4z0w9aw880jnsr700jg5w6jp, min_gas_price: 1.250000000000000000ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9>`,
 		msg.String())
 
 	signers := msg.GetSigners()
