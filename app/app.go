fbda10cf76d27fef30a9e40ee699ba22c4e04c40
Robert Zaremba <robert@zaremba.ch>
2023-05-19 21:40:03 +0200

    Update app/app.go
    Co-authored-by: Sai Kumar <17549398+gsk967@users.noreply.github.com>

--- app/app.go
+++ app/app.go
@@ -640,7 +640,7 @@ func New(
 
 	// The last arguments can contain custom message handlers, and custom query handlers,
 	// if we want to allow any custom callbacks
-	availableCapabilities := "iterator,staking,stargate,cosmwasm_1_1"
+	availableCapabilities := "iterator,staking,stargate,cosmwasm_1_1,umee"
 	app.WasmKeeper = wasm.NewKeeper(
 		appCodec,
 		keys[wasm.StoreKey],
