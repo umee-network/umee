d4bddf26fe73629cd3c8ea393d8fb7913224d0ec
Sai Kumar <sai@umee.cc>
2023-06-12 14:56:15 +0530

    move stargate helpers to client pkg

--- app/wasm/stargate/querier.go
+++ app/wasm/stargate/querier.go
@@ -8,6 +8,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
+
+	// stargate client
+	"github.com/umee-network/umee/v5/client"
 )
 
 // Querier dispatches whitelisted stargate queries
@@ -34,50 +37,11 @@ func Querier(
 			return nil, err
 		}
 
-		bz, err := ConvertProtoToJSONMarshal(protoResponseType, res.Value, cdc)
+		bz, err := client.ConvertProtoToJSONMarshal(protoResponseType, res.Value, cdc)
 		if err != nil {
 			return nil, err
 		}
 
 		return bz, nil
-	}
-}
-
-// ConvertProtoToJsonMarshal  unmarshals the given bytes into a proto message and then marshals it to json.
-// This is done so that clients calling stargate queries do not need to define their own proto unmarshalers,
-// being able to use response directly by json `marshaling, which is supported in cosmwasm.
-func ConvertProtoToJSONMarshal(protoResponseType codec.ProtoMarshaler, bz []byte, cdc codec.Codec) ([]byte, error) {
-	// unmarshal binary into stargate response data structure
-	err := cdc.Unmarshal(bz, protoResponseType)
-	if err != nil {
-		return nil, wasmvmtypes.Unknown{}
-	}
-
-	bz, err = cdc.MarshalJSON(protoResponseType)
-	if err != nil {
-		return nil, wasmvmtypes.Unknown{}
-	}
-
-	protoResponseType.Reset()
-
-	return bz, nil
-}
-
-// ConvertSdkCoinsToWasmCoins converts sdk type coins to wasm vm type coins
-func ConvertSdkCoinsToWasmCoins(coins []sdk.Coin) wasmvmtypes.Coins {
-	var toSend wasmvmtypes.Coins
-	for _, coin := range coins {
-		c := ConvertSdkCoinToWasmCoin(coin)
-		toSend = append(toSend, c)
-	}
-	return toSend
-}
-
-// ConvertSdkCoinToWasmCoin converts a sdk type coin to a wasm vm type coin
-func ConvertSdkCoinToWasmCoin(coin sdk.Coin) wasmvmtypes.Coin {
-	return wasmvmtypes.Coin{
-		Denom: coin.Denom,
-		// Note: gamm tokens have 18 decimal places, so 10^22 is common, no longer in u64 range
-		Amount: coin.Amount.String(),
 	}
 }
---
+++ client/stargate.go
@@ -0,0 +1,46 @@
+package client
+
+import (
+	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// ConvertProtoToJsonMarshal  unmarshals the given bytes into a proto message and then marshals it to json.
+// This is done so that clients calling stargate queries do not need to define their own proto unmarshalers,
+// being able to use response directly by json `marshaling, which is supported in cosmwasm.
+func ConvertProtoToJSONMarshal(protoResponseType codec.ProtoMarshaler, bz []byte, cdc codec.Codec) ([]byte, error) {
+	// unmarshal binary into stargate response data structure
+	err := cdc.Unmarshal(bz, protoResponseType)
+	if err != nil {
+		return nil, wasmvmtypes.Unknown{}
+	}
+
+	bz, err = cdc.MarshalJSON(protoResponseType)
+	if err != nil {
+		return nil, wasmvmtypes.Unknown{}
+	}
+
+	protoResponseType.Reset()
+
+	return bz, nil
+}
+
+// ConvertSdkCoinsToWasmCoins converts sdk type coins to wasm vm type coins
+func ConvertSdkCoinsToWasmCoins(coins []sdk.Coin) wasmvmtypes.Coins {
+	var toSend wasmvmtypes.Coins
+	for _, coin := range coins {
+		c := ConvertSdkCoinToWasmCoin(coin)
+		toSend = append(toSend, c)
+	}
+	return toSend
+}
+
+// ConvertSdkCoinToWasmCoin converts a sdk type coin to a wasm vm type coin
+func ConvertSdkCoinToWasmCoin(coin sdk.Coin) wasmvmtypes.Coin {
+	return wasmvmtypes.Coin{
+		Denom: coin.Denom,
+		// Note: gamm tokens have 18 decimal places, so 10^22 is common, no longer in u64 range
+		Amount: coin.Amount.String(),
+	}
+}
