4104e6f04eaa08c82e8d118b6df4a2b2318618d2
toteki <63419657+toteki@users.noreply.github.com>
2023-06-27 04:09:05 -0600

    borrow and maxborrow tests

--- x/leverage/keeper/msg_server_test.go
+++ x/leverage/keeper/msg_server_test.go
@@ -1310,6 +1310,13 @@ func (s *IntegrationTestSuite) TestMsgBorrow() {
 	// collateral value is $50 (current) or $100 (historic)
 	// collateral weights are always 0.25 in testing
 
+	// create an UMEE borrower using STABLE collateral
+	stableUmeeBorrower := s.newAccount(coin.New(stableDenom, 40_000000))
+	s.supply(stableUmeeBorrower, coin.New(stableDenom, 40_000000))
+	s.collateralize(stableUmeeBorrower, coin.New("u/"+stableDenom, 40_000000))
+	s.borrow(stableUmeeBorrower, coin.New(umeeDenom, 3_000000))
+	// UMEE and STABLE have the same price but different collateral weights
+
 	tcs := []testCase{
 		{
 			"uToken",
@@ -1332,14 +1339,19 @@ func (s *IntegrationTestSuite) TestMsgBorrow() {
 			coin.New(umeeDenom, 70_000000),
 			nil,
 		}, {
-			"additional borrow",
-			borrower,
-			coin.New(umeeDenom, 20_000000),
+			"stable umee borrower (acceptable)",
+			stableUmeeBorrower,
+			coin.New(umeeDenom, 17_000000),
 			nil,
+		}, {
+			"stable umee borrower (borrow factor limit)",
+			stableUmeeBorrower,
+			coin.New(umeeDenom, 1_000000),
+			types.ErrUndercollaterized,
 		}, {
 			"max supply utilization",
 			borrower,
-			coin.New(umeeDenom, 10_000000),
+			coin.New(umeeDenom, 9_000000),
 			types.ErrMaxSupplyUtilization,
 		}, {
 			"atom borrow",
@@ -1457,6 +1469,13 @@ func (s *IntegrationTestSuite) TestMsgMaxBorrow() {
 	// collateral value is $50 (current) or $100 (historic)
 	// collateral weights are always 0.25 in testing
 
+	// create an UMEE borrower using STABLE collateral
+	stableUmeeBorrower := s.newAccount(coin.New(stableDenom, 100_000000))
+	s.supply(stableUmeeBorrower, coin.New(stableDenom, 100_000000))
+	s.collateralize(stableUmeeBorrower, coin.New("u/"+stableDenom, 100_000000))
+	s.borrow(stableUmeeBorrower, coin.New(umeeDenom, 30_000000))
+	// UMEE and STABLE have the same price but different collateral weights
+
 	tcs := []struct {
 		msg  string
 		addr sdk.AccAddress
@@ -1497,6 +1516,11 @@ func (s *IntegrationTestSuite) TestMsgMaxBorrow() {
 			"pump borrower",
 			pumpborrower,
 			coin.New(pumpDenom, 6_250000),
+			nil,
+		}, {
+			"stable umee borrower",
+			stableUmeeBorrower,
+			coin.New(umeeDenom, 20_000000),
 			nil,
 		},
 	}
