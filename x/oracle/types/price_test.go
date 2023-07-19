6cb7a98adb947ac77ba7ee3c6e61a5b81c00900d
Robert Zaremba <robert@zaremba.ch>
2023-07-19 11:01:45 +0200

    tests

---
+++ x/oracle/types/price_test.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"gotest.tools/v3/assert"
+)
+
+func TestPrices(t *testing.T) {
+	zero := sdk.ZeroDec()
+	p1 := *NewPrice(zero, "atom", 4)
+	p2 := *NewPrice(zero, "atom", 3)
+	prices := Prices{p1, p2}
+	prices.Sort()
+	assert.DeepEqual(t, Prices{p2, p1}, prices)
+}
