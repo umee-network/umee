567205a6b7ca4af40b0de8bbaa1bdd1b529d1875
Robert Zaremba <robert@zaremba.ch>
2023-07-19 11:27:19 +0200

    docs update

--- util/bpmath/doc.go
+++ util/bpmath/doc.go
@@ -1,2 +1,4 @@
 // Package bpmath provides types and functions for doing basis point operations.
+// There is no precision lost for operations like `a * b` where `a` or `b` is basis points,
+// and the other variable is `Int`.
 package bpmath
