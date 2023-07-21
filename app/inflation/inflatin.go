b804fbd0d165c861a6fe7bbdc675ecfccdc7c835
Robert Zaremba <robert@zaremba.ch>
2023-07-21 18:02:06 +0200

    lint & fix

--- app/inflation/inflatin.go
+++ app/inflation/inflatin.go
@@ -1,6 +1,7 @@
 package inflation
 
 import (
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 
@@ -12,7 +13,9 @@ type Calculator struct {
 	MintKeeper  MintKeeper
 }
 
-func (c Calculator) InflationRate(ctx sdk.Context, minter minttypes.Minter, params minttypes.Params, bondedRatio sdk.Dec) sdk.Dec {
+func (c Calculator) InflationRate(
+	ctx sdk.Context, minter minttypes.Minter, p minttypes.Params, bondedRatio sdk.Dec) sdk.Dec {
+
 	maxSupply, _ := sdk.NewIntFromString("21_000_000_000_000_000000")
 
 	totalSupply := c.MintKeeper.StakingTokenSupply(ctx)
@@ -23,17 +26,17 @@ func (c Calculator) InflationRate(ctx sdk.Context, minter minttypes.Minter, para
 
 	// TODO: here we need to use a new inflation function and check if we need to go to the
 	// next inflation cycle
-	// minter.Inflation = c.calculateInflation()
-	minter.Inflation = minttypes.DefaultInflationCalculationFn(ctx, minter, params, bondedRatio)
-	return readjustSupply(totalSupply, maxSupply, minter, params)
+	minter.Inflation = c.calculateInflation(ctx, minter, p, bondedRatio)
+	return readjustSupply(totalSupply, maxSupply, minter, p)
 }
 
-func (c Calculator) calculateInflation() sdk.Dec {
-	panic("todo")
+func (c Calculator) calculateInflation(
+	ctx sdk.Context, minter minttypes.Minter, p minttypes.Params, bondedRatio sdk.Dec) sdk.Dec {
+	return minttypes.DefaultInflationCalculationFn(ctx, minter, p, bondedRatio)
 }
 
 // TODO: add unit tests to this function
-func readjustSupply(totalSupply, maxSupply sdk.Int, minter minttypes.Minter, params minttypes.Params) sdk.Dec {
+func readjustSupply(totalSupply, maxSupply math.Int, minter minttypes.Minter, params minttypes.Params) sdk.Dec {
 	minter.AnnualProvisions = minter.NextAnnualProvisions(params, totalSupply)
 	newSupply := minter.BlockProvision(params).Amount
 	newTotalSupply := totalSupply.Add(newSupply)
