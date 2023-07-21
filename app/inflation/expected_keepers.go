cab66ffc64e054a4419895c39fd579b36e4c6424
Robert Zaremba <robert@zaremba.ch>
2023-07-21 17:47:36 +0200

    simplify mint keeper interface

--- app/inflation/expected_keepers.go
+++ app/inflation/expected_keepers.go
@@ -3,17 +3,8 @@ package inflation
 import (
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	// ugov "github.com/umee-network/umee/v5/x/ugov"
 )
 
 type MintKeeper interface {
-	SetParams(ctx sdk.Context, params minttypes.Params)
-	GetParams(ctx sdk.Context) (params minttypes.Params)
 	StakingTokenSupply(ctx sdk.Context) math.Int
-	SetMinter(ctx sdk.Context, minter minttypes.Minter)
-	GetMinter(ctx sdk.Context) (minter minttypes.Minter)
-	BondedRatio(ctx sdk.Context) sdk.Dec
-	MintCoins(ctx sdk.Context, newCoins sdk.Coins) error
-	AddCollectedFees(ctx sdk.Context, fees sdk.Coins) error
 }
