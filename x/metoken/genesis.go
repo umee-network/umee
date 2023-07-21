1537007e3fa3d2e703eda8aad416a8b320fc74df
Robert Zaremba <robert@zaremba.ch>
2023-07-21 19:12:58 +0200

    fix: metoken Index receiver (#2160)

--- x/metoken/genesis.go
+++ x/metoken/genesis.go
@@ -103,7 +103,7 @@ func (ib IndexBalances) AssetBalance(denom string) (int, AssetBalance) {
 }
 
 // SetAssetBalance overrides an asset balance if exists in the list, otherwise add it to the list.
-func (ib IndexBalances) SetAssetBalance(balance AssetBalance) {
+func (ib *IndexBalances) SetAssetBalance(balance AssetBalance) {
 	i, _ := ib.AssetBalance(balance.Denom)
 
 	if i < 0 {
