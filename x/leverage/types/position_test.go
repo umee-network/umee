f7b1fe9f313c5d8df5970fe68840dea831afe779
toteki <63419657+toteki@users.noreply.github.com>
2023-08-02 06:39:30 -0600

    create wide range of position testing assets, special pairs

--- x/leverage/types/position_test.go
+++ x/leverage/types/position_test.go
@@ -8,16 +8,67 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+func testToken(denom, cw, lt string) Token {
+	return Token{
+		BaseDenom:            denom,
+		CollateralWeight:     sdk.MustNewDecFromStr(cw),
+		LiquidationThreshold: sdk.MustNewDecFromStr(lt),
+	}
+}
+
+func testPair(borrow, collateral, cw, lt string) SpecialAssetPair {
+	return SpecialAssetPair{
+		Borrow:               borrow,
+		Collateral:           collateral,
+		CollateralWeight:     sdk.MustNewDecFromStr(cw),
+		LiquidationThreshold: sdk.MustNewDecFromStr(lt),
+	}
+}
+
+// These tokens are used for testing asset positions. They are arranged so that
+// A < B < C < D < E < F < G for both collateral weight and liquidation threshold,
+// but H = I = 0 for collateral weight and G < H < I for liquidation threshold.
+// This should produce a wide range of behaviors.
+var orderedTokens = []Token{
+	testToken("AAAA", "0.1", "0.15"),
+	testToken("BBBB", "0.2", "0.25"),
+	testToken("CCCC", "0.3", "0.35"),
+	testToken("DDDD", "0.4", "0.45"),
+	testToken("EEEE", "0.5", "0.55"),
+	testToken("FFFF", "0.6", "0.65"),
+	testToken("GGGG", "0.7", "0.75"),
+	testToken("HHHH", "0.0", "0.85"),
+	testToken("IIII", "0.0", "0.95"),
+}
+
+// These special asset pairs are used for testing asset positions.
+// The even-numbered assets (B,D,F,H) are involved in special pairs, and the others aren't.
+// When combined with the order of the test assets, many complex positions can be formed.
+var orderedPairs = []SpecialAssetPair{
+	// F and H are paired at [0.6,0.8] but not looped
+	// D can borrow any (B,D,F,H) at [0.5,0.5]
+	// B can be borrowed by any (B,D,F,H) at [0.3,0.3]
+	// H is looped at [0.1,0.1] even though its token weights are [0.0,0.85]
+	testPair("FFFF", "HHHH", "0.6", "0.8"),
+	testPair("HHHH", "FFFF", "0.6", "0.8"),
+
+	testPair("DDDD", "BBBB", "0.5", "0.5"),
+	testPair("DDDD", "DDDD", "0.5", "0.5"),
+	testPair("DDDD", "FFFF", "0.5", "0.5"),
+	testPair("DDDD", "HHHH", "0.5", "0.5"),
+
+	testPair("BBBB", "BBBB", "0.3", "0.3"),
+	testPair("BBBB", "DDDD", "0.3", "0.3"),
+	testPair("BBBB", "FFFF", "0.3", "0.3"),
+	testPair("BBBB", "HHHH", "0.3", "0.3"),
+
+	testPair("HHHH", "HHHH", "0.1", "0.1"),
+}
+
 func TestBorrowLimit(t *testing.T) {
 	position := NewAccountPosition(
-		[]Token{
-			{
-				BaseDenom:            "AAAA",
-				CollateralWeight:     sdk.MustNewDecFromStr("0.25"),
-				LiquidationThreshold: sdk.MustNewDecFromStr("0.5"),
-			},
-		},
-		[]SpecialAssetPair{},
+		orderedTokens,
+		orderedPairs,
 		sdk.NewDecCoins(
 			sdk.NewDecCoinFromDec("AAAA", sdk.MustNewDecFromStr("100.00")),
 		),
@@ -25,19 +76,13 @@ func TestBorrowLimit(t *testing.T) {
 		false,
 	)
 
-	assert.Assert(t, sdk.MustNewDecFromStr("25.00").Equal(position.Limit()), "simple borrow limit")
+	assert.Assert(t, sdk.MustNewDecFromStr("10.00").Equal(position.Limit()), "simple borrow limit")
 }
 
 func TestLiquidationThreshold(t *testing.T) {
 	position := NewAccountPosition(
-		[]Token{
-			{
-				BaseDenom:            "AAAA",
-				CollateralWeight:     sdk.MustNewDecFromStr("0.25"),
-				LiquidationThreshold: sdk.MustNewDecFromStr("0.5"),
-			},
-		},
-		[]SpecialAssetPair{},
+		orderedTokens,
+		orderedPairs,
 		sdk.NewDecCoins(
 			sdk.NewDecCoinFromDec("AAAA", sdk.MustNewDecFromStr("100.00")),
 		),
@@ -45,5 +90,5 @@ func TestLiquidationThreshold(t *testing.T) {
 		true,
 	)
 
-	assert.Assert(t, sdk.MustNewDecFromStr("50.00").Equal(position.Limit()), "simple liquidation threshold")
+	assert.Assert(t, sdk.MustNewDecFromStr("15.00").Equal(position.Limit()), "simple liquidation threshold")
 }
