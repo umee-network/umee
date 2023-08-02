424ae4e1c81309c0322991d5f0deb4a8792835b3
Robert Zaremba <robert@zaremba.ch>
2023-08-02 11:35:00 +0200

    refactore(uibc): clean up ibc middleware organization

--- x/uibc/quota/ics4_wrapper.go
+++ x/uibc/quota/ics4_wrapper.go
@@ -1,19 +1,30 @@
 package quota
 
 import (
+	"cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+	ics20types "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
-	"github.com/cosmos/ibc-go/v6/modules/core/exported"
+	porttypes "github.com/cosmos/ibc-go/v6/modules/core/05-port/types"
+
+	ibcutil "github.com/umee-network/umee/v5/util/ibc"
+	"github.com/umee-network/umee/v5/x/uibc/quota/keeper"
 )
 
-// GetAppVersion implements types.Middleware
-func (im ICS20Middleware) GetAppVersion(ctx sdk.Context, portID string, channelID string) (string, bool) {
-	return im.kb.GetAppVersion(ctx, portID, channelID)
+// QuotaICS4 wraps SendPacket to check IBC quota.
+type QuotaICS4 struct {
+	porttypes.ICS4Wrapper
+
+	kb keeper.Builder
+}
+
+func NewQuotaICS4(parent porttypes.ICS4Wrapper, kb keeper.Builder) QuotaICS4 {
+	return QuotaICS4{parent, kb}
 }
 
 // SendPacket implements types.Middleware
-func (im ICS20Middleware) SendPacket(
+func (q QuotaICS4) SendPacket(
 	ctx sdk.Context,
 	chanCap *capabilitytypes.Capability,
 	sourcePort string,
@@ -22,12 +33,23 @@ func (im ICS20Middleware) SendPacket(
 	timeoutTimestamp uint64,
 	data []byte,
 ) (uint64, error) {
-	return im.kb.SendPacket(ctx, chanCap, sourcePort, sourceChannel, timeoutHeight, timeoutTimestamp, data)
-}
+	k := q.kb.Keeper(&ctx)
+	params := k.GetParams()
 
-// WriteAcknowledgement implements types.Middleware
-func (im ICS20Middleware) WriteAcknowledgement(ctx sdk.Context, chanCap *capabilitytypes.Capability,
-	packet exported.PacketI, ack exported.Acknowledgement,
-) error {
-	return im.kb.WriteAcknowledgement(ctx, chanCap, packet, ack)
+	if !params.IbcStatus.IBCTransferEnabled() {
+		return 0, ics20types.ErrSendDisabled
+	}
+
+	funds, denom, err := ibcutil.GetFundsFromPacket(data)
+	if err != nil {
+		return 0, errors.Wrap(err, "bad packet in rate limit's SendPacket")
+	}
+
+	if params.IbcStatus.OutflowQuotaEnabled() {
+		if err := k.CheckAndUpdateQuota(denom, funds); err != nil {
+			return 0, errors.Wrap(err, "sendPacket over the IBC Quota")
+		}
+	}
+
+	return q.ICS4Wrapper.SendPacket(ctx, chanCap, sourcePort, sourceChannel, timeoutHeight, timeoutTimestamp, data)
 }
