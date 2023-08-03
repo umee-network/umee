b7705fa670d06a28766004ad908c8053700ee0ba
Robert Zaremba <robert@zaremba.ch>
2023-08-03 16:37:31 +0200

    fix

--- x/leverage/keeper/token.go
+++ x/leverage/keeper/token.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/umee-network/umee/v5/util/checkers"
 	"github.com/umee-network/umee/v5/util/store"
