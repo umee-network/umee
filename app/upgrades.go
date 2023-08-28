5b804a4ade9da1058f839f0dcbe1e23b8185359f
Robert Zaremba <robert@zaremba.ch>
2023-08-28 20:09:00 +0200

    feat: v6.0 proposal migration (#2208)
    * migrate leverage proposals
    
    * add upgradev6 module
    
    * cleanup
    
    * remove props.txt
    
    * update directory name
    
    * lint
    
    * cleanup proto descriptor in upgrade package
    
    * Update app/upgrades.go
    
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>
    
    * fix leverage SetParams
    
    ---------
    
    Co-authored-by: Adam Moser <63419657+toteki@users.noreply.github.com>

--- app/upgrades.go
+++ app/upgrades.go
@@ -29,6 +29,7 @@ import (
 	leveragekeeper "github.com/umee-network/umee/v6/x/leverage/keeper"
 	leveragetypes "github.com/umee-network/umee/v6/x/leverage/types"
 
+	"github.com/umee-network/umee/v6/app/upgradev6x0"
 	oraclekeeper "github.com/umee-network/umee/v6/x/oracle/keeper"
 	oracletypes "github.com/umee-network/umee/v6/x/oracle/types"
 	"github.com/umee-network/umee/v6/x/ugov"
@@ -56,25 +57,7 @@ func (app UmeeApp) RegisterUpgradeHandlers(bool) {
 	app.registerUpgrade("v5.0", upgradeInfo, ugov.ModuleName, wasm.ModuleName)
 	app.registerUpgrade5_1(upgradeInfo)
 	app.registerUpgrade6(upgradeInfo)
-	// app.registerNewTokenEmissionUpgrade(upgradeInfo)
 }
-
-// TODO: this upgrade registration is just for testing purpose, once we finalize the release for new token emission
-// then we need to change planName and storeUpgrades
-// func (app *UmeeApp) registerNewTokenEmissionUpgrade(upgradeInfo upgradetypes.Plan) {
-// 	// TODO:finalize the name
-// 	planName := "token_emission"
-// 	app.UpgradeKeeper.SetUpgradeHandler(planName,
-// 		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-// 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
-// 		},
-// 	)
-
-// 	app.storeUpgrade(planName, upgradeInfo, storetypes.StoreUpgrades{
-// 		Added:   []string{metoken.ModuleName},
-// 		Deleted: []string{"gravity"},
-// 	})
-// }
 
 func (app *UmeeApp) registerUpgrade6(upgradeInfo upgradetypes.Plan) {
 	planName := "v6.0"
@@ -82,6 +65,13 @@ func (app *UmeeApp) registerUpgrade6(upgradeInfo upgradetypes.Plan) {
 
 	app.UpgradeKeeper.SetUpgradeHandler(planName,
 		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			if err := upgradev6x0.Migrate(ctx, app.GovKeeper); err != nil {
+				return fromVM, err
+			}
+			app.LeverageKeeper.SetParams(ctx, leveragetypes.DefaultParams())
+			// TODO: need to register emergency group
+			// TODO: set new inflation params
+
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		},
 	)
