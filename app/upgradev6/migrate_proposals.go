41e72b2c623d87b771c6afeae9c4a8e3e7553191
Robert Zaremba <robert@zaremba.ch>
2023-08-28 17:36:20 +0200

    add upgradev6 module

---
+++ app/upgradev6/migrate_proposals.go
@@ -0,0 +1,63 @@
+package upgradev6
+
+import (
+	"fmt"
+
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	"github.com/gogo/protobuf/proto"
+	"github.com/tendermint/tendermint/libs/log"
+
+	"github.com/umee-network/umee/v6/x/leverage/types"
+)
+
+var done bool
+
+func Migrate(ctx sdk.Context, gk govkeeper.Keeper) error {
+	logger := ctx.Logger()
+	if done {
+		logger.Error("Migration already done")
+		return nil
+	}
+	done = true
+
+	proposals := gk.GetProposals(ctx)
+	for _, p := range proposals {
+		if len(p.Messages) != 1 {
+			logger.Debug("Ignoring, too many messages", "msgs", p.Messages)
+			continue
+		}
+
+		cached := p.Messages[0].GetCachedValue()
+		if oldUpdateRegistry, ok := cached.(*MsgGovUpdateRegistry); ok {
+			migrateMsgGovUpdateRegistry(ctx, p, oldUpdateRegistry, gk, logger)
+		} else {
+			logger.Debug("Ignoring, not MsgGovUpdateRegistry",
+				"msg_type", proto.MessageName(cached.(proto.Message)))
+		}
+
+	}
+	return nil
+}
+
+func migrateMsgGovUpdateRegistry(ctx sdk.Context, p *govv1.Proposal, old *MsgGovUpdateRegistry, gk govkeeper.Keeper, logger log.Logger) {
+	newMsg := types.MsgGovUpdateRegistry{
+		Authority:    old.Authority,
+		Description:  "",
+		AddTokens:    old.AddTokens,
+		UpdateTokens: old.UpdateTokens,
+	}
+	var err error
+	p.Messages[0], err = cdctypes.NewAnyWithValue(&newMsg)
+	if err != nil {
+		logger.Error("Can't pack ANY", err)
+	} else {
+		if p.Metadata != "" {
+			p.Metadata = fmt.Sprintf("{\"title\":%q,\"summary\":%q}", old.Title, old.Description)
+		}
+		logger.Info("\n\nMIGRATING proposal:\n" + p.String())
+		gk.SetProposal(ctx, *p)
+	}
+}
