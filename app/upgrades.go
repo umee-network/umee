d14764ca38d029c102be220e6baa9632eb9af82a
Robert Zaremba <robert@zaremba.ch>
2023-08-30 23:36:54 +0200

    remove proposal migration

--- app/upgrades.go
+++ app/upgrades.go
@@ -29,7 +29,6 @@ import (
 	leveragekeeper "github.com/umee-network/umee/v6/x/leverage/keeper"
 	leveragetypes "github.com/umee-network/umee/v6/x/leverage/types"
 
-	"github.com/umee-network/umee/v6/app/upgradev6x0"
 	oraclekeeper "github.com/umee-network/umee/v6/x/oracle/keeper"
 	oracletypes "github.com/umee-network/umee/v6/x/oracle/types"
 	"github.com/umee-network/umee/v6/x/ugov"
@@ -55,6 +54,7 @@ func (app UmeeApp) RegisterUpgradeHandlers() {
 	app.registerUpgrade("v4.4", upgradeInfo)
 	app.registerUpgrade("v5.0", upgradeInfo, ugov.ModuleName, wasm.ModuleName)
 	app.registerUpgrade5_1(upgradeInfo)
+	app.registerUpgrade("v5.2", upgradeInfo) // v5.2 migration is not compatible with v6, so leaving default here.
 	app.registerUpgrade6(upgradeInfo)
 }
 
@@ -65,9 +65,6 @@ func (app *UmeeApp) registerUpgrade6(upgradeInfo upgradetypes.Plan) {
 	app.UpgradeKeeper.SetUpgradeHandler(planName,
 		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			ctx.Logger().Info("-----------------------------\n---------------")
-			if err := upgradev6x0.Migrate(ctx, app.GovKeeper); err != nil {
-				return fromVM, err
-			}
 			if err := app.LeverageKeeper.SetParams(ctx, leveragetypes.DefaultParams()); err != nil {
 				return fromVM, err
 			}
