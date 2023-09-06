7ebe7b8bb965821e648b1f40db0b2facabff2aa7
toteki <63419657+toteki@users.noreply.github.com>
2023-09-06 09:07:17 -0600

    provide examples

--- util/sdkutil/string.go
+++ util/sdkutil/string.go
@@ -9,6 +9,7 @@ import (
 
 // FormatDec formats a sdk.Dec as a string with no trailing zeroes after the decimal point,
 // omitting the decimal point as well for whole numbers.
+// e.g. 4.000 -> 4 and 3.500 -> 3.5
 func FormatDec(d sdk.Dec) string {
 	dStr := d.String()
 	parts := strings.Split(dStr, ".")
@@ -16,7 +17,7 @@ func FormatDec(d sdk.Dec) string {
 		return dStr
 	}
 	integer, decimal := parts[0], parts[1]
-	decimal = strings.TrimRight(decimal, "0")  // no need for trailing zeros after the "."
+	decimal = strings.TrimRight(decimal, "0") // no need for trailing zeros after the "."
 	if decimal == "" {
 		return integer
 	}
@@ -24,7 +25,8 @@ func FormatDec(d sdk.Dec) string {
 }
 
 // FormatDecCoin formats a sdk.DecCoin with no trailing zeroes after the decimal point in its amount,
-// omitting the decimal point as well for whole numbers.
+// omitting the decimal point as well for whole numbers. Also places a space between amount and denom.
+// e.g. 4.000uumee -> 4 uumee and 3.500ibc/abcd -> 3.5 ibc/abcd
 func FormatDecCoin(c sdk.DecCoin) string {
 	return fmt.Sprintf("%s %s", FormatDec(c.Amount), c.Denom)
 }
