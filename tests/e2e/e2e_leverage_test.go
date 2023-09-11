2f9ec2bf52c7b2b70f312cdb9da52c9570b264da
toteki <63419657+toteki@users.noreply.github.com>
2023-09-11 08:41:15 -0600

    leverage gov setup

--- tests/e2e/e2e_leverage_test.go
+++ tests/e2e/e2e_leverage_test.go
@@ -3,6 +3,8 @@ package e2e
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/umee-network/umee/v6/tests/grpc"
+	"github.com/umee-network/umee/v6/x/leverage/fixtures"
 	leveragetypes "github.com/umee-network/umee/v6/x/leverage/types"
 )
 
@@ -49,3 +51,94 @@ func (s *E2ETest) liquidate(addr, target sdk.AccAddress, reward string, repay sd
 func (s *E2ETest) leveragedLiquidate(addr, target sdk.AccAddress, repay, reward string) {
 	s.mustSucceedTx(leveragetypes.NewMsgLeveragedLiquidate(addr, target, repay, reward))
 }
+
+func (s *E2ETest) TestLeverageScenario() {
+	s.Run(
+		"register leverage tokens", func() {
+			tokens := []leveragetypes.Token{
+				fixtures.Token("test1", "WBTC", 8),
+				fixtures.Token("test2", "WETH", 18),
+				fixtures.Token("test3", "USDT", 6),
+			}
+
+			err := grpc.LeverageRegistryUpdate(s.Umee, tokens, nil)
+			s.Require().NoError(err)
+		},
+	)
+}
+
+/*
+func (s *E2ETest) TestMetokenSwapAndRedeem() {
+	var prices []metoken.IndexPrices
+	var index metoken.Index
+	valAddr, err := s.Chain.Validators[0].KeyInfo.GetAddress()
+	s.Require().NoError(err)
+	expectedBalance := mocks.EmptyUSDIndexBalances(mocks.MeUSDDenom)
+
+	s.Run(
+		"create_stable_index", func() {
+			tokens := []ltypes.Token{
+				mocks.ValidToken(mocks.USDTBaseDenom, mocks.USDTSymbolDenom, 6),
+				mocks.ValidToken(mocks.USDCBaseDenom, mocks.USDCSymbolDenom, 6),
+				mocks.ValidToken(mocks.ISTBaseDenom, mocks.ISTSymbolDenom, 6),
+			}
+
+			err = grpc.LeverageRegistryUpdate(s.Umee, tokens, nil)
+			s.Require().NoError(err)
+
+			meUSD := mocks.StableIndex(mocks.MeUSDDenom)
+			err = grpc.MetokenRegistryUpdate(s.Umee, []metoken.Index{meUSD}, nil)
+			s.Require().NoError(err)
+
+			prices = s.checkMetokenBalance(meUSD.Denom, expectedBalance)
+		},
+	)
+
+	s.Run(
+		"swap_100USDT_success", func() {
+			index = s.getIndex(mocks.MeUSDDenom)
+
+			hundredUSDT := sdk.NewCoin(mocks.USDTBaseDenom, sdkmath.NewInt(100_000000))
+			fee := index.Fee.MinFee.MulInt(hundredUSDT.Amount).TruncateInt()
+
+			assetSettings, i := index.AcceptedAsset(mocks.USDTBaseDenom)
+			s.Require().True(i >= 0)
+
+			amountToSwap := hundredUSDT.Amount.Sub(fee)
+			amountToReserves := assetSettings.ReservePortion.MulInt(amountToSwap).TruncateInt()
+			amountToLeverage := amountToSwap.Sub(amountToReserves)
+
+			usdtPrice, err := prices[0].PriceByBaseDenom(mocks.USDTBaseDenom)
+			s.Require().NoError(err)
+			returned := usdtPrice.SwapRate.MulInt(amountToSwap).TruncateInt()
+
+			s.executeSwap(valAddr, hundredUSDT, mocks.MeUSDDenom)
+
+			expectedBalance.MetokenSupply.Amount = expectedBalance.MetokenSupply.Amount.Add(returned)
+			usdtBalance, i := expectedBalance.AssetBalance(mocks.USDTBaseDenom)
+			s.Require().True(i >= 0)
+			usdtBalance.Fees = usdtBalance.Fees.Add(fee)
+			usdtBalance.Reserved = usdtBalance.Reserved.Add(amountToReserves)
+			usdtBalance.Leveraged = usdtBalance.Leveraged.Add(amountToLeverage)
+			expectedBalance.SetAssetBalance(usdtBalance)
+
+			prices = s.checkMetokenBalance(mocks.MeUSDDenom, expectedBalance)
+		},
+	)
+
+	s.Run(
+		"redeem_200meUSD_failure", func() {
+			twoHundredsMeUSD := sdk.NewCoin(mocks.MeUSDDenom, sdkmath.NewInt(200_000000))
+
+			s.executeRedeemWithFailure(
+				valAddr,
+				twoHundredsMeUSD,
+				mocks.USDTBaseDenom,
+				"not enough",
+			)
+
+			prices = s.checkMetokenBalance(mocks.MeUSDDenom, expectedBalance)
+		},
+	)
+}
+*/
