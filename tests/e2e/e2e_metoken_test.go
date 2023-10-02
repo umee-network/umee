091520ea07b5e4f242fd55542a0e0d9bac98d837
kosegor <30661385+kosegor@users.noreply.github.com>
2023-10-02 12:20:09 -0300

    feat: move meToken out of experimental (#2269)
    * move meToken out of experimental
    
    * pr comments
    
    * lint and test
    
    * t
    
    * typo

--- tests/e2e/e2e_metoken_test.go
+++ tests/e2e/e2e_metoken_test.go
@@ -8,7 +8,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
-	"github.com/umee-network/umee/v6/app"
 	"github.com/umee-network/umee/v6/tests/grpc"
 	ltypes "github.com/umee-network/umee/v6/x/leverage/types"
 	"github.com/umee-network/umee/v6/x/metoken"
@@ -20,10 +19,6 @@ func (s *E2ETest) TestMetokenSwapAndRedeem() {
 	valAddr, err := s.Chain.Validators[0].KeyInfo.GetAddress()
 	s.Require().NoError(err)
 	expectedBalance := mocks.EmptyUSDIndexBalances(mocks.MeUSDDenom)
-
-	if app.Experimental {
-		s.T().Skip("Skipping tests for experimental module x/metoken")
-	}
 
 	s.Run(
 		"create_stable_index", func() {
