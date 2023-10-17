95b2b830115c837585b33b47b91905bc6d28ef8c
Robert Zaremba <robert@zaremba.ch>
2023-10-17 11:01:08 +0300

    chore: prepare v6.1 release (#2286)
    * chore: prepare v6.1 release
    
    * add cosmos-sdk update
    
    * lint
    
    * lint

--- x/oracle/migrations/exg_rates_migrations.go
+++ x/oracle/migrations/exg_rates_migrations.go
@@ -4,9 +4,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// MigrateOldExgRatesToExgRatesWithTimestamp will migrate old exchnage rate of denoms into new exchange rate format with
+// ExgRatesWithTimestamp will migrate old exchnage rate of denoms into new exchange rate format with
 // timestamp into store.
-func (m Migrator) MigrateOldExgRatesToExgRatesWithTimestamp(ctx sdk.Context) {
+func (m Migrator) ExgRatesWithTimestamp(ctx sdk.Context) {
 	m.keeper.IterateOldExchangeRates(ctx, func(s string, d sdk.Dec) bool {
 		ctx.Logger().Info("Migrating old exchange rate to new exchange rate format", "denom", s)
 		m.keeper.SetExchangeRateWithTimestamp(ctx, s, d, ctx.BlockTime())
