71ee975f2ec5af2b02e226adc66b3d3d19d336d8
Robert Zaremba <robert@zaremba.ch>
2023-10-17 19:57:57 +0200

    release notes: validator notes

--- app/upgrades.go
+++ app/upgrades.go
@@ -77,7 +77,7 @@ func (app *UmeeApp) registerUpgrade6_1(planName string, upgradeInfo upgradetypes
 			// reference: (today) avg block size in Ethereum is 170kb
 			// Cosmwasm binaries can be few hundreds KB up to 3MB
 			// our blocks with oracle txs as JSON are about 80kB, so protobuf should be less than 40kB.
-			var newMaxBytes int64 = 6_000_000 // 6 MB
+			var newMaxBytes int64 = 4_000_000 // 4 MB
 			p := app.GetConsensusParams(ctx)
 			ctx.Logger().Info("Changing consensus params", "prev", p.Block.MaxBytes, "new", newMaxBytes)
 			p.Block.MaxBytes = newMaxBytes
