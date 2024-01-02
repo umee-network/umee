802b882ca7d4b79afd84fd7945158394817657b6
Sai Kumar <sai@umee.cc>
2024-01-02 17:53:28 +0530

    added tests for record ibc inflow

--- x/uibc/quota/intest/quota_test.go
+++ x/uibc/quota/intest/quota_test.go
@@ -6,6 +6,7 @@ import (
 	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	"github.com/golang/mock/gomock"
 	"gotest.tools/v3/assert"
 
@@ -104,7 +105,7 @@ func TestKeeper_UndoUpdateQuota(t *testing.T) {
 	umeeAmount := sdkmath.NewInt(100_000000)
 	umeePrice := sdk.MustNewDecFromStr("0.37")
 	umeeQuota := sdkmath.NewInt(10000)
-	umeeToken := sdk.NewCoin("umee", umeeAmount)
+	atomToken := sdk.NewCoin("umee", umeeAmount)
 	umeeExponent := 6
 
 	ctrl := gomock.NewController(t)
@@ -124,25 +125,84 @@ func TestKeeper_UndoUpdateQuota(t *testing.T) {
 	).AnyTimes()
 	oracleMock.EXPECT().Price(ctx, "UMEE").Return(umeePrice, nil).AnyTimes()
 
-	err = k.UndoUpdateQuota(umeeToken.Denom, umeeToken.Amount)
+	err = k.UndoUpdateQuota(atomToken.Denom, atomToken.Amount)
 	// the result is ignored due to quota reset
 	assert.NilError(t, err)
 
-	o := k.GetTokenOutflows(umeeToken.Denom)
+	o := k.GetTokenOutflows(atomToken.Denom)
 	assert.DeepEqual(t, o.Amount, sdk.ZeroDec())
 
 	setQuotas := sdk.DecCoins{sdk.NewInt64DecCoin("umee", umeeQuota.Int64())}
 	k.SetTokenOutflows(setQuotas)
 
-	err = k.UndoUpdateQuota(umeeToken.Denom, umeeToken.Amount)
+	err = k.UndoUpdateQuota(atomToken.Denom, atomToken.Amount)
 	assert.NilError(t, err)
 
-	o = k.GetTokenOutflows(umeeToken.Denom)
+	o = k.GetTokenOutflows(atomToken.Denom)
 
 	// the expected quota is calculated as follows:
 	// umee_value = umee_amount * umee_price
 	// expected_quota = current_quota - umee_value
 	powerReduction := sdk.MustNewDecFromStr("10").Power(uint64(umeeExponent))
-	expectedQuota := sdk.NewDec(umeeQuota.Int64()).Sub(sdk.NewDecFromInt(umeeToken.Amount).Quo(powerReduction).Mul(umeePrice))
+	expectedQuota := sdk.NewDec(umeeQuota.Int64()).Sub(sdk.NewDecFromInt(atomToken.Amount).Quo(powerReduction).Mul(umeePrice))
 	assert.DeepEqual(t, o.Amount, expectedQuota)
+}
+
+func TestKeeper_RecordIBCInflow(t *testing.T) {
+	atomAmount := sdkmath.NewInt(100_000000)
+	atomPrice := sdk.MustNewDecFromStr("0.37")
+	// ibc incoming denom from packet is `port/path/base_denom`
+	atomToken := sdk.NewCoin("transfer/channel-10/uatom", atomAmount)
+	atomExponent := 6
+	inflowBaseDenom := "ibc/D6372674F0E9A3A7ADC2FEFD8B2708C5008C7ED04DA6566E279DC1321BDDCB6F"
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	leverageMock := mocks.NewMockLeverage(ctrl)
+	oracleMock := mocks.NewMockOracle(ctrl)
+
+	marshaller := codec.NewProtoCodec(nil)
+	ctx, k := initKeeper(t, marshaller, leverageMock, oracleMock)
+	err := k.ResetAllQuotas()
+	assert.NilError(t, err)
+
+	// ATOM, returns token settings and prices from mock leverage and oracle keepers, no errors expected
+	leverageMock.EXPECT().GetTokenSettings(ctx, inflowBaseDenom).Return(
+		lfixtures.Token(inflowBaseDenom, "ATOM", uint32(atomExponent)), nil,
+	).AnyTimes()
+	oracleMock.EXPECT().Price(ctx, "ATOM").Return(atomPrice, nil).AnyTimes()
+
+	packet := channeltypes.Packet{
+		Sequence:           10,
+		SourcePort:         "transfer",
+		DestinationPort:    "transfer",
+		SourceChannel:      "channel-10",
+		DestinationChannel: "channel-1",
+		Data:               nil,
+	}
+
+	ackErr := k.RecordIBCInflow(packet, atomToken.Denom, atomToken.Amount.String())
+	assert.Assert(t, nil, ackErr)
+
+	o := k.GetTokenInflow(inflowBaseDenom)
+	// expected inflow amount 37 =( atomPrice * atomAmount) / atomExponent
+	inflowAmount := sdkmath.LegacyMustNewDecFromStr("37")
+	assert.DeepEqual(t, o.Amount, inflowAmount)
+
+	ackErr = k.RecordIBCInflow(packet, atomToken.Denom, atomToken.Amount.String())
+	assert.Assert(t, nil, ackErr)
+
+	o = k.GetTokenInflow(inflowBaseDenom)
+	assert.DeepEqual(t, o.Amount, inflowAmount.Add(inflowAmount))
+
+	allInflows, err := k.GetAllInflows()
+	assert.NilError(t, err)
+	assert.DeepEqual(t, allInflows, sdk.DecCoins{o})
+
+	err = k.ResetAllQuotas()
+	assert.NilError(t, err)
+
+	o = k.GetTokenInflow(inflowBaseDenom)
+	assert.DeepEqual(t, o.Amount, sdk.ZeroDec())
 }
