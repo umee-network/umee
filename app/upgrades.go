fe155f75327509da98dbe8803ed534c0336d98eb
Sai Kumar <17549398+gsk967@users.noreply.github.com>
2024-01-10 16:39:24 +0530

    chore: remove the v6.2 upgrade handler (#2362)
    * remove the v6.2 upgrade handler
    
    * remove v6.2 upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -7,32 +7,19 @@ import (
 	icahosttypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/host/types"
 	icatypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/types"
 	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
-	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
-
-	"github.com/cosmos/cosmos-sdk/baseapp"
-
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
-	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	govv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/umee-network/umee/v6/util"
 	leveragetypes "github.com/umee-network/umee/v6/x/leverage/types"
-	"github.com/umee-network/umee/v6/x/uibc"
 )
 
 // RegisterUpgradeHandlersregisters upgrade handlers.
@@ -58,85 +45,8 @@ func (app UmeeApp) RegisterUpgradeHandlers() {
 	app.registerOutdatedPlaceholderUpgrade("v5.2")
 	app.registerUpgrade6_0(upgradeInfo)
 	app.registerOutdatedPlaceholderUpgrade("v6.1")
-	app.registerUpgrade6_2(upgradeInfo)
+	app.registerOutdatedPlaceholderUpgrade("v6.2")
 	app.registerUpgrade("v6.3", upgradeInfo)
-}
-
-func (app *UmeeApp) registerUpgrade6_2(upgradeInfo upgradetypes.Plan) {
-	planName := "v6.2"
-
-	// Set param key table for params module migration
-	for _, subspace := range app.ParamsKeeper.GetSubspaces() {
-		subspace := subspace
-		found := true
-		var keyTable paramstypes.KeyTable
-		switch subspace.Name() {
-		case authtypes.ModuleName:
-			keyTable = authtypes.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		case banktypes.ModuleName:
-			keyTable = banktypes.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		case stakingtypes.ModuleName:
-			keyTable = stakingtypes.ParamKeyTable()
-		case minttypes.ModuleName:
-			keyTable = minttypes.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		case distrtypes.ModuleName:
-			keyTable = distrtypes.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		case slashingtypes.ModuleName:
-			keyTable = slashingtypes.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		case govtypes.ModuleName:
-			keyTable = govv1.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		case crisistypes.ModuleName:
-			keyTable = crisistypes.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		case wasmtypes.ModuleName:
-			keyTable = wasmtypes.ParamKeyTable() //nolint: staticcheck // deprecated but required for upgrade
-		default:
-			// subspace not handled
-			found = false
-		}
-		if found && !subspace.HasKeyTable() {
-			subspace.WithKeyTable(keyTable)
-		}
-	}
-	baseAppLegacySS := app.ParamsKeeper.Subspace(baseapp.Paramspace).WithKeyTable(paramstypes.ConsensusParamsKeyTable())
-
-	app.UpgradeKeeper.SetUpgradeHandler(planName,
-		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-			printPlanName(planName, ctx.Logger())
-
-			// Migrate CometBFT consensus parameters from x/params module to a dedicated x/consensus module.
-			baseapp.MigrateParams(ctx, baseAppLegacySS, &app.ConsensusParamsKeeper)
-
-			// explicitly update the IBC 02-client params, adding the localhost client type
-			params := app.IBCKeeper.ClientKeeper.GetParams(ctx)
-			params.AllowedClients = append(params.AllowedClients, ibcexported.Localhost)
-			app.IBCKeeper.ClientKeeper.SetParams(ctx, params)
-
-			fromVM, err := app.mm.RunMigrations(ctx, app.configurator, fromVM)
-			if err != nil {
-				return fromVM, err
-			}
-			govParams := app.GovKeeper.GetParams(ctx)
-			govParams.MinInitialDepositRatio = sdk.NewDecWithPrec(1, 1).String()
-			err = app.GovKeeper.SetParams(ctx, govParams)
-			if err != nil {
-				return fromVM, err
-			}
-
-			// uibc migrations
-			uIBCKeeper := app.UIbcQuotaKeeperB.Keeper(&ctx)
-			uIBCKeeper.MigrateOutflowSum()
-			err = uIBCKeeper.SetParams(uibc.DefaultParams())
-			return fromVM, err
-		},
-	)
-
-	app.storeUpgrade(planName, upgradeInfo, storetypes.StoreUpgrades{
-		Added: []string{
-			consensustypes.ModuleName,
-			crisistypes.ModuleName,
-		},
-	})
-	// app.registerNewTokenEmissionUpgrade(upgradeInfo)
 }
 
 func (app *UmeeApp) registerUpgrade6_0(upgradeInfo upgradetypes.Plan) {
