647a430c1446964972de2498547074ad145b0a73
toteki <63419657+toteki@users.noreply.github.com>
2024-01-11 10:13:35 -0700

    adjust interest rate algorithm and associated token param rules

--- x/leverage/keeper/interest.go
+++ x/leverage/keeper/interest.go
@@ -17,21 +17,27 @@ func (k Keeper) DeriveBorrowAPY(ctx sdk.Context, denom string) sdk.Dec {
 	if err != nil {
 		return sdk.ZeroDec()
 	}
-
 	if token.Blacklist {
 		// Regardless of params, AccrueAllInterest skips blacklisted denoms
 		return sdk.ZeroDec()
 	}
 
+	// Derive current supply utilization, which will always be between 0.0 and 1.0
 	utilization := k.SupplyUtilization(ctx, denom)
 
+	// Tokens which have reached or exceeded their max supply utilization always use max borrow APY
+	if utilization.GTE(token.MaxSupplyUtilization) {
+		return token.MaxBorrowRate
+	}
+
+	// Tokens which are past kink value but have not reached max supply utilization interpolate between the two
 	if utilization.GTE(token.KinkUtilization) {
 		return Interpolate(
-			utilization,           // x
-			token.KinkUtilization, // x1
-			token.KinkBorrowRate,  // y1
-			sdk.OneDec(),          // x2
-			token.MaxBorrowRate,   // y2
+			utilization,                // x
+			token.KinkUtilization,      // x1
+			token.KinkBorrowRate,       // y1
+			token.MaxSupplyUtilization, // x2
+			token.MaxBorrowRate,        // y2
 		)
 	}
 
