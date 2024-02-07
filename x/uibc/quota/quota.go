098e6b7600ee0a13dbdcc171e3a96219af414743
Sai Kumar <sai@umee.cc>
2024-02-07 19:24:02 +0530

    ++

--- x/uibc/quota/quota.go
+++ x/uibc/quota/quota.go
@@ -231,8 +231,7 @@ func (k Keeper) UndoUpdateQuota(denom string, amount sdkmath.Int) error {
 // RecordIBCInflow will save the inflow amount if token is registered otherwise it will skip
 func (k Keeper) RecordIBCInflow(packet channeltypes.Packet, denom, amount string,
 ) exported.Acknowledgement {
-	// if chain is recevier and sender chain is source then we need create ibc_denom (ibc/hash(channel,denom)) to
-	// check ibc_denom to if it exists in the leverage token registry.
+	// if chain is recevier and sender chain is source then we need create ibc_denom (ibc/hash(channel,denom)).
 	if ics20types.SenderChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), denom) {
 		// SendPacket did not prefix the denom, so we must prefix denom here
 		// NOTE: sourcePrefix already contains the trailing "/"
@@ -243,7 +242,7 @@ func (k Keeper) RecordIBCInflow(packet channeltypes.Packet, denom, amount string
 		// if we receive back a token, that was originally sent from UMEE, then we need to fetch the native denom
 		// receive denom(port/channel/base_denom) to base_denom
 
-		// remove prefix added by sender chain
+		// remove prefix added by the sender chain
 		voucherPrefix := ics20types.GetDenomPrefix(packet.GetSourcePort(), packet.GetSourceChannel())
 		unprefixedDenom := denom[len(voucherPrefix):]
 		// coin denomination used in sending from the escrow address
