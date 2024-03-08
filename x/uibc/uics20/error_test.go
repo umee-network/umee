cf76f8ce15f0e38066bba455f4e29bc8ce9d77d3
Robert Zaremba <robert@zaremba.ch>
2024-03-08 18:59:02 +0100

    update tests

--- x/uibc/uics20/error_test.go
+++ x/uibc/uics20/error_test.go
@@ -7,8 +7,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestErrIs(t *testing.T) {
+func TestErrMemoValidation(t *testing.T) {
 	err := errMemoValidation{errWrongSigner}
 
 	assert.True(t, errors.Is(err, errMemoValidation{}))
+	assert.True(t, errors.Is(errMemoValidation{}, err))
+	assert.False(t, errors.Is(errWrongSigner, err))
+
+	assert.Equal(t, errWrongSigner, errors.Unwrap(err))
+	assert.Contains(t, err.Error(), errWrongSigner.Error())
 }
