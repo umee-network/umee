1b1e5484b01c1cf8d577cdfa2ed9cb0915828499
Robert Zaremba <robert@zaremba.ch>
2024-04-12 22:04:21 +0200

    feat(auction): add noop msg server implementation (#2491)
    * feat(auction): add noop msg server implementation
    
    * auction: update proto
    
    * feat(auction): add noop query implementation
    
    * finish querier

--- x/auction/keeper/keeper.go
+++ x/auction/keeper/keeper.go
@@ -6,24 +6,34 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/umee-network/umee/v6/x/auction"
+	"github.com/umee-network/umee/v6/x/ugov"
 )
 
 type Builder struct {
 	cdc      codec.Codec
 	storeKey storetypes.StoreKey
 	bank     auction.BankKeeper
+	ugov     ugov.EmergencyGroupBuilder
 }
 
-func NewBuilder(cdc codec.Codec, key storetypes.StoreKey, b auction.BankKeeper) Builder {
-	return Builder{cdc: cdc, storeKey: key, bank: b}
+func NewBuilder(cdc codec.Codec,
+	key storetypes.StoreKey,
+	b auction.BankKeeper,
+	ugov ugov.EmergencyGroupBuilder) Builder {
+
+	return Builder{cdc: cdc, storeKey: key, bank: b, ugov: ugov}
 }
 
 func (kb Builder) Keeper(ctx *sdk.Context) Keeper {
 	return Keeper{
-		ctx: ctx,
+		ctx:  ctx,
+		bank: kb.bank,
+		ugov: kb.ugov(ctx),
 	}
 }
 
 type Keeper struct {
-	ctx *sdk.Context
+	ctx  *sdk.Context
+	bank auction.BankKeeper
+	ugov ugov.WithEmergencyGroup
 }
---
+++ x/auction/keeper/rewards.go
@@ -0,0 +1,13 @@
+package keeper
+
+import (
+	"github.com/umee-network/umee/v6/x/auction"
+)
+
+func (k Keeper) rewardsBid(msg *auction.MsgRewardsBid) error {
+	panic("not implemented")
+}
+
+func (k Keeper) currentRewardsAuction() (*auction.QueryRewardsAuctionResponse, error) {
+	panic("not implemented")
+}
