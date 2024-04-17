b0413b65ccd26276824bd694a3325edd28f9b02d
Sai Kumar <17549398+gsk967@users.noreply.github.com>
2024-04-17 20:36:35 +0530

    removed custom msgs and queries for cosmwasm (#2492)

--- app/wasm/custom_plugins.go
+++ app/wasm/custom_plugins.go
@@ -5,33 +5,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
 
-	"github.com/umee-network/umee/v6/app/wasm/msg"
 	"github.com/umee-network/umee/v6/app/wasm/query"
-	inckeeper "github.com/umee-network/umee/v6/x/incentive/keeper"
-	leveragekeeper "github.com/umee-network/umee/v6/x/leverage/keeper"
-	metokenkeeper "github.com/umee-network/umee/v6/x/metoken/keeper"
-	oraclekeeper "github.com/umee-network/umee/v6/x/oracle/keeper"
 )
-
-// RegisterCustomPlugins expose the queries and msgs of native modules to wasm.
-func RegisterCustomPlugins(
-	leverageKeeper leveragekeeper.Keeper,
-	oracleKeeper oraclekeeper.Keeper,
-	incentiveKeeper inckeeper.Keeper,
-	metokenBuilder metokenkeeper.Builder,
-) []wasmkeeper.Option {
-	wasmQueryPlugin := query.NewQueryPlugin(leverageKeeper, oracleKeeper, incentiveKeeper, metokenBuilder)
-	queryPluginOpt := wasmkeeper.WithQueryPlugins(&wasmkeeper.QueryPlugins{
-		Custom: wasmQueryPlugin.CustomQuerier(),
-	})
-
-	messagePluginOpt := wasmkeeper.WithMessageHandlerDecorator(msg.NewMessagePlugin(leverageKeeper))
-
-	return []wasmkeeper.Option{
-		queryPluginOpt,
-		messagePluginOpt,
-	}
-}
 
 // RegisterStargateQueries expose the stargate queries
 func RegisterStargateQueries(queryRouter baseapp.GRPCQueryRouter, codec codec.Codec) []wasmkeeper.Option {
