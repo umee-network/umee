f60a9ce5f4b2b9008bf5754f7ab483b1de0d4667
Robert Zaremba <robert@zaremba.ch>
2024-05-16 10:57:59 +0200

    fix default params

--- x/metoken/params.go
+++ x/metoken/params.go
@@ -5,6 +5,6 @@ func DefaultParams() Params {
 	return Params{
 		RebalancingFrequency:    60 * 60 * 12,     // 12h
 		ClaimingFrequency:       60 * 60 * 24 * 7, // 7d
-		RewardsAuctionFeeFactor: 2000,             // 20% of fees goes to rewards auction
+		RewardsAuctionFeeFactor: 1000,             // 10% of fees goes to rewards auction
 	}
 }
