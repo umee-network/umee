1db9b3c67f7c951202076b39d2046ae9e97d98a6
Robert Zaremba <robert@zaremba.ch>
2024-05-29 21:24:00 +0200

    fix FinalizeAuction and add tests

--- x/auction/keeper/init_test.go
+++ x/auction/keeper/init_test.go
@@ -5,6 +5,7 @@ import (
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 
 	"github.com/umee-network/umee/v6/tests/accs"
 	"github.com/umee-network/umee/v6/tests/tsdk"
@@ -24,6 +25,13 @@ func initKeeper(t *testing.T) TestKeeper {
 	ctx, _ := tsdk.NewCtxOneStore(t, storeKey)
 
 	return TestKeeper{kb.Keeper(&ctx), t, &ctx}
+}
+
+func initKeeperWithGen(t *testing.T) TestKeeper {
+	k := initKeeper(t)
+	err := k.InitGenesis(auction.DefaultGenesis())
+	require.NoError(t, err)
+	return k
 }
 
 type TestKeeper struct {
