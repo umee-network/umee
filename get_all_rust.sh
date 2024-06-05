88b6ed95ab65d5fa28be437050d22e52d3f32a5b
costcould <fliter@myyahoo.com>
2024-06-05 16:04:05 +0800

    chore: remove repeat word
    Signed-off-by: costcould <fliter@myyahoo.com>

---
+++ get_all_rust.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+#cd /Users/fliter
+cd ~
+mkdir new-rust
+
+cd new-rust
+
+
+#git clone git@github.com:rust-lang/rust.git
+git clone git@github.com:rust-lang/rustlings.git
+git clone git@github.com:rust-lang/mdBook.git
+git clone git@github.com:rust-lang/book.git
+git clone git@github.com:rust-lang/rust-analyzer.git
+git clone git@github.com:rust-lang/cargo.git
+git clone git@github.com:rust-lang/rust-clippy.git
+git clone git@github.com:rust-lang/rust-by-example.git
+git clone git@github.com:rust-lang/rustup.git
+git clone git@github.com:rust-lang/rustfmt.git
+git clone git@github.com:rust-lang/rfcs.git
+git clone git@github.com:rust-lang/futures-rs.git
+git clone git@github.com:rust-lang/rust-bindgen.git
+git clone git@github.com:rust-lang/miri.git
+git clone git@github.com:rust-lang/rust.vim.git
+git clone git@github.com:rust-lang/regex.git
+git clone git@github.com:rust-lang/crates.io.git
+git clone git@github.com:rust-lang/hashbrown.git
+git clone git@github.com:rust-lang/log.git
+git clone git@github.com:rust-lang/this-week-in-rust.git
+git clone git@github.com:rust-lang/libc.git
+git clone git@github.com:rust-lang/chalk.git
+git clone git@github.com:rust-lang/cc-rs.git
+git clone git@github.com:rust-lang/async-book.git
+git clone git@github.com:rust-lang/nomicon.git
+git clone git@github.com:rust-lang/git2-rs.git
+git clone git@github.com:rust-lang/rustc-dev-guide.git
+git clone git@github.com:rust-lang/rustc_codegen_cranelift.git
+git clone git@github.com:rust-lang/api-guidelines.git
+
+echo "------------------------"
+
+
+#misspell .
+
+
+echo "------------------------"
+
+
+#golangci-lint run --disable-all -E unconvert
+
+
